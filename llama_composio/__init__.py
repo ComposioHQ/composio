926579819b6483dccd4b914fa7c289e8d6b535e3
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-02-26 16:29:40 +0530

    feat: restrcutre and setup pip distribution for llama and autogen

--- llama_composio/__init__.py
+++ llama_composio/__init__.py
@@ -1,2 +1,2 @@
-from .openapi_tool_spec import APIToolSpec
-from .composio_tool_spec import ComposioToolSpec
+from .tool_spec.openapi_tool_spec import APIToolSpec
+from .tool_spec.composio_tool_spec import ComposioToolSpec
---
+++ llama_composio/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os 
+
+def get_current_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+def resolve_paths(*paths):
+    return os.path.join(*paths)
+
+readme_path = resolve_paths(get_current_dir(), 'README.md')
+
+setup(
+    name = 'composio-llamaindex',
+    version = '0.0.1',
+    author = 'Karan',
+    author_email = 'karan@composio.dev',
+    description = 'Provides integrations skill with 50+ services in LlamaIndex',
+    long_description = open(readme_path).read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/SamparkAI/llama-composio',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.7',
+    include_package_data = True,
+    scripts = ['composio-llamaindex'],
+)
