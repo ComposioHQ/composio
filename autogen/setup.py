6009f9e2e0ee11259b1e4b082285347df5d67f5d
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-03-15 15:15:34 +0530

    chore: update versions

--- autogen/setup.py
+++ autogen/setup.py
@@ -11,7 +11,7 @@ readme_path = resolve_paths(get_current_dir(), 'README.md')
 
 setup(
     name = 'composio-autogen',
-    version = '0.0.6',
+    version = '0.0.7',
     author = 'Utkarsh',
     author_email = 'utkarsh@composio.dev',
     description = 'Provides integrations skill with 50+ services in autogen',
