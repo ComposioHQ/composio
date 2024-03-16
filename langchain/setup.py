090bf4bcc5297215b99f34b048deb742fd28d283
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-03-16 16:04:55 +0530

    feat: add pyproject.toml for langchain

---
+++ langchain/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os 
+
+def get_current_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+def resolve_paths(*paths):
+    return os.path.join(*paths)
+
+readme_path = resolve_paths(get_current_dir(), 'README.md')
+
+setup(
+    name = 'composio_langchain',
+    version = '0.0.1',
+    author = 'Karan',
+    author_email = 'karan@composio.dev',
+    description = 'Use Composio to get an array of tools with your LangChain agent.',
+    long_description = open(readme_path).read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/SamparkAI/composio_sdk',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.7',
+    include_package_data = True
+)
