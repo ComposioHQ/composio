1a45281f8e1afb5071fc517b075a0b5e750bffba
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-03-19 14:53:00 +0530

    feat: add composio core

--- core/composio/sdk/core.py
+++ core/composio/sdk/core.py
@@ -1,15 +1,15 @@
 import time
 import requests
 from pydantic import BaseModel
+
+from .sdk import ConnectionRequest, ConnectedAccount
 from .storage import get_user_connection, get_api_key, save_api_key, save_user_connection
 from uuid import getnode as get_mac
-
-class ConnectionResponse(BaseModel):
-        connectionStatus: str
-        connectionId: str
-        redirectUrl: str
+from .sdk import ComposioSdk
+from .enums import TestConnector
 
 class ComposioCore:
+    sdk: ComposioSdk = None
 
     def __init__(self, base_url = "https://backend.composio.dev/api", manage_auth = True):
         self.base_url = base_url
@@ -26,6 +26,7 @@ class ComposioCore:
                     'Content-Type': 'application/json',
                     'x-api-key': api_key
                 })
+                self.sdk = ComposioSdk(api_key, base_url)
 
     def authenticate(self, hash: str):
         resp = self.http_client.post(f"{self.base_url}/v1/client/auth/identify", json={
@@ -37,75 +38,30 @@ class ComposioCore:
                 'Content-Type': 'application/json',
                 'x-api-key': api_key
             })
+            self.sdk = ComposioSdk(api_key, self.base_url)
             if self.manage_auth:
                 save_api_key(api_key)
             return api_key
 
         raise Exception("Failed to authenticate")
-
-    def get_list_of_apps(self):
-        resp = self.http_client.get(f"{self.base_url}/v1/apps") 
-        return resp.json()
     
-    def get_connector(self, tool_name: str):
-        connector_id = f"test-{tool_name}-connector"
-        resp = self.http_client.get(f"{self.base_url}/v1/connectors/{connector_id}")
-        if resp.status_code == 200:
-            return resp.json()
+    def initiate_connection(self, connectorId: str | TestConnector) -> ConnectionRequest:
+        if isinstance(connectorId, TestConnector):
+            connectorId = connectorId.value
 
-        raise Exception("Failed to get connector")
-
-    def create_connection(self, tool_name: str) -> ConnectionResponse:
-        connector_id = f"test-{tool_name}-connector"
         resp = self.http_client.post(f"{self.base_url}/v1/connections", json={
-            "connectorId": connector_id,
+            "connectorId": connectorId,
         })
         if resp.status_code == 200:
-            return ConnectionResponse(**resp.json())
+            return ConnectionRequest(self.sdk, **resp.json())
         
         raise Exception("Failed to create connection")
-
-    def get_connection(self, connection_id: str):
-        resp = self.http_client.get(f"{self.base_url}/v1/connections/{connection_id}")
-        if resp.status_code == 200:
-            return resp.json()
-        
-        raise Exception("Failed to get connection")
-    
-    def _execute_action(self, action_name: str, connection_id: str, input_data: dict):
-        resp = self.http_client.post(f"{self.base_url}/v1/actions/{action_name}/execute", json={
-            "connectionId": connection_id,
-            "input": input_data
-        })
-        if resp.status_code == 200:
-            return resp.json()
-        
-        raise Exception("Failed to execute action")
     
     def execute_action(self, action_name: str, tool_name: str, input_data: dict):
-        connection = get_user_connection(tool_name.lower())
-        if not connection:
+        connectionId = get_user_connection(tool_name)
+        if not connectionId:
             raise Exception(f"User not authenticated or connection not found. Please authenticate using: composio-cli add {tool_name}")
 
-        resp = self._execute_action(action_name, connection, input_data)
+        app_integration = self.sdk.get_connected_account(connectionId)
+        resp = app_integration.execute_action(action_name, input_data)
         return resp
-
-    def wait_for_connection(self, connection_id: str, app_name: str):
-        while True:
-            connection_info = self.get_connection(connection_id)
-            if connection_info.get('status') == 'ACTIVE':
-                if self.manage_auth:
-                    app_name = connection_info.get("appName", app_name)
-                    save_user_connection(connection_id, app_name)
-                    return True
-            time.sleep(1)  # Wait for a bit before retrying
-    
-    def get_actions(self, tool_names: list[str]):
-        if not tool_names:
-            resp = self.http_client.get(f"{self.base_url}/v1/actions")
-        else:
-            resp = self.http_client.get(f"{self.base_url}/v1/actions?appNames={','.join(tool_names)}")
-        if resp.status_code == 200:
-            return resp.json()
-        
-        raise Exception("Failed to get actions")
