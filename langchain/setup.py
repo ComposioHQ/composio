cee5d9a41cc1e6125731052b2e5675d06d03fa9a
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-03-19 18:13:38 +0530

    feat: update pypi version

--- langchain/setup.py
+++ langchain/setup.py
@@ -11,7 +11,7 @@ readme_path = resolve_paths(get_current_dir(), 'README.md')
 
 setup(
     name = 'composio_langchain',
-    version = '0.0.7',
+    version = '0.0.8',
     author = 'Karan',
     author_email = 'karan@composio.dev',
     description = 'Use Composio to get an array of tools with your LangChain agent.',
