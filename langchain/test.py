1eeb43b2d331101c7abe1dd36aa4f647968801f8
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-03-24 17:53:34 +0530

    fix: errors

--- langchain/test.py
+++ langchain/test.py
@@ -1,12 +1,10 @@
 from langchain.agents import create_openai_functions_agent
 from langchain.agents import AgentExecutor
 from langchain import hub
-from composio_langchain import ComposioToolset
+from composio_langchain import ComposioToolset, App
 from langchain_openai import ChatOpenAI
-from composio.sdk import App
 
 llm = ChatOpenAI(openai_api_key="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")
-
 prompt = hub.pull("hwchase17/openai-functions-agent")
 
 tools = ComposioToolset([App.GITHUB])
