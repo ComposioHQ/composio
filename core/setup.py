21869bd4e8f2099ab4075f55b6d8992311211579
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-04 23:16:32 +0530

    feat: update python requirement to 3.9

--- core/setup.py
+++ core/setup.py
@@ -37,7 +37,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     include_package_data=True,
     cmdclass={
         "install": InstallCommandMiddleware,
