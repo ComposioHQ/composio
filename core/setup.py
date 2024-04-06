6a3c47a5e86e51d1a379a50e5b5af00a816c0981
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-06 18:26:11 +0530

    feat: update version to 0.1.70

--- core/setup.py
+++ core/setup.py
@@ -25,7 +25,7 @@ class InstallCommandMiddleware(install):
 
 setup(
     name="composio_core",
-    version="0.1.69",
+    version="0.1.70",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
