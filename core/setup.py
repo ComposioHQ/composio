7232941ffcfbe37863b487c2fb89d03d72aeca39
Karan Vaidya <kaavee315@gmail.com>
2024-04-08 00:01:00 +0530

    Update enums

--- core/setup.py
+++ core/setup.py
@@ -25,7 +25,7 @@ class InstallCommandMiddleware(install):
 
 setup(
     name="composio_core",
-    version="0.1.82",
+    version="0.1.83",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
