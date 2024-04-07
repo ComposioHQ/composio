361f0378c6afbae391659ba9baaede68ec206071
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-07 23:50:27 +0530

    feat: update version to 0.1.82

--- crew_ai/setup.py
+++ crew_ai/setup.py
@@ -14,7 +14,7 @@ readme_path = resolve_paths(get_current_dir(), "README.md")
 
 setup(
     name="composio_crewai",
-    version="0.1.81",
+    version="0.1.82",
     author="Himanshu",
     author_email="himanshu@composio.dev",
     description="Use Composio to get an array of tools with your CrewAI agent.",
--- langchain/setup.py
+++ langchain/setup.py
@@ -14,7 +14,7 @@ readme_path = resolve_paths(get_current_dir(), "README.md")
 
 setup(
     name="composio_langchain",
-    version="0.1.81",
+    version="0.1.82",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
