a26b891280cc8976123f22ca81b66ec881b73bb3
Himanshu Dixit <hudixt@gmail.com>
2024-04-10 23:10:13 +0530

    feat: changes

--- test/test.mjs
+++ test/test.mjs
@@ -1,8 +1,8 @@
-const userDataPath = '~/.composio';
+const userDataPath = `${process.env.HOME}/.composio`;
 
 // Check if directory exists, delete it if it does
 try {
-  console.log(`sudo rm -rf ${userDataPath}`)
+  console.log(`rm -rf ${userDataPath}`)
   await $`rm -rf ${userDataPath}`;
   console.log(`Existing directory '${userDataPath}' deleted successfully.`);
 } catch (error) {
@@ -10,9 +10,19 @@ try {
 }
 
 // Create directory and write file
-await $`sudo mkdir -p ${userDataPath}`;
-await fs.write(
+await $`mkdir -p ${userDataPath}`;
+await fs.writeFileSync(
   `${userDataPath}/user_data.json`,
   JSON.stringify({ "api_key": "3kmtwhffkxvwebhnm7qwzj" }, null, 2)
 );
-// console.log('user_data.json created successfully.');
+
+// Read file
+const data = await fs.readFile(`${userDataPath}/user_data.json`, 'utf8');
+console.log(data,`${userDataPath}/user_data.json`);
+
+console.log('user_data.json created successfully.');
+
+// Run python script
+const { stdout } = await $`python3 core/start_cli.py`;
+console.log(stdout);
+
