0b6a20ebf9d87b2c448ae5896dc66ec95e816536
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-12 18:40:13 +0530

    feat: update version 0.1.94

--- langchain/setup.py
+++ langchain/setup.py
@@ -14,7 +14,7 @@ readme_path = resolve_paths(get_current_dir(), "README.md")
 
 setup(
     name="composio_langchain",
-    version="0.1.93",
+    version="0.1.94",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
