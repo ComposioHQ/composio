75116866fb95c4ba6d5c066d38e162d617627e8e
Karan Vaidya <kaavee315@gmail.com>
2024-04-15 14:37:18 +0530

    Update version

--- langchain/setup.py
+++ langchain/setup.py
@@ -14,7 +14,7 @@ readme_path = resolve_paths(get_current_dir(), "README.md")
 
 setup(
     name="composio_langchain",
-    version="0.1.101",
+    version="0.2.0",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
--- lyzr/setup.py
+++ lyzr/setup.py
@@ -14,7 +14,7 @@ readme_path = resolve_paths(get_current_dir(), "README.md")
 
 setup(
     name="composio_lyzr",
-    version="0.1.101",
+    version="0.2.0",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Lyzr workflow.",
