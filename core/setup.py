36aa53669958e1d8015cb2a7ae471b221e3f9ce4
Karan Vaidya <kaavee315@gmail.com>
2024-04-24 01:45:47 +0530

    Update version

--- core/setup.py
+++ core/setup.py
@@ -19,7 +19,7 @@ class InstallCommandMiddleware(install):
 
 setup(
     name="composio_core",
-    version="0.2.15",
+    version="0.2.16",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
