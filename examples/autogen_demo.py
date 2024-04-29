1f9da7e883038e3a50c38f317ab6b92deabe9288
sawradip <sawradip0@gmail.com>
2024-04-29 20:23:07 +0600

    Clears example from inside extensions and adds root folder

--- examples/autogen_demo.py
+++ examples/autogen_demo.py
@@ -2,7 +2,7 @@ import os
 
 import dotenv
 from autogen import AssistantAgent, UserProxyAgent
-from plugins.autogen.composio_autogen import App, ComposioToolset
+from composio_autogen import App, ComposioToolset
 
 
 # Loading the variables from .env file
