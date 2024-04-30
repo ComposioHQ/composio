805c2cda8342814a08a91103b818b0454d9a82f0
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-30 14:41:39 +0530

    feat: remove console statements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="composio_core",
-    version="0.2.30",
+    version="0.2.31",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
