eb2f604886943a2f6525d8040df23c2cb5db84c1
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-04-30 13:53:43 +0530

    feat: update test-publish command

--- plugins/crew_ai/setup.py
+++ plugins/crew_ai/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_crewai",
-    version="0.2.26",
+    version="0.2.27",
     author="Himanshu",
     author_email="himanshu@composio.dev",
     description="Use Composio to get an array of tools with your CrewAI agent.",
@@ -23,7 +23,7 @@ setup(
     ],
     python_requires=">=3.9,<4",
     install_requires=[
-        "composio_langchain===0.2.26",
+        "composio_langchain===0.2.27",
     ],
     include_package_data=True,
 )
