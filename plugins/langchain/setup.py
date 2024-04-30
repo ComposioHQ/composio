c022990f13a3eb01cfa79e79ba289a6dce9690c4
sawradip <sawradip0@gmail.com>
2024-04-30 20:22:45 +0600

    Refactoring and Bug hunting
    demo notebooks
    
    Added julep and OpenAI extension
    
    Fixed Readme for julep & OpenAI
    
    Fixing issues with Entity and version

--- plugins/langchain/setup.py
+++ plugins/langchain/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="0.2.41",
+    version="0.2.46",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
@@ -27,7 +27,11 @@ setup(
         "langchain-openai>=0.0.2.post1",
         "pydantic>=2.6.4",
         "langchainhub>=0.1.15",
+<<<<<<< HEAD
         "composio_core===0.2.41",
+=======
+        "composio_core===0.2.46",
+>>>>>>> 1526b77 (Refactoring and Bug hunting)
     ],
     include_package_data=True,
 )
