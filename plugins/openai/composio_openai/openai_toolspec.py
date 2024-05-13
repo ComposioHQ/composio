3bcfba511876a7d317f659b2cdeaf1a965a19418
sawradip <sawradip0@gmail.com>
2024-05-13 15:21:49 +0530

    Added OpenAI assistant support

--- plugins/openai/composio_openai/openai_toolspec.py
+++ plugins/openai/composio_openai/openai_toolspec.py
@@ -3,8 +3,12 @@ import logging
 import os
 import json
 import types
+import time
 from inspect import Signature
 from typing import List, Union
+from openai import Client
+from openai.types.beta import thread
+from openai.types.beta.threads import run
 from openai.types.chat.chat_completion import ChatCompletion
 
 from composio.sdk import format_schema, SchemaFormat
@@ -14,10 +18,10 @@ logger = logging.getLogger(__name__)
 
 
 class OpenaiStyleToolsetBase:
-    def __init__(self, framework = None, entity_id: str = "default", schema_format = SchemaFormat.OPENAI):
+    def __init__(self, framework, entity_id: str = "default", schema_format = SchemaFormat.OPENAI):
         self.entity_id = entity_id
         self.client = ComposioCore(
-                            framework=framework, api_key=os.environ.get("COMPOSIO_API_KEY", None)
+                            framework=framework, api_key=os.environ.get("COMPOSIO_API_KEY")
                             )
         self.schema_format = schema_format
 
@@ -34,6 +38,7 @@ class OpenaiStyleToolsetBase:
     def get_tools(self, 
         tools: Union[App, List[App]],
         tags: List[Union[str, Tag]] = None):
+
         if isinstance(tools, App):
             tools = [tools]
         
@@ -43,12 +48,20 @@ class OpenaiStyleToolsetBase:
                                            format=self.schema_format) for action_schema in action_schemas]
         return formatted_schemas
     
-
-        
         
 class ComposioToolset(OpenaiStyleToolsetBase):
     def __init__(self, *args, framework=FrameworkEnum.OPENAI, **kwargs):
         super().__init__(*args, framework=framework, **kwargs)
+
+    def execute_tool_call(self, tool_call):
+        action_name_to_execute = tool_call.function.name
+        action = self.client.sdk.get_action_enum_without_tool(
+            action_name=action_name_to_execute
+        )
+        arguments = json.loads(tool_call.function.arguments)
+        account = entity.get_connection(app_name=action.service)
+        tool_response = account.execute_action(action, arguments)
+        return tool_response
 
     def handle_tool_calls(self,
                           llm_response: ChatCompletion, 
@@ -60,18 +73,53 @@ class ComposioToolset(OpenaiStyleToolsetBase):
                 for choice in llm_response.choices:
                     if choice.message.tool_calls:
                         for tool_call in choice.message.tool_calls:
-                            action_name_to_execute = tool_call.function.name
-                            action = self.client.sdk.get_action_enum_without_tool(
-                                action_name=action_name_to_execute
-                            )
-                            arguments = json.loads(tool_call.function.arguments)
-                            account = entity.get_connection(app_name=action.service)
-                            output.append(account.execute_action(action, arguments))
+                            tool_response = self.execute_tool_call(tool_call)
+                            output.append(tool_response)
 
         except Exception as e:
             raise e from e
 
         return output
+    
+    def handle_assistant_tool_calls(self, run_object):
+        tool_calls = run_object["required_action"]['submit_tool_outputs']["tool_calls"]
+        tool_outputs = []
+        for tool_call in tool_calls:
+            # tool_call_id = tool_call.pop("id")
+            tool_response = self.execute_tool_call(tool_call)
+            tool_output = {
+                "tool_call_id": tool_call.id,
+                "output": json.dumps(tool_response),
+            }
+            tool_outputs.append(tool_output)
+        return tool_outputs
+
+    def wait_and_handle_assistant_tool_calls(
+        self,
+        client: Client,
+        run: run,
+        thread: thread,
+        verbose: bool = False,
+    ):
+        run_object = run
+        thread_object = thread
+        while run_object.status in ("queued", "in_progress", "requires_action"):
+            # Look here
+            if run_object.status == "requires_action":
+                # print(run_object)
+                # print("***&*&*&*&S")
+                run_object = client.beta.threads.runs.submit_tool_outputs(
+                    thread_id=thread_object.id,
+                    run_id=run_object.id,
+                    tool_outputs=self.handle_assistant_tool_calls(run_object)
+                )
+            else:
+                run_object = client.beta.threads.runs.retrieve(
+                    thread_id=thread_object.id,
+                    run_id=run_object.id,
+                )
+                time.sleep(0.5)
+        return run_object
 
 if __name__ == '__main__':
     from pprint import pprint
