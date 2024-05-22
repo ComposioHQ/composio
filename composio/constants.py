3d999dfafc753be0a5238c605e84c429a6b5c096
angrybayblade <vptl185@gmail.com>
2024-05-22 15:58:19 +0530

    feat: define global constants

---
+++ composio/constants.py
@@ -0,0 +1,18 @@
+"""
+Global constants for Composio SDK
+"""
+
+ENV_COMPOSIO_API_KEY = "COMPOSIO_API_KEY"
+"""
+Environment variable for Composio API key
+"""
+
+LOCAL_CACHE_DIRECTORY_NAME = ".composio"
+"""
+Local cache directory name for composio CLI
+"""
+
+USER_DATA_FILE_NAME = "user_data.json"
+"""
+Filename for storing user data.
+"""
