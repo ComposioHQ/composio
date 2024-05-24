9a81416c3494a1e2c6bb474df5519541bd77bc8e
angrybayblade <vptl185@gmail.com>
2024-05-24 16:24:43 +0530

    refactor: crewai plugin

--- plugins/crew_ai/crewai_demo.py
+++ plugins/crew_ai/crewai_demo.py
@@ -1,34 +1,44 @@
+"""
+CrewAI demo.
+"""
+
 import os
 
 import dotenv
-from composio_crewai import App, ComposioToolset
+from composio_crewai import App, ComposioToolSet
 from crewai import Agent, Task
 from langchain_openai import ChatOpenAI
 
 
-# Loading the variables from .env file
+# Load environment variables from .env
 dotenv.load_dotenv()
 
-llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
-
+# Initialize tools.
+openai_client = ChatOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+composio_toolset = ComposioToolSet()
 
 # Get All the tools
-tools = ComposioToolset(apps=[App.GITHUB])
+tools = composio_toolset.get_tools(apps=[App.GITHUB])
 
-
+# Define agent
 crewai_agent = Agent(
     role="Github Agent",
     goal="""You take action on Github using Github APIs""",
-    backstory="""You are AI agent that is responsible for taking actions on Github on users behalf. You need to take action on Github using Github APIs""",
+    backstory=(
+        "You are AI agent that is responsible for taking actions on Github "
+        "on users behalf. You need to take action on Github using Github APIs"
+    ),
     verbose=True,
     tools=tools,
-    llm=llm,
+    llm=openai_client,
 )
 
+# Define task
 task = Task(
     description="Star a repo SamparkAI/docs on GitHub",
     agent=crewai_agent,
     expected_output="if the star happened",
 )
 
+# Execute task
 task.execute()
