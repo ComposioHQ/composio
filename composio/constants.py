4aa49d4c1859146a3f3b726e0c3496b1d9ad7ad8
angrybayblade <vptl185@gmail.com>
2024-05-25 13:36:57 +0530

    feat: define API and web URL helpers

--- composio/constants.py
+++ composio/constants.py
@@ -21,3 +21,52 @@ DEFAULT_ENTITY_ID = "default"
 """
 Default entity ID value.
 """
+
+DEFAULT_BASE_URL = "https://backend.composio.dev/api"
+"""
+Base URL for composio API server.
+"""
+
+ENV_COMPOSIO_BASE_URL = "COMPOSIO_BASE_URL"
+"""
+Environment variable name for composio base URL
+"""
+
+BASE_URL_PROD = "https://backend.composio.dev/api"
+"""
+Base URL for production server.
+"""
+
+WEB_URL_PROD = "https://app.composio.dev"
+"""
+Web URL for production server.
+"""
+
+BASE_URL_STAGING = "https://hermes-development.up.railway.app/api"
+"""
+Base URL for staging server.
+"""
+
+WEB_URL_STAGING = "https://hermes-frontend-git-master-composio.vercel.app"
+"""
+Web URL for staging server.
+"""
+
+BASE_URL_LOCAL = "http://localhost:9900/api"
+"""
+Base URL for local server.
+"""
+
+WEB_URL_LOCAL = "http://localhost:3000"
+"""
+Web URL for local server.
+"""
+
+BASE_URL_TO_PROD_MAPPING = {
+    BASE_URL_PROD: WEB_URL_PROD,
+    BASE_URL_STAGING: WEB_URL_STAGING,
+    BASE_URL_LOCAL: WEB_URL_LOCAL,
+}
+"""
+Composio API server base url -> web url mappings.
+"""
---
+++ composio/utils/url.py
@@ -0,0 +1,30 @@
+"""
+URL utilities.
+"""
+
+import os
+
+from composio.constants import (
+    BASE_URL_TO_PROD_MAPPING,
+    DEFAULT_BASE_URL,
+    ENV_COMPOSIO_BASE_URL,
+)
+
+
+def get_web_url(path: str) -> str:
+    """Get URL for the Composio web app."""
+    base_url = os.environ.get(
+        ENV_COMPOSIO_BASE_URL,
+        DEFAULT_BASE_URL,
+    )
+    web_url = BASE_URL_TO_PROD_MAPPING.get(base_url)
+    if web_url is None:
+        raise ValueError(
+            f"Incorrect format for base_url: {base_url}. "
+            "Format should be on of follwing {"
+            "https://backend.composio.dev/api, "
+            "https://hermes-development.up.railway.app/api, "
+            "http://localhost:9900/api"
+            "}"
+        )
+    return f"{web_url}/{path}"
