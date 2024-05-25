92ac7c52165b6aa02c009a85a77f08fa7b8089a9
shubhra <shubhras01@gmail.com>
2024-05-26 00:56:28 +0530

    changes

--- composio/__init__.py
+++ composio/__init__.py
@@ -1,6 +1,8 @@
 from .sdk import Composio, Action, App, Tag
 from .sdk.core import ComposioCore, FrameworkEnum
 from .sdk.sdk import SchemaFormat
+from .sdk.local_tools.lib.tool import Tool
+from .sdk.local_tools.lib.action import Action
 
 
 __all__ = (
