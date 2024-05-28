2cb4d67b0b1d1fe7b5213c6d0827b8025576247f
angrybayblade <vptl185@gmail.com>
2024-05-28 19:03:11 +0530

    fix: composio connections get query

--- composio/cli/connections.py
+++ composio/cli/connections.py
@@ -11,20 +11,29 @@ from composio.cli.context import Context, pass_context
 from composio.exceptions import ComposioSDKError
 
 
-@click.group(name="connections")
-def _connections() -> None:
+@click.group(name="connections", invoke_without_command=True)
+@pass_context
+def _connections(context: Context) -> None:
     """Manage composio connections"""
+    if context.click_ctx.invoked_subcommand:
+        return
+
+    connections = context.client.connected_accounts.get()
+    for connection in connections:
+        print(connection)
 
 
 @_connections.command(name="get")
-@click.argument("name", type=str)
+@click.argument("id", type=str)
 @pass_context
-def _get(context: Context, name: str) -> None:
+def _get(context: Context, id: str) -> None:
     """Get connection information"""
     try:
-        connection = context.client.get_entity().get_connection(app=name)
-        context.console.print(f"[green]App:[/green] {name}")
-        context.console.print(f"[green]Id:[/green] {connection.integrationId}")
+        connection = context.client.get_entity().get_connection(
+            connected_account_id=id,
+        )
+        context.console.print(f"[green]App:[/green] {connection.appUniqueId}")
+        context.console.print(f"[green]Id:[/green] {connection.id}")
         context.console.print(f"[green]Status:[/green] {connection.status}")
     except ComposioSDKError as e:
         raise click.ClickException(message=e.message) from e
