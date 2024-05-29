fa6cfdb8deb41a5549bca1a1799db68a39a1ca8c
shubhra <shubhras01@gmail.com>
2024-05-29 21:28:30 +0530

    adidng main

--- examples/swe/try-swe.py
+++ examples/swe/try-swe.py
@@ -21,9 +21,6 @@ composio_toolset = ComposioToolSet()
 
 llm = ChatOpenAI(model="gpt-4-turbo")
 
-assert os.environ.get("GITHUB_ACCESS_TOKEN") is not None
-assert os.environ.get("HARD_CODED_REPO_NAME") is not None
-
 base_role = "You are the part of best programming system to ever exist. You think carefully and step by step."
 goal = "Help fix the given issue / bug in the code."
 
@@ -32,56 +29,58 @@ commandmanager_tools = composio_toolset.get_tools([App.CMDMANAGERTOOL])
 Historymanager_tools = composio_toolset.get_tools([App.HISTORYKEEPER])
 composio_toolset.get_actions(actions=[Action.WORKSPACESTATUS])
 
+if __name__ == "__main__":
+    assert os.environ.get("GITHUB_ACCESS_TOKEN") is not None
+    assert os.environ.get("HARD_CODED_REPO_NAME") is not None
+    # start agent and task
+    agent_1 = Agent(
+        role=base_role + " You manage workspaces.",
+        goal=goal,
+        backstory=task_data["backstory"],
+        verbose=True,
+        tools=workspace_tools,
+        llm=llm,
+        memory=True,
+        cachetools=False,
+    )
 
-# start agent and task
-agent_1 = Agent(
-    role=base_role + " You manage workspaces.",
-    goal=goal,
-    backstory=task_data["backstory"],
-    verbose=True,
-    tools=workspace_tools,
-    llm=llm,
-    memory=True,
-    cachetools=False,
-)
-
-# start agent and task
-agent_2 = Agent(
-    role=base_role + "You manage history and help fetch it to make sure right decisions are taken by looking at all the datapoints.",
-    goal=goal,
-    backstory=task_data["backstory"],
-    verbose=True,
-    tools=commandmanager_tools,
-    llm=llm,
-    memory=True,
-    cachetools=False,
-)
+    # start agent and task
+    agent_2 = Agent(
+        role=base_role + "You manage history and help fetch it to make sure right decisions are taken by looking at all the datapoints.",
+        goal=goal,
+        backstory=task_data["backstory"],
+        verbose=True,
+        tools=commandmanager_tools,
+        llm=llm,
+        memory=True,
+        cachetools=False,
+    )
 
 
-# start agent and task
-agent_3 = Agent(
-    role=base_role,
-    goal=goal,
-    backstory=task_data["backstory"],
-    verbose=True,
-    tools=Historymanager_tools,
-    llm=llm,
-    memory=True,
-    cachetools=False,
-)
+    # start agent and task
+    agent_3 = Agent(
+        role=base_role,
+        goal=goal,
+        backstory=task_data["backstory"],
+        verbose=True,
+        tools=Historymanager_tools,
+        llm=llm,
+        memory=True,
+        cachetools=False,
+    )
 
-task = Task(
-        description=task_data["issue_description"],
-        agent=agent_1,
-        expected_output="issue should not be reproduced")
+    task = Task(
+            description=task_data["issue_description"],
+            agent=agent_1,
+            expected_output="issue should not be reproduced")
 
-my_crew = Crew(
-    agents=[agent_1,agent_2,agent_3],
-    tasks=[task],
-    process=Process.sequential,
-    full_output=True,
-    verbose=True,
-)
+    my_crew = Crew(
+        agents=[agent_1,agent_2,agent_3],
+        tasks=[task],
+        process=Process.sequential,
+        full_output=True,
+        verbose=True,
+    )
 
-my_crew.kickoff()
-print(my_crew.usage_metrics)
+    my_crew.kickoff()
+    print(my_crew.usage_metrics)
