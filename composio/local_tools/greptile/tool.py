21ff796ec6b31cf1f1a0cce9ca16a2615cee5e0b
sohamganatra <sohamganatra1@gmail.com>
2024-05-31 19:12:39 +0530

    WIP

---
+++ composio/local_tools/greptile/tool.py
@@ -0,0 +1,14 @@
+from composio.local_tools.tool import Tool
+from .codequery import CodeQuery
+
+
+class Greptile(Tool):
+    """
+    Code understanding tool. Index Code and answer questions about it.
+    """
+
+    def actions(self) -> list:
+        return [CodeQuery]
+
+    def triggers(self) -> list:
+        return []
