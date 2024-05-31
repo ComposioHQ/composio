c289de53d5d9bdbdf8d39115780093c7e46af2a6
shubhra <shubhras01@gmail.com>
2024-05-31 11:07:32 +0530

    add requirements.txt

---
+++ examples/benchmark/prepare_data.py
@@ -0,0 +1,23 @@
+from datasets import load_dataset
+
+'''
+huggingface dataset download :
+- dataset link: https://huggingface.co/datasets/princeton-nlp/SWE-bench_Lite
+'''
+
+
+def filter_short_problem_statements(instance):
+    return len(instance["problem_statement"].split()) > 40
+
+
+def main():
+    # Load the SWE-bench dataset
+    dev_dataset = load_dataset("princeton-nlp/SWE-bench", split="dev")
+    test_dataset = load_dataset("princeton-nlp/SWE-bench", split="test")
+
+    # Display the first few entries
+    print(test_dataset[:5])
+
+
+if __name__ == "__main__":
+    main()
