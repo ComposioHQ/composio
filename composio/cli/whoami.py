4c7ee713606b570fef465b178c805c337f78bf99
Himanshu Dixit <hudixt@gmail.com>
2024-06-01 02:37:48 +0530

    feat: better description

--- composio/cli/whoami.py
+++ composio/cli/whoami.py
@@ -14,7 +14,7 @@ from composio.exceptions import ComposioSDKError
 @click.command(name="whoami")
 @pass_context
 def _whoami(context: Context) -> None:
-    """Manage composio whoami"""
+    """List your account information"""
     try:
         context.console.print(f"API Key: [green]{context.user_data.api_key}[/green]")
     except ComposioSDKError as e:
