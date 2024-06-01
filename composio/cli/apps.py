5c2d8ec42afca3d432af6c2937d5eb7a7f01ec27
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-06-01 07:57:25 +0530

    fix: use proper enum key standard in local tool enums

--- composio/cli/apps.py
+++ composio/cli/apps.py
@@ -252,7 +252,7 @@ def _get_action_enum(apps: t.List[AppModel], actions: t.List[ActionModel]) -> st
     local_tool_handler = LocalToolHandler()
     for tool in local_tool_handler.registered_tools:
         for action in tool.actions():
-            enum_name = f"{_get_enum_key(action().action_name)}"
+            enum_name = f"{_get_enum_key(action().get_tool_merged_action_name())}"
             enum_value = f'("{tool.tool_name}", "{tool.tool_name}_{action().action_name}", True, True)'
             action_enums += f"    {enum_name} = {enum_value}\n"
     return ACTION_ENUM_TEMPLATE.format(actions=action_enums)
