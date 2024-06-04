cd549711f3a6018bae303c2b2670c9f9a1426fa8
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-06-04 14:16:51 +0530

    feat: add did you mena in all the commands

--- composio/cli/connections.py
+++ composio/cli/connections.py
@@ -8,10 +8,20 @@ Usage:
 import click
 
 from composio.cli.context import Context, pass_context
+from composio.core.cls.did_you_mean import DYMGroup
 from composio.exceptions import ComposioSDKError
+from composio.cli.utils.helpfulcmd import HelpfulCmdBase
+
+class ConnectionsExamples(HelpfulCmdBase, DYMGroup):
+    examples = [
+        click.style("composio connections", fg='green') + click.style("             # List all connections\n", fg='black'),
+        click.style("composio connections get 123", fg='green') + click.style("     # Get details of connection with ID 123\n", fg='black'),
+        click.style("composio connections delete 456", fg='green') + click.style("  # Delete connection with ID 456\n", fg='black'),
+    ]
 
 
-@click.group(name="connections", invoke_without_command=True)
+@click.group(name="connections", invoke_without_command=True, cls=ConnectionsExamples)
+@click.help_option("--help", "-h", "-help")
 @pass_context
 def _connections(context: Context) -> None:
     """List composio connections for your account"""
@@ -23,8 +33,14 @@ def _connections(context: Context) -> None:
         print(connection)
 
 
-@_connections.command(name="get")
+class GetExamples(HelpfulCmdBase, click.Command):
+    examples = [
+        click.style("composio connections get 123", fg='green') + click.style("  # Get details of connection with ID 123\n", fg='black'),
+    ]
+
+@_connections.command(name="get", cls=GetExamples)
 @click.argument("id", type=str)
+@click.help_option("--help", "-h", "-help")
 @pass_context
 def _get(context: Context, id: str) -> None:
     """Get connection information"""
