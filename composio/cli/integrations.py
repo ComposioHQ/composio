91e98dc5b4ae4f54f5f1dddee85480f054f1a5f0
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-06-04 14:16:51 +0530

    feat: add did you mena in all the commands

--- composio/cli/integrations.py
+++ composio/cli/integrations.py
@@ -8,10 +8,11 @@ Usage:
 import click
 
 from composio.cli.context import Context, login_required, pass_context
+from composio.core.cls.did_you_mean import DYMGroup
 from composio.exceptions import ComposioSDKError
 
 
-@click.group(name="integrations", invoke_without_command=True)
+@click.group(name="integrations", invoke_without_command=True, cls=DYMGroup)
 @login_required
 @pass_context
 def _integrations(context: Context) -> None:
