d0d4e2f6b090c5a857b1c01c29c8920c2a4f9db2
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-06-06 22:22:00 +0530

    feat: add models

---
+++ src/models/activeTriggers.ts
@@ -0,0 +1,34 @@
+import { CancelablePromise, GetActiveTriggerData, GetActiveTriggerResponse, ListActiveTriggersData, ListActiveTriggersResponse, getActiveTrigger, listActiveTriggers } from "../client";
+import { Composio } from "../sdk";
+
+export class ActiveTriggers {
+    constructor(private client: Composio) {
+        this.client = client;
+    }
+
+    /**
+     * Retrieves details of a specific active trigger in the Composio platform by providing its trigger name.
+     * 
+     * The response includes the trigger's name, description, input parameters, expected response, associated app information, and enabled status.
+     * 
+     * @param {GetActiveTriggerData} data The data for the request.
+     * @returns {CancelablePromise<GetActiveTriggerResponse>} A promise that resolves to the details of the active trigger.
+     * @throws {ApiError} If the request fails.
+     */
+    get(data: GetActiveTriggerData): CancelablePromise<GetActiveTriggerResponse> {
+        return getActiveTrigger(data);
+    }
+
+    /**
+     * Retrieves a list of all active triggers in the Composio platform.
+     * 
+     * This method allows you to fetch a list of all the available active triggers. It supports pagination to handle large numbers of triggers. The response includes an array of trigger objects, each containing information such as the trigger's name, description, input parameters, expected response, associated app information, and enabled status.
+     * 
+     * @param {ListActiveTriggersData} data The data for the request.
+     * @returns {CancelablePromise<ListActiveTriggersResponse>} A promise that resolves to the list of all active triggers.
+     * @throws {ApiError} If the request fails.
+     */
+    list(data: ListActiveTriggersData = {}): CancelablePromise<ListActiveTriggersResponse> {
+        return listActiveTriggers(data);
+    }
+}
