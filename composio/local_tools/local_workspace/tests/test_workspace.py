d2cbc8282db5185106ef1bab490b2a93430c17ad
shubhra <shubhras01@gmail.com>
2024-06-06 16:20:40 +0530

    black and flak8 fixes

--- composio/local_tools/local_workspace/tests/test_workspace.py
+++ composio/local_tools/local_workspace/tests/test_workspace.py
@@ -1,7 +1,15 @@
 import unittest
-from composio.local_tools.local_workspace.commons.local_docker_workspace import WorkspaceManagerFactory
-from composio.local_tools.local_workspace.commons.history_processor import HistoryProcessor
-from composio.local_tools.local_workspace.workspace.actions.create_workspace import CreateWorkspaceAction, CreateWorkspaceRequest
+
+from composio.local_tools.local_workspace.commons.history_processor import (
+    HistoryProcessor,
+)
+from composio.local_tools.local_workspace.commons.local_docker_workspace import (
+    WorkspaceManagerFactory,
+)
+from composio.local_tools.local_workspace.workspace.actions.create_workspace import (
+    CreateWorkspaceAction,
+    CreateWorkspaceRequest,
+)
 
 
 class TestCreateWorkspaceAction(unittest.TestCase):
@@ -12,12 +20,14 @@ class TestCreateWorkspaceAction(unittest.TestCase):
         action = CreateWorkspaceAction()
         action.set_workspace_and_history(w, h)
 
-
         # Execute the action
-        result = action.execute(CreateWorkspaceRequest(image_name="sweagent/swe-agent:latest"), {})
+        result = action.execute(
+            CreateWorkspaceRequest(image_name="sweagent/swe-agent:latest"), {}
+        )
 
         # Verify - Check if the workspace was created successfully
         self.assertIsNotNone(result.workspace_id)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
