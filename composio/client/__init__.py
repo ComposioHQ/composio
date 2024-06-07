c117ba44517a5497b747c6bcbae5cda38a915d7a
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-06-08 00:04:34 +0530

    feat: add integrations, connectedAccount classes

--- composio/client/__init__.py
+++ composio/client/__init__.py
@@ -789,6 +789,8 @@ class IntegrationModel(BaseModel):
     appName: str
     logo: str
 
+    expectedInputFields: list = []
+
     defaultConnectorId: t.Optional[str] = None
     connections: t.Optional[t.List[t.Dict]] = None
 
@@ -922,6 +924,134 @@ class Composio:
         """
         return Entity(id=id, client=self)
 
+class Integration(IntegrationModel):
+    sdk_instance: "Composio" = None  # type: ignore
+
+    def __init__(self, sdk_instance: "Composio", **data):
+        super().__init__(**data)
+        self.sdk_instance = sdk_instance
+
+    def initiate_connection(
+        self,
+        entity_id: str = None,
+        params: t.Optional[dict] = None,
+        redirect_url: str = None,
+    ) -> "ConnectionRequest":
+        resp = self.sdk_instance.http_client.post(
+            f"v1/connectedAccounts",
+            json={
+                "integrationId": self.id,
+                "userUuid": entity_id,
+                "data": params or {},
+                "redirectUri": redirect_url,
+            },
+        )
+        return ConnectionRequest(self.sdk_instance, **resp.json())
+
+    def get_required_variables(self):
+        return self.expectedInputFields
+
+class ConnectionRequest(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    connectionStatus: str
+    connectedAccountId: str
+    redirectUrl: t.Optional[str] = None
+
+    sdk_instance: "Composio" = None  # type: ignore
+
+    def __init__(self, sdk_instance: "Composio", **data):
+        super().__init__(**data)
+        self.sdk_instance = sdk_instance
+
+    def save_user_access_data(
+        self, field_inputs: dict, redirect_url: str = None, entity_id: str = None
+    ):
+        connected_account_id = self.sdk_instance.get_connected_account(
+            self.connectedAccountId
+        )
+        resp = self.sdk_instance.http_client.post(
+            f"v1/connectedAccounts",
+            json={
+                "integrationId": connected_account_id.integrationId,
+                "data": field_inputs,
+                "redirectUri": redirect_url,
+                "userUuid": entity_id,
+            },
+        )
+        return resp.json()
+
+    def wait_until_active(
+        self, timeout=60
+    ) -> "ConnectedAccount":  # Timeout adjusted to seconds
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            connection_info = self.sdk_instance.get_connected_account(
+                self.connectedAccountId
+            )
+            if connection_info.status == "ACTIVE":
+                return connection_info
+
+            time.sleep(1)
+
+        raise TimeoutException(
+            "Connection did not become active within the timeout period."
+        ) 
+
+class ConnectedAccount(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    integrationId: str
+    connectionParams: AuthConnectionParams
+    appUniqueId: str
+    id: str
+    status: str
+    createdAt: str
+    updatedAt: str
+
+    sdk_instance: "Composio" = None
+
+    def __init__(self, sdk_instance: "Composio", **data):
+        super().__init__(**data)
+        # self.connectionParams = OAuth2ConnectionParams(**self.connectionParams)
+        self.sdk_instance = sdk_instance
+
+    def _execute_action(
+        self, action_name: Action, connected_account_id: str, params: dict
+    ):
+        resp = self.sdk_instance.http_client.post(
+            f"v1/actions/{action_name.value[1]}/execute",
+            json={"connectedAccountId": connected_account_id, "input": params},
+        )
+        return resp.json()
+
+    def execute_action(self, action_name: Action, params: dict):
+        resp = self._execute_action(action_name, self.id, params)
+        return resp
+
+    def get_all_actions(self, format: SchemaFormat = SchemaFormat.OPENAI, tags: list[Union[str, Tag]] = None):
+        app_unique_id = self.appUniqueId
+        resp = self.sdk_instance.http_client.get(
+            f"v1/actions?appNames={app_unique_id}"
+        )
+        actions = resp.json()
+        return [format_schema(action_schema, format = format) for action_schema in actions["items"]]
+
+    def handle_tools_calls(self, tool_calls: ChatCompletion) -> list[any]:
+        output = []
+        try:
+            if tool_calls.choices:
+                for choice in tool_calls.choices:
+                    if choice.message.tool_calls:
+                        for tool_call in choice.message.tool_calls:
+                            function = tool_call.function
+                            action = self.sdk_instance.get_action_enum(
+                                function.name, self.appUniqueId
+                            )
+                            arguments = json.loads(function.arguments)
+                            output.append(self.execute_action(action, arguments))
+        except Exception as e:
+            raise e from e
+
+        return output
 
 class Entity:
     """Class to represent Entity object."""
