0c8b392201014c360256f33f102c245ea88951f9
Himanshu Dixit <hudixt@gmail.com>
2024-06-08 19:35:48 +0530

    fix: type issue

--- composio/cli/utils/helpfulcmd.py
+++ composio/cli/utils/helpfulcmd.py
@@ -15,10 +15,6 @@ class HelpfulCmdBase:
     examples: list[str] = []
     help: t.Optional[str] = None
 
-    def get_params(self, ctx: ClickContext) -> t.List[click.Parameter]:
-        """Retrieve the list of parameters for the command."""
-        return ctx.command.get_params(ctx)
-
     def format_help_text(self, ctx: ClickContext, formatter: HelpFormatter) -> None:
         """Writes the help text to the formatter if it exists."""
         if self.help is not None:
@@ -38,7 +34,7 @@ class HelpfulCmdBase:
     def format_options(self, ctx: ClickContext, formatter: HelpFormatter) -> None:
         """Writes all the options into the formatter if they exist."""
         opts = []
-        for param in self.get_params(ctx):
+        for param in self.get_params(ctx): # type: ignore
             rv = param.get_help_record(ctx)
             if rv is not None:
                 if "-h" in rv[0] or "-help" in rv[0] or "--help" in rv[0]:
