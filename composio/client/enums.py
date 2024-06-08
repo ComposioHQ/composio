fecbb2795c9495bb803cfecec54fbd72d851cf2d
shubhra <shubhras01@gmail.com>
2024-06-08 22:41:58 +0530

    fix

--- composio/client/enums.py
+++ composio/client/enums.py
@@ -10,13 +10,13 @@ from enum import Enum
 class Tag(tuple, Enum):
     """App tags."""
 
-    # pylint: disable=function-redefined
+    # pylint: disable=function-redefined,invalid-overridden-method
     @property
     def name(self) -> str:
         """Returns trigger name."""
         return self.value[0]
 
-    # pylint: enable=function-redefined
+    # pylint: enable=function-redefined,invalid-overridden-method
 
     IMPORTANT = ("default", "important")
     ASANA_ALLOCATIONS = ("asana", "Allocations")
