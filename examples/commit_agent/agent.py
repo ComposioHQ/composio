83f636b1c3f035addc6106702d1a4b1795727568
Sai Yashwanth <taddishetty34@gmail.com>
2024-06-11 00:25:02 +0530

    Changed to comments in code

--- examples/commit_agent/agent.py
+++ examples/commit_agent/agent.py
@@ -19,10 +19,11 @@ model = os.getenv("MODEL")
 llm = ChatOpenAI(model=model, api_key=openai_api_key)
 
 # Define tools for the agents
+# We are using Trello tool from composio to connect to our trello account.
 composio_toolset = ComposioToolSet()
 tools = composio_toolset.get_tools(apps=[App.TRELLO])
 
-# Define the commit agent
+# Define the commit agent. This agent will read the github patch and add appropriate cards to trello list.
 commit_agent = Agent(
     role="Github-Trello TODO Agent",
     goal="""Take 'trello_create_trello_card' action on TRELLO via TRELLO APIs based on the Github patch.""",
@@ -38,19 +39,18 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    t_id = "your_trello_list_id_for_todos"  # Replace with your Trello list ID for TODOs
-    d_id = "your_trello_list_id_for_done"  # Replace with your Trello list ID for done tasks
-
+    # Task to add a card to `todo` list based on the commit patch
     task1 = Task(
-        description=f"""Given the following Github patch: {request.json}, create a TRELLO card (trello_create_trello_card) for the TODOs from code comments in the patch. TRELLO list (id:{t_id}).
+        description=f"""Given the following Github patch: {request.json}, create a TRELLO card (trello_create_trello_card) for the TODOs from code comments in the patch. TRELLO list (id:{trello_todo_list_id}).
         Please read the patch carefully and create cards for the new TODOs only, avoid removed/old TODOs. Card name should reflect the todo comment present in code""",
         expected_output="A TRELLO card created for the commit",
         agent=commit_agent,
     )
     task1.execute()
 
+    # Task to add a card to `done` list based on the commit patch
     task2 = Task(
-        description=f"""Given the following Github patch: {request.json}, create a TRELLO card (trello_create_trello_card) for the Commit Message in the patch. TRELLO list (id:{d_id})
+        description=f"""Given the following Github patch: {request.json}, create a TRELLO card (trello_create_trello_card) for the Commit Message in the patch. TRELLO list (id:{trello_done_list_id})
         to add card. Create only if the commit message indicates that a task is completed.""",
         expected_output="A TRELLO card created for the commit",
         agent=commit_agent,
