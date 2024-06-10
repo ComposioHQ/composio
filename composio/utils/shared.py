49424bb5cd26d33e6610ae9cf2ca0c0c7e2ee3c1
sawradip <sawradip0@gmail.com>
2024-06-10 14:04:21 +0600

    Both langchain & llamaindex working

--- composio/utils/shared.py
+++ composio/utils/shared.py
@@ -257,19 +257,19 @@ def get_signature_format_from_schema_params(schema_params: t.Dict) -> t.List[Par
             elif len(param_types) == 2:
                 # Check as redefinition and union was incompatible
                 # @karan to check if this is the right way to do it
-                t1: t.Type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_types[0]]  # type: ignore
-                t2: t.Type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_types[1]]  # type: ignore
+                t1: t.Type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_types[0]]  # type: ignore
+                t2: t.Type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_types[1]]  # type: ignore
                 signature_param_type: t.Type = t.Union[t1, t2]  # type: ignore
             elif len(param_types) == 3:
-                t1: t.Type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_types[0]]  # type: ignore
-                t2: t.Type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_types[1]]  # type: ignore
-                t3: t.Type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_types[2]]
+                t1: t.Type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_types[0]]  # type: ignore
+                t2: t.Type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_types[1]]  # type: ignore
+                t3: t.Type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_types[2]]
                 signature_param_type: t.Type = t.Union[t1, t2, t3]  # type: ignore
             else:
                 raise ValueError("Invalid 'oneOf' schema")
             param_default = param_schema.get("default", "")
-        elif param_type in SCHEMA_TYPE_TO_PYTHON_TYPE:
-            signature_param_type = SCHEMA_TYPE_TO_PYTHON_TYPE[param_type]
+        elif param_type in PYDANTIC_TYPE_TO_PYTHON_TYPE:
+            signature_param_type = PYDANTIC_TYPE_TO_PYTHON_TYPE[param_type]
             param_default = param_schema.get("default", FALLBACK_VALUES[param_type])
         else:
             signature_param_type = pydantic_model_from_param_schema(param_schema)
