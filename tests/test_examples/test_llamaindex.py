34ab9cd06b5c8ed927c5d838334516949261f406
sawradip <sawradip0@gmail.com>
2024-06-12 18:08:02 +0600

    Solved Linting issues

--- tests/test_examples/test_llamaindex.py
+++ tests/test_examples/test_llamaindex.py
@@ -1,8 +1,5 @@
-import json
-import sys
-import unittest.mock as mock
-import click
 import os
+import sys
 import pytest
 
 from composio.exceptions import ApiKeyNotProvidedError
@@ -10,12 +7,14 @@ from composio.exceptions import ApiKeyNotProvidedError
 
 def run_llamaindex_script():
     from plugins.llamaindex.llamaindex_demo import main
+
     main()
 
 
 @pytest.fixture(scope="session", autouse=True)
 def pytest_sessionstart_llamaindex():
     from composio.cli import composio as composio_cli
+
     """
     Called after the Session object has been created and
     before performing collection and entering the run test loop.
@@ -27,8 +26,8 @@ def pytest_sessionstart_llamaindex():
     ]
     try:
         # INSERT_YOUR_CODE
-        if 'COMPOSIO_API_KEY' in os.environ:
-            os.environ.pop('COMPOSIO_API_KEY', None)
+        if "COMPOSIO_API_KEY" in os.environ:
+            os.environ.pop("COMPOSIO_API_KEY", None)
         composio_cli()
     except SystemExit as e:
         print(f"SystemExit ignored: {e}")
@@ -37,14 +36,15 @@ def pytest_sessionstart_llamaindex():
     finally:
         sys.argv = original_argv  # Restore original arguments
 
+
 def test_llamaindex_script_not_authorized_error():
     with pytest.raises(ApiKeyNotProvidedError) as exc_info:
         run_llamaindex_script()
-    assert "API Key not provided" in str(
-        exc_info.value
-    )
+    assert "API Key not provided" in str(exc_info.value)
+
 
 def test_llamaindex_script_is_working():
     import os
-    os.environ['COMPOSIO_API_KEY'] = 'kwrjjvgedmuw5jt1fet2'
+
+    os.environ["COMPOSIO_API_KEY"] = "kwrjjvgedmuw5jt1fet2"
     run_llamaindex_script()
