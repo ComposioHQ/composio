dacf36ffe3c5d3d2514db1a6efd50e1c97cb52b0
angrybayblade <vptl185@gmail.com>
2024-06-13 16:44:27 +0530

    test: `composio actions` command group

--- tests/test_cli/test_actions.py
+++ tests/test_cli/test_actions.py
@@ -2,35 +2,63 @@
 Test actions command group.
 """
 
-import logging
 import typing as t
 from unittest import mock
+
+import pytest
 
 from tests.test_cli.base import BaseCliTest
 
 
-logging.basicConfig(level=logging.DEBUG)
-
-
-@mock.patch("click.prompt", return_value="n")
-class TestListActions(BaseCliTest):
+class TestListActions(BaseCliTest):  # pylint: disable=unused-argument
     """Test list actions."""
 
-    def test_list_all(self, *args: t.Any) -> None:  # pylint: disable=unused-argument
-        """Test list all actions."""
-        result = self.run("actions")
-
-        assert result.exit_code == 0
-        assert "strava_" in result.stdout
-        assert "github_" in result.stdout
-
-    def test_list_enabled(  # pylint: disable=unused-argument
+    @pytest.mark.parametrize(
+        argnames="arguments,exptected_outputs,unexptected_outputs",
+        argvalues=(
+            (tuple(), ("strava_", "github_"), tuple()),  # List all apps
+            (("--enabled",), ("github_",), ("strava_",)),  # List enabled apps
+            (
+                ("--app", "slack"),
+                ("slack_",),
+                ("strava_", "github_"),
+            ),  # Filter by a specific app
+        ),
+    )
+    @mock.patch("click.prompt", return_value="n")
+    def test_list_all(
         self,
-        *args: t.Any,
+        patch: t.Any,
+        arguments: t.Tuple[str, ...],
+        exptected_outputs: t.Tuple[str, ...],
+        unexptected_outputs: t.Tuple[str, ...],
     ) -> None:
-        """Test list enabled apps."""
-        result = self.run("actions", "--enabled")
+        """Test list all actions."""
+        result = self.run("actions", *arguments)
 
         assert result.exit_code == 0
-        assert "strava_" not in result.stdout
-        assert "github_" in result.stdout
+        for output in exptected_outputs:
+            assert output in result.stdout
+
+        for output in unexptected_outputs:
+            assert output not in result.stdout
+
+    @pytest.mark.skip(reason="Limit filter is not working atm!")
+    @mock.patch("click.prompt", return_value="n")
+    def test_limit(self, patch: t.Any) -> None:
+        """Test limit flag."""
+        result = self.run("actions", "--use-case", "github", "--limit", "5")
+
+        assert result.exit_code == 0
+        assert len(result.output.splitlines()) == 6
+
+    def test_copy(self) -> None:
+        """Test copy flag."""
+
+        def _assert_copy(text: str) -> None:
+            """Assert copy function call."""
+            assert "Action.GITHUB" in text
+            assert "Action.SLACK" not in text
+
+        with mock.patch("pyperclip.copy", new=_assert_copy):
+            self.run("actions", "--app", "--github", "--copy")
