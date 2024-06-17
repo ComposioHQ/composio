fd9bd8130e46352ca517c8fbb8e38208ec4acfec
shubhra <shubhras01@gmail.com>
2024-06-17 17:22:41 +0530

    format changes

--- coders/setup.py
+++ coders/setup.py
@@ -4,7 +4,7 @@ Setup configuration for Composio SWE Agent plugin
 
 from pathlib import Path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 setup(
@@ -24,16 +24,16 @@ setup(
     python_requires=">=3.9,<4",
     packages=find_packages(include=["composio_coders"]),
     entry_points={
-        'console_scripts': [
-             'composio-coder = composio_coders.cli:cli',
-         ],
+        "console_scripts": [
+            "composio-coder = composio_coders.cli:cli",
+        ],
     },
     install_requires=[
         "composio_core===0.3.9",
         "gitpython>=3.1.43",
         "composio_crewai>=0.3.9",
         "crewai>=0.30.11",
-        "datetime"
+        "datetime",
     ],
     include_package_data=True,
 )
