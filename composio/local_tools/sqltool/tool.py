e7d7a1f454297c20296dd6968e390b7f83761b2c
Prathit-tech <ppjoshi2100@gmail.com>
2024-06-17 19:44:36 +0530

    changes were made in sqltool

--- composio/local_tools/sqltool/tool.py
+++ composio/local_tools/sqltool/tool.py
@@ -2,13 +2,14 @@ from composio.core.local import Action, Tool
 
 from .actions.sql_query import SqlQuery
 
+from typing import Type
 
 class SqlTool(Tool):
     """
     This class enables us to execute sql queries in a database
     """
 
-    def actions(self) -> list[Action]:
+    def actions(self) -> list[Type[Action]]:
         return [SqlQuery]
 
     def triggers(self) -> list:
