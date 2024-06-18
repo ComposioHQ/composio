c939860d42a69afcd05365df09cc6cefb492b0e9
Karan Vaidya <kaavee315@gmail.com>
2024-06-18 14:11:49 +0530

    Update

--- composio/client/collections.py
+++ composio/client/collections.py
@@ -383,6 +383,18 @@ class TriggerModel(BaseModel):
 
     logo: t.Optional[str] = None
 
+class ExecutionDetailsModel(BaseModel):
+    """Execution details data model."""
+    executed: bool
+    response_data: t.Dict
+
+class SuccessExecuteActionResponseModel(BaseModel):
+    """Success execute action response data model."""
+    execution_details: ExecutionDetailsModel
+
+class FileModel(BaseModel):
+    name: str 
+    content: bytes
 
 class Triggers(Collection[TriggerModel]):
     """Collection of triggers."""
