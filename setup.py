99cbdfd0056dfddee76cea0e03d708f6cea3f5ee
angrybayblade <vptl185@gmail.com>
2024-06-19 17:42:58 +0530

    chore: define `pysher` as dependency

--- setup.py
+++ setup.py
@@ -47,13 +47,14 @@ setup(
         "docker>=7.1.0",
         "gymnasium>=0.29.1",
         "pyyaml>=6.0.1",
-        "sentry-sdk>=2.0.0"
+        "sentry-sdk>=2.0.0",
+        "pysher==1.0.8",
     ],
     include_package_data=True,
     package_data={
         "composio": [
             "local_tools/local_workspace/config/*.yaml",
-            "local_tools/local_workspace/config/commands/*.sh"
+            "local_tools/local_workspace/config/commands/*.sh",
         ]
-    }
+    },
 )
