73d62fb97f244027d9d5f9cbebe6f3e22952c9ea
shubhra <shubhras01@gmail.com>
2024-06-19 09:43:07 +0530

    wip - workspace without agent

--- coders/composio_coders/swe.py
+++ coders/composio_coders/swe.py
@@ -1,9 +1,13 @@
 import datetime
 import json
 import logging
+import os
 from pathlib import Path
 from pydantic import BaseModel, Field
-from composio_crewai import ComposioToolSet, App
+from composio_crewai import ComposioToolSet, App, Action
+from composio.local_tools.local_workspace.workspace.actions.create_workspace import CreateWorkspaceResponse
+from composio.local_tools.local_workspace.cmd_manager.actions.clone_github import GithubCloneRequest, GithubCloneResponse
+from composio import Composio
 from crewai import Agent, Task, Crew
 from typing import Any, Dict, List
 
@@ -26,26 +30,25 @@ from rich.logging import RichHandler
 AGENT_BACKSTORY_TMPL = """
 You are an autonomous programmer, your task is to solve the issue given in task with the tools in hand.
   Your mentor gave you following tips.
-  1. Always start by initializing the workspace.
-  2. Use the workspace_id returned to use tools to run commands. The commands are run on shell.
-  3. Use clone action to clone the git repo {repo_name} from the base_commit {base_commit} in workspace.
-  4. PLEASE READ THE CODE AND UNDERSTAND THE FILE STRUCTURE OF THE CODEBASE USING GIT REPO TREE ACTION.
-  5. POST THAT READ ALL THE RELEVANT READMEs AND TRY TO LOOK AT THE FILES RELATED TO THE ISSUE.
-  6. Form a thesis around the issue and the codebase.
-  7. THEN TRY TO REPLICATE THE BUG THAT THE ISSUES DISCUSSES.
+  1. A workspace is initialized for you, and you will be working on {workspace_id}. The git repo is cloned in 
+  the path {repo_name}, you need to work in this directory.
+  2. PLEASE READ THE CODE AND UNDERSTAND THE FILE STRUCTURE OF THE CODEBASE USING GIT REPO TREE ACTION.
+  3. POST THAT READ ALL THE RELEVANT READMEs AND TRY TO LOOK AT THE FILES RELATED TO THE ISSUE.
+  4. Form a thesis around the issue and the codebase.
+  5. THEN TRY TO REPLICATE THE BUG THAT THE ISSUES DISCUSSES.
      If the issue includes code for reproducing the bug, we recommend that you re-implement that in your environment, and run it to make sure you can reproduce the bug.
      Then start trying to fix it.
      When you think you've fixed the bug, re-run the bug reproduction script to make sure that the bug has indeed been fixed.
      If the bug reproduction script does not print anything when it successfully runs, 
      we recommend adding a print("Script completed successfully, no errors.") command at the end of the file,
      so that you can be sure that the script indeed ran fine all the way through.
-  8. If you run a command and it doesn't work, try running a different command. A command that did not work once will not work the second time unless you modify it!
-  9. If you open a file and need to get to an area around a specific line that is not in the first 100 lines, say line 583, don't just use the scroll_down command multiple times. Instead, use the goto 583 command. It's much quicker.
-  10. If the bug reproduction script requires inputting/reading a specific file, such as buggy-input.png, and you'd like to understand how to input that file, conduct a search in the existing repo code, to see whether someone else has already done that. Do this by running the command: find_file "buggy-input.png" If that doesn't work, use the linux 'find' command.
-  11. Always make sure to look at the currently open file and the current working directory (which appears right after the currently open file). The currently open file might be in a different directory than the working directory! Note that some commands, such as 'create', open files, so they might change the current  open file.
-  12. When editing files, it is easy to accidentally specify a wrong line number or to write code with incorrect indentation. Always check the code after you issue an edit to make sure that it reflects what you wanted to accomplish. If it didn't, issue another command to fix it.
-  13. When you finish working on the issue, use submit patch tool to submit your patch.
-  14. SUBMIT THE PATCH TO THE REVIEWER AGENT AGAIN AND ASK THEM TO REVIEW THE PATCH AND SUBMIT IT ONLY IF THEY APPROVE IT.
+  6. If you run a command and it doesn't work, try running a different command. A command that did not work once will not work the second time unless you modify it!
+  7. If you open a file and need to get to an area around a specific line that is not in the first 100 lines, say line 583, don't just use the scroll_down command multiple times. Instead, use the goto 583 command. It's much quicker.
+  8. If the bug reproduction script requires inputting/reading a specific file, such as buggy-input.png, and you'd like to understand how to input that file, conduct a search in the existing repo code, to see whether someone else has already done that. Do this by running the command: find_file "buggy-input.png" If that doesn't work, use the linux 'find' command.
+  9. Always make sure to look at the currently open file and the current working directory (which appears right after the currently open file). The currently open file might be in a different directory than the working directory! Note that some commands, such as 'create', open files, so they might change the current  open file.
+  10. When editing files, it is easy to accidentally specify a wrong line number or to write code with incorrect indentation. Always check the code after you issue an edit to make sure that it reflects what you wanted to accomplish. If it didn't, issue another command to fix it.
+  11. When you finish working on the issue, use submit patch tool to submit your patch.
+  12. SUBMIT THE PATCH TO THE REVIEWER AGENT AGAIN AND ASK THEM TO REVIEW THE PATCH AND SUBMIT IT ONLY IF THEY APPROVE IT.
 """
 ISSUE_DESC_TMPL = """
  We're currently solving the following issue within our repository. Here's the issue text:
@@ -109,7 +112,8 @@ class CoderAgent:
         self.composio_toolset = tool_set.get_tools(apps=[App.LOCALWORKSPACE,
                                                          App.CMDMANAGERTOOL,
                                                          App.HISTORYKEEPER,])
-        print("composio_toolset: ", self.composio_toolset)
+        composio_client = Composio()
+        self.entity = composio_client.get_entity("swe-agent")
 
         # initialize agent-related different prompts
         self.agent_role = "You are the best programmer. You think carefully and step by step take action."
@@ -169,6 +173,8 @@ class CoderAgent:
         if model_env == MODEL_ENV_AZURE:
             azure_endpoint = self.model_env.get(KEY_AZURE_ENDPOINT)
             azure_key = self.model_env.get(KEY_API_KEY)
+            os.environ["AZURE_OPENAI_API_KEY"] = self.model_env[KEY_AZURE_ENDPOINT]
+            os.environ["AZURE_OPENAI_API_KEY"] = self.model_env[KEY_API_KEY]
             azure_llm = AzureChatOpenAI(
                 azure_endpoint=azure_endpoint,
                 api_key=azure_key,
@@ -182,21 +188,21 @@ class CoderAgent:
     def run(self):
         llm = self.get_llm()
 
-        self.logger.info(
-            "starting agent for issue-id: %s\n"
-            "issue-description: %s\n"
-            "repo_name: %s\n",
-            self.issue_config.issue_id,
-            self.issue_config.issue_desc,
-            self.issue_config.issue_desc,
-        )
-
+        workspace_create_resp: CreateWorkspaceResponse = self.entity.execute(Action.LOCALWORKSPACE_CREATEWORKSPACEACTION, {})
+        workspace_id = workspace_create_resp.workspace_id
+        logger.info(f"workspace is created, workspace-id is: {workspace_id}")
+        git_clone_response = self.entity.execute(Action.CMDMANAGERTOOL_GITHUBCLONECMD,
+                                                 params={"workspace_id": workspace_id, "repo_name": self.issue_config.repo_name})
         issue_added_instruction = self.issue_description_tmpl.format(
             issue=self.issue_config.issue_desc, issue_id=self.issue_config.issue_id
         )
         backstory_added_instruction = self.agent_backstory_tmpl.format(
-            repo_name=self.repo_name, base_commit=self.issue_config.base_commit_id
+            workspace_id=workspace_id,
+            repo_name=self.repo_name,
+            base_commit=self.issue_config.base_commit_id
         )
+        logger.info(f"git clone response: {git_clone_response}")
+
         swe_agent = Agent(
             role=self.agent_role,
             goal=self.agent_goal,
