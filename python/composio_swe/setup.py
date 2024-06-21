27c1c348f186f36d2ca9f7b067d05c4f84268997
shubhra <shubhras01@gmail.com>
2024-06-21 17:45:36 +0530

    get scores

--- python/composio_swe/setup.py
+++ python/composio_swe/setup.py
@@ -32,7 +32,9 @@ setup(
         "composio_core>=0.3.9",
         "gitpython>=3.1.43",
         "composio_crewai>=0.3.9",
-        "crewai>=0.30.11",
+        "crewai==0.30.11",
+        "argsparse",
+        "datasets>=2.20.0",
     ],
     include_package_data=True,
 )
