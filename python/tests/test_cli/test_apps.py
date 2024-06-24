f97e7a8a836b8ec00e157af7444660c84fe39a97
angrybayblade <vptl185@gmail.com>
2024-06-24 12:25:06 +0530

    test: app updates

--- python/tests/test_cli/test_apps.py
+++ python/tests/test_cli/test_apps.py
@@ -2,12 +2,16 @@
 Test `composio apps` command group.
 """
 
+import random
 import typing as t
 from pathlib import Path
 
 from composio.client import enums
 
 from tests.test_cli.base import BaseCliTest
+
+
+ENUM_CLS = ("App", "Action", "Tag", "Trigger")
 
 
 class TestList(BaseCliTest):
@@ -39,8 +43,30 @@ class TestUpdate(BaseCliTest):
         for file, content in self.files.items():
             file.write_text(data=content, encoding="utf-8")
 
-    def test_update(self) -> None:
+    def test_update_not_required(self) -> None:
         """Test app enums update."""
         self.run("apps", "update")
         self.assert_exit_code(code=0)
-        self.assert_stdout("App database updated successfully")
+        for cls in ENUM_CLS:
+            self.assert_stdout(f"⚠️ {cls}s does not require update")
+
+    def test_update(self) -> None:
+        """Test app enums update."""
+        to_update = random.choice(ENUM_CLS)
+        file = Path(enums.__file__).parent / f"_{to_update.lower()}.py"
+        content = file.read_text(encoding="utf-8")
+        annotations = []
+        for line in content.splitlines():
+            if f': "{to_update}"' in line:
+                annotations.append(line)
+        to_remove = random.choice(annotations)
+        content = content.replace(to_remove, "")
+        file.write_text(content)
+
+        self.run("apps", "update")
+        self.assert_exit_code(code=0)
+        self.assert_stdout(f"{to_update}s updated")
+        for cls in ENUM_CLS:
+            if cls == to_update:
+                continue
+            self.assert_stdout(f"{cls}s does not require update")
