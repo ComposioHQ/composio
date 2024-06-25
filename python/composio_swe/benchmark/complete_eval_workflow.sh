55a46510c0f7d0212ef794fda88e91f98d2125a5
Karan Vaidya <kaavee315@gmail.com>
2024-06-25 17:28:14 +0530

    Update

--- python/composio_swe/benchmark/complete_eval_workflow.sh
+++ python/composio_swe/benchmark/complete_eval_workflow.sh
@@ -38,7 +38,7 @@ run_evaluation() {
     cd ~/codes/SWE-bench-docker/
     #
     ## Run the evaluation
-    python run_evaluation.py --predictions_path "{$predictions_json_path}" --log_dir "{$log_dir_path}" --swe_bench_tasks "{$dataset_on_disk_path}" --namespace aorwall
+    python run_evaluation.py --predictions_path "$predictions_json_path" --log_dir "$log_dir_path" --swe_bench_tasks "$dataset_on_disk_path" --namespace aorwall
     #popd
 }
 
