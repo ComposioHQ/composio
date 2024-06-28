f85eeebcadebe7f2bf65ccfd4f5081e0cf15c713
Karan Vaidya <kaavee315@gmail.com>
2024-06-28 15:36:17 +0530

    Fix swe run scripts

--- python/swe/composio_swe/agent/swe_run.py
+++ python/swe/composio_swe/agent/swe_run.py
@@ -1,15 +1,18 @@
 from composio_swe.agent.base_swe_agent import SWEArgs
-from composio_swe.agent.crewai import CrewaiAgent
+from composio_swe.agent.crewai_agent import CrewaiAgent
+from composio_swe.config.config_store import IssueConfig
 from composio_swe.config.context import Context, set_context
 
 
 if __name__ == "__main__":
-    issue_config = {
-        "repo_name": "ComposioHQ/composio",
-        "issue_id": "123",
-        "base_commit_id": "abc",
-        "issue_desc": "Add a local tool do terminal commands locally",
-    }
+    issue_config = IssueConfig(
+        repo_name="ComposioHQ/composio",
+        issue_id="123",
+        base_commit_id="abc",
+        issue_desc="""Composio Client should be able to run without API Key. It should err to the user only if local tools are not used.
+        In case of local tools are being used, then it should not err if the API Key is not provided.
+        Main change should happen only in python/composio/client/__init__.py""",
+    )
     ctx = Context()
     ctx.issue_config = issue_config  # type: ignore
     set_context(ctx)
