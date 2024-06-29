9eb0a1ee4486e1ac9b0f1f6eb8394d406503bffd
shubhra <shubhras01@gmail.com>
2024-06-29 14:58:58 +0530

    remove local workspace

--- python/composio/local_tools/local_workspace/workspace/actions/workspace_status.py
+++ python/composio/local_tools/local_workspace/workspace/actions/workspace_status.py
@@ -1,30 +1,32 @@
-from pydantic import Field
+from pydantic import Field, BaseModel
+from typing import TypeVar, Optional
 
+from composio.core.local import Action
+from abc import ABC
 from composio.local_tools.local_workspace.commons.get_logger import get_logger
-
-from .base_workspace_action import (
-    BaseWorkspaceAction,
-    BaseWorkspaceRequest,
-    BaseWorkspaceResponse,
-)
+from composio.workspace.workspace_factory import WorkspaceFactory
+from composio.workspace.base_workspace import Workspace
 
 
 logger = get_logger("workspace")
 
+RequestType = TypeVar("RequestType", bound=BaseModel)
+ResponseType = TypeVar("ResponseType", bound=BaseModel)
 
-class WorkspaceStatusRequest(BaseWorkspaceRequest):
+
+class WorkspaceStatusRequest(BaseModel):
     workspace_id: str = Field(
         ..., description="workspace-id will be used to get status of the workspace"
     )
 
 
-class WorkspaceStatusResponse(BaseWorkspaceResponse):
+class WorkspaceStatusResponse(BaseModel):
     workspace_status: str = Field(
         ..., description="status of the workspace given in request"
     )
 
 
-class WorkspaceStatusAction(BaseWorkspaceAction):
+class WorkspaceStatusAction(Action[RequestType, ResponseType], ABC):
     """
     Returns the status of workspace given in the request
     """
@@ -32,12 +34,13 @@ class WorkspaceStatusAction(BaseWorkspaceAction):
     _display_name = "Get workspace status"
     _request_schema = WorkspaceStatusRequest
     _response_schema = WorkspaceStatusResponse
+    workspace: Optional[Workspace] = None
 
     def execute(
         self, request_data: WorkspaceStatusRequest, authorisation_data: dict
     ) -> dict:
-        if authorisation_data is None:
-            authorisation_data = {}
+        authorisation_data = {} if not authorisation_data else authorisation_data
+        self.workspace = WorkspaceFactory.get_instance().get_registered_manager(request_data.workspace_id)
         if not self.workspace:
             raise ValueError(
                 f"workspace not found for workspace_id: {request_data.workspace_id}"
