4d1fbc9191aed8352676b9f5ecaa3069acaa65f4
shubhra <shubhras01@gmail.com>
2024-06-29 12:09:56 +0530

    local path

--- python/swe/benchmark/run_evaluation.py
+++ python/swe/benchmark/run_evaluation.py
@@ -51,7 +51,7 @@ def filter_from_repo_name(curr_dataset, repo_name):
 
 def get_issues_dataset():
     test_dataset = load_dataset(
-        "/home/shubhra/work/composio/swe-data/SWE-bench_Lite/data/",
+        "princeton-nlp/SWE-bench_Lite",
         split="test[210:300]",
     )
     return test_dataset
