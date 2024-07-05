e7f7ca6141e06fb999c1f6cc3f729c3ff685fec6
Karan Vaidya <kaavee315@gmail.com>
2024-07-05 22:58:54 +0530

    Make local env default

--- python/plugins/griptape/composio_griptape/__init__.py
+++ python/plugins/griptape/composio_griptape/__init__.py
@@ -1,12 +1,14 @@
 from composio_griptape.toolset import ComposioToolSet
 
 from composio.client.enums import Action, App, Tag, Trigger
+from composio.tools.env.factory import ExecEnv
 
 
 __all__ = (
     "Action",
     "App",
     "Tag",
+    "ExecEnv",
     "Trigger",
     "ComposioToolSet",
 )
