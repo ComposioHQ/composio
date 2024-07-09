14a9e58457df03e2d5ccb7f7c90050b81f32832f
angrybayblade <vptl185@gmail.com>
2024-07-09 16:05:45 +0530

    fix: add workaround for exit code issue on edit

--- python/composio/tools/env/host/shell.py
+++ python/composio/tools/env/host/shell.py
@@ -50,7 +50,11 @@ class HostShell(Shell):
     def _get_exit_code(self) -> int:
         """Get exit code of the last process."""
         self._write(ECHO_EXIT_CODE)
-        return int(self._read(wait=False).get(STDOUT).strip())  # type: ignore
+        exit_code = self._read().get(STDOUT).strip()  # type: ignore
+        if len(exit_code) == 0:
+            # `edit` command sometimes does not work as expected
+            return 0
+        return int(exit_code)
 
     def _read(
         self,
