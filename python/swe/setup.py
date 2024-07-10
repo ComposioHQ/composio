493dd7321b0e3af13435bfb4599e0e4fd32bc1f1
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-07-10 12:41:12 +0530

    Port docker utils to `swekit` (#282)

--- python/swe/setup.py
+++ python/swe/setup.py
@@ -2,9 +2,35 @@
 Setup configuration for SWE Kit.
 """
 
+import typing as t
 from pathlib import Path
 
 from setuptools import find_packages, setup
+
+
+SWEKIT = Path(__file__).parent.resolve() / "swekit"
+
+
+def scan_for_package_data(
+    directory: Path,
+    package: Path,
+    data: t.Optional[t.List[str]] = None,
+) -> t.List[str]:
+    """Walk the package and scan for package files."""
+    data = data or []
+    for child in directory.iterdir():
+        if child.name.endswith(".py") or child.name.endswith(".pyc"):
+            continue
+
+        if child.is_file():
+            data.append(str(child.relative_to(package)))
+            continue
+
+        data += scan_for_package_data(
+            directory=child,
+            package=package,
+        )
+    return data
 
 
 setup(
@@ -29,10 +55,10 @@ setup(
         ],
     },
     package_data={
-        "swekit": [
-            "py.typed",
-            "scaffold/templates/crewai/*",
-        ],
+        "swekit": scan_for_package_data(
+            directory=SWEKIT,
+            package=SWEKIT,
+        ),
     },
     include_package_data=True,
     install_requires=[
