e8b2af58f0bf58e3ab63cfed76493a228c2814c8
Karan Vaidya <kaavee315@gmail.com>
2024-07-10 13:11:30 +0530

    Update setup.py to include data files

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,34 @@
 Setup configuration for compsio core.
 """
 
-import os
+import typing as t
 from pathlib import Path
 
 from setuptools import find_packages, setup
+
+COMPOSIO = Path(__file__).parent.resolve() / "composio"
+
+
+def scan_for_package_data(
+    directory: Path,
+    package: Path,
+    data: t.Optional[t.List[str]] = None,
+) -> t.List[str]:
+    """Walk the package and scan for package files."""
+    data = data or []
+    for child in directory.iterdir():
+        if child.name.endswith(".py") or child.name.endswith(".pyc"):
+            continue
+
+        if child.is_file():
+            data.append(str(child.relative_to(package)))
+            continue
+
+        data += scan_for_package_data(
+            directory=child,
+            package=package,
+        )
+    return data
 
 
 setup(
@@ -52,9 +76,9 @@ setup(
     ],
     include_package_data=True,
     package_data={
-        "composio": [
-            "local_tools/local_workspace/config/*.yaml",
-            "local_tools/local_workspace/config/commands/*.sh",
-        ]
+        "composio": scan_for_package_data(
+            directory=COMPOSIO,
+            package=COMPOSIO,
+        ),
     },
 )
