452b10e4f5231694ef5cc5b71e8f92465e0c5458
sawradip <sawradip0@gmail.com>
2024-07-10 18:21:30 +0600

    black error fixed

--- python/plugins/openai/openai_demo.py
+++ python/plugins/openai/openai_demo.py
@@ -25,8 +25,8 @@ _ = composio_toolset.get_agent_instructions(
     apps=[App.GMAIL],
     actions=[
         Action.GITHUB_ACTIONS_ADD_CUSTOM_LABELS_TO_SELF_HOSTED_RUNNER_FOR_ORG,
-        Action.ASANA_ALLOCATIONS_DELETE_ALLOCATION_BY_ID
-    ]
+        Action.ASANA_ALLOCATIONS_DELETE_ALLOCATION_BY_ID,
+    ],
 )
 
 # Get response from the LLM
