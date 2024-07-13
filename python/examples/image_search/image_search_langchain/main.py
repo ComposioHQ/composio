1976801e0a74900c83d7bbb1c5a0a4c1341049ac
Prathit-tech <ppjoshi2100@gmail.com>
2024-07-13 08:15:20 +0530

    fix:image-search was improved and frotnend was changed

--- python/examples/image_search/image_search_langchain/main.py
+++ python/examples/image_search/image_search_langchain/main.py
@@ -4,7 +4,8 @@ import os  # For accessing environment variables
 import dotenv  # For loading environment variables from a .env file
 
 # Import modules from Composio and LangChain
-from composio import ComposioToolSet, App,Action 
+from composio_langchain import ComposioToolSet, App,Action
+from composio.tools.local import embedtool  # For embedding tool
 from langchain import hub  # For accessing LangChain hub
 from langchain.agents import (  # For creating agents
     AgentExecutor,
@@ -14,7 +15,6 @@ from langchain.agents import (  # For creating agents
 # Import ChatOpenAI from langchain_openai
 from langchain_openai import ChatOpenAI
 
-from composio.tools.local import embedtool  # For embedding tool
 
 # Load environment variables from a .env file (if applicable)
 dotenv.load_dotenv()  # Uncomment if you are using a .env file
@@ -32,18 +32,19 @@ tools = composio_toolset.get_tools(apps=[App.EMBEDTOOL])
 
 
 # Define the images path and query string
-images_path = "/path/to/the/images/folder"
-search_prompt = "image_description"
-top_no_of_images = 1 #returns n closest images to the search 
+images_path = input("Enter the path to the images folder:")
+search_prompt = input("Enter the image description for the image you want to search:")
+top_no_of_images = int(input("What number of images that are closest to the description that should be returned:")) #returns n closest images to the search 
 
+task_description = f"""
+    Check if a Vector Store exists for the image directory
+    If it doesn't create a vector store.
+    If it already exists, query the vector store
+    Search the vector store for {search_prompt}
+    The images path and indexed directory is {images_path}
+    return the top {top_no_of_images} results.
 
-#Create a vector store prompt template
-create_task_description = "Create a vector store of the images in the "+images_path
-
-#Query the vector store prompt template
-query_task_description = "Only Query the vector store for the top"+str(top_no_of_images)+",image of:"+search_prompt+"at the indexed directory:"+images_path
-
-
+"""
 # Create an OpenAI functions agent with the given LLM, tools, and prompt
 query_agent = create_openai_functions_agent(llm, tools, prompt)
 
@@ -51,7 +52,8 @@ query_agent = create_openai_functions_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=query_agent, tools=tools, verbose=True) # type: ignore
 
 # Execute the query task and get the result
-res = agent_executor.invoke({"input": create_task})
+res = agent_executor.invoke({"input": task_description})
 
 # Print the result
 print(res)
+
