6b188a837dc83787bcd232723630d401c699f8e2
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-14 11:05:38 +0530

    feat: create a RC release

--- js/src/index.ts
+++ js/src/index.ts
@@ -2,5 +2,6 @@ import { Composio } from "./sdk";
 import { LangchainToolSet } from "./frameworks/langchain";
 import { OpenAIToolSet } from "./frameworks/openai";
 import { CloudflareToolSet } from "./frameworks/cloudflare";
+import { ExecEnv } from "./utils/workspaceFactory";
 
-export { Composio, LangchainToolSet, OpenAIToolSet, CloudflareToolSet };
+export { Composio, LangchainToolSet, OpenAIToolSet, CloudflareToolSet, ExecEnv };
