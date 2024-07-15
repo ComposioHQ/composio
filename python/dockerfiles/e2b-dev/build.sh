806176bef05b4c662c3fd21f1b8cddccd790b0d1
angrybayblade <vptl185@gmail.com>
2024-07-15 13:11:21 +0530

    chore: clean dockerfiles

--- python/dockerfiles/e2b-dev/build.sh
+++ python/dockerfiles/e2b-dev/build.sh
@@ -1,3 +1,12 @@
-cp -r ../../ ./composio
+rm -rf composio
+mkdir composio/
+
+cp -r ../../composio/ ./composio/composio
+cp -r ../../setup.py ./composio/setup.py
+cp -r ../../README.md ./composio/README.md
+cp -r ../entrypoint.sh ./entrypoint.sh
+
 e2b template build
+
 rm -rf composio/
+rm ./entrypoint.sh
