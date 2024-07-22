3d0fa301ade2a2f37fe954deaf370f36ea08d37b
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-22 15:39:02 +0530

    chore: remove composioServer model

--- js/src/sdk/models/composioServer.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export class ComposioServer {
-    static async getAction(actionName: string): Promise<any> {
-        return axios.get(`https://tools-server.composio.dev/api/actions/${actionName}`).then((res) => res.data?.data).catch((err) => {
-            return null;
-        });
-    }
-}
