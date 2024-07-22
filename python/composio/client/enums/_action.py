94af3be77e4079e41951c7f18e4b2bcd635f3f91
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-22 16:24:53 +0530

    chore: add #type: ingore in _action.py

--- python/composio/client/enums/_action.py
+++ python/composio/client/enums/_action.py
@@ -3818,8 +3818,8 @@ class Action(_AnnotatedEnum[ActionData], path=ACTIONS_CACHE):
     def __init__(self, value: t.Union[str, te.Self, t.Type["SentinalObject"]]) -> None:
         """Create an Enum"""
         if hasattr(value, "sentinal"):
-            value = value().get_tool_merged_action_name()
-        super().__init__(value=value)
+            value = value().get_tool_merged_action_name()  # type: ignore
+        super().__init__(value=value)  # type: ignore
 
     @property
     def name(self) -> str:
