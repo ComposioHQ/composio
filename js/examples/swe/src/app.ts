05ea1873ea6e16f5a6014a4e186e53453907bc64
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-23 16:30:40 +0530

    chore: update README.md for js SWE

--- js/examples/swe/src/app.ts
+++ js/examples/swe/src/app.ts
@@ -1,21 +1,17 @@
 import { fromGithub } from './agents/inputs';
-import { initSWEAgent } from './agents/agent';
+import { initSWEAgent } from './agents/swe';
 
 async function main() {
   /**Run the agent.**/
   const { agent_executor, agent, tools, toolset } = await initSWEAgent();
-//   const { repo, issue } = await fromGithub(toolset);
-const repo = "utkarsh-dixit/speedy";
-const issue = "update readme.md and fix all typos";
-  console.log("Repo is", repo);
-  console.log("Issue is", issue);
+  const { repo, issue } = await fromGithub(toolset);
   await agent_executor.invoke({
     issue,
     repo
   });
 
   const response = await toolset.executeAction("gitcmdtool_get_patch_cmd", {});
-  console.log("Response is ", response);
+
   if (response.stderr && response.stderr.length > 0) {
     console.log('Error:', response.stderr);
   } else if (response.stdout) {
