8b46a6c890207e08d5a9b954ab46abed4a3711d9
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-24 20:18:46 +0530

    feat: update action enums

--- python/composio/client/enums/_app.py
+++ python/composio/client/enums/_app.py
@@ -2,91 +2,87 @@
 App enums.
 """
 import typing as t
+
 from composio.client.enums._action import Action
 from composio.client.enums.base import APPS_CACHE, AppData, _AnnotatedEnum, enum
+
 
 @enum
 class App(_AnnotatedEnum[AppData], path=APPS_CACHE):
     """Class to represent `App` entity."""
-    APIFY: 'App'
-    ASANA: 'App'
-    ATTIO: 'App'
-    BREVO: 'App'
-    CALENDLY1: 'App'
-    CLICKUP: 'App'
-    CODEFORMATTOOL: 'App'
-    CODEGREPTOOL: 'App'
-    CODEINDEXTOOL: 'App'
-    CODEINTERPRETER: 'App'
-    CODEMAPTOOL: 'App'
-    COMPOSIO: 'App'
-    DISCORD: 'App'
-    DROPBOX: 'App'
-    ELEVENLABS: 'App'
-    EMBEDTOOL: 'App'
-    EXA: 'App'
-    FIGMA: 'App'
-    FILEEDITTOOL: 'App'
-    FILETOOL: 'App'
-    FIRECRAWL: 'App'
-    GITCMDTOOL: 'App'
-    GITHUB: 'App'
-    GMAIL: 'App'
-    GMAIL1: 'App'
-    GOOGLE5: 'App'
-    GOOGLECALENDAR: 'App'
-    GOOGLECALENDAR3: 'App'
-    GOOGLEDOCS: 'App'
-    GOOGLEDOCS3: 'App'
-    GOOGLEDRIVE: 'App'
-    GOOGLEDRIVE1: 'App'
-    GOOGLEMEET: 'App'
-    GOOGLESHEETS: 'App'
-    GOOGLESHEETS1: 'App'
-    GOOGLETASKS: 'App'
-    GREPTILE: 'App'
-    HACKERNEWS: 'App'
-    HEROKU: 'App'
-    HISTORYFETCHERTOOL: 'App'
-    INDUCEDAI: 'App'
-    LINEAR: 'App'
-    LISTENNOTES: 'App'
-    MATHEMATICAL: 'App'
-    MULTIONAI: 'App'
-    NASA: 'App'
-    NOTION: 'App'
-    NOTION1: 'App'
-    OKTA: 'App'
-    PAGERDUTY: 'App'
-    PERPLEXITYAI: 'App'
-    RAGTOOL: 'App'
-    SCHEDULER: 'App'
-    SEARCHTOOL: 'App'
-    SERPAPI: 'App'
-    SHELLTOOL: 'App'
-    SLACK: 'App'
-    SLACKBOT: 'App'
-    SNOWFLAKE: 'App'
-    SOUNDCLOUD: 'App'
-    SPIDERTOOL: 'App'
-    SPLITWISE: 'App'
-    SPOTIFY: 'App'
-    SQLTOOL: 'App'
-    STRAVA: 'App'
-    TASKADE: 'App'
-    TAVILY: 'App'
-    TRELLO: 'App'
-    TWILIO: 'App'
-    TYPEFORM: 'App'
-    WEATHERMAP: 'App'
-    WEBTOOL: 'App'
-    WHATSAPP: 'App'
-    WORKABLE: 'App'
-    YOUSEARCH: 'App'
-    YOUTUBE: 'App'
-    ZENDESK: 'App'
-    ZEPTOOL: 'App'
-    ZOOM: 'App'
+
+    APIFY: "App"
+    ASANA: "App"
+    ATTIO: "App"
+    BREVO: "App"
+    CLICKUP: "App"
+    CODEFORMATTOOL: "App"
+    CODEGREPTOOL: "App"
+    CODEINDEXTOOL: "App"
+    CODEINTERPRETER: "App"
+    CODEMAPTOOL: "App"
+    COMPOSIO: "App"
+    DISCORD: "App"
+    DROPBOX: "App"
+    ELEVENLABS: "App"
+    EMBEDTOOL: "App"
+    EXA: "App"
+    FIGMA: "App"
+    FILEEDITTOOL: "App"
+    FILETOOL: "App"
+    FIRECRAWL: "App"
+    GITCMDTOOL: "App"
+    GITHUB: "App"
+    GMAIL: "App"
+    GOOGLECALENDAR: "App"
+    GOOGLEDOCS: "App"
+    GOOGLEDRIVE: "App"
+    GOOGLEMEET: "App"
+    GOOGLESHEETS: "App"
+    GOOGLETASKS: "App"
+    GREPTILE: "App"
+    HACKERNEWS: "App"
+    HEROKU: "App"
+    HISTORYFETCHERTOOL: "App"
+    INDUCEDAI: "App"
+    LINEAR: "App"
+    LISTENNOTES: "App"
+    MATHEMATICAL: "App"
+    MULTIONAI: "App"
+    NASA: "App"
+    NOTION: "App"
+    OKTA: "App"
+    PAGERDUTY: "App"
+    PERPLEXITYAI: "App"
+    RAGTOOL: "App"
+    SCHEDULER: "App"
+    SEARCHTOOL: "App"
+    SERPAPI: "App"
+    SHELLTOOL: "App"
+    SLACK: "App"
+    SLACKBOT: "App"
+    SNOWFLAKE: "App"
+    SOUNDCLOUD: "App"
+    SPIDERTOOL: "App"
+    SPLITWISE: "App"
+    SPOTIFY: "App"
+    SQLTOOL: "App"
+    STRAVA: "App"
+    TASKADE: "App"
+    TAVILY: "App"
+    TEST_ASANA: "App"
+    TRELLO: "App"
+    TWILIO: "App"
+    TYPEFORM: "App"
+    WEATHERMAP: "App"
+    WEBTOOL: "App"
+    WHATSAPP: "App"
+    WORKABLE: "App"
+    YOUSEARCH: "App"
+    YOUTUBE: "App"
+    ZENDESK: "App"
+    ZEPTOOL: "App"
+    ZOOM: "App"
 
     @property
     def is_local(self) -> bool:
@@ -98,7 +94,7 @@ class App(_AnnotatedEnum[AppData], path=APPS_CACHE):
         """Name of the app."""
         return self.load().name
 
-    def get_actions(self, tags: t.Optional[t.List[str]]=None) -> t.Iterator[Action]:
+    def get_actions(self, tags: t.Optional[t.List[str]] = None) -> t.Iterator[Action]:
         """
         Get actions for the given app filtered by the `tags`
 
@@ -106,7 +102,7 @@ class App(_AnnotatedEnum[AppData], path=APPS_CACHE):
         :return: Iterator object which yields `Action`
         """
         tags = tags or []
-        app = f'{self.slug.lower()}_'
+        app = f"{self.slug.lower()}_"
         for action in Action.all():
             if not action.slug.lower().startswith(app):
                 continue
