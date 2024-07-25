a12437e9bdc995846675465bbc1b9e576929a385
Karan Vaidya <kaavee315@gmail.com>
2024-07-25 19:44:28 +0530

    Fix file manager tools (#386)

--- python/swe/examples/crewai_agent/agent.py
+++ python/swe/examples/crewai_agent/agent.py
@@ -5,12 +5,11 @@
 import dotenv
 import os
 import typing as t
-from composio_crewai import App, Action, ComposioToolSet, WorkspaceType
+from composio_crewai import App, ComposioToolSet, WorkspaceType
 from crewai import Agent, Crew, Process, Task
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from prompts import BACKSTORY, DESCRIPTION, EXPECTED_OUTPUT, GOAL, ROLE
-from tools import calculate_operation
 
 # Load environment variables from .env
 dotenv.load_dotenv()
@@ -57,21 +56,12 @@ def get_langchain_llm() -> t.Union[ChatOpenAI, AzureChatOpenAI, ChatAnthropic]:
 composio_toolset = ComposioToolSet(workspace_config=WorkspaceType.Docker())
 
 # Get required tools
-tools = [
-    *composio_toolset.get_tools(
-        apps=[
-            App.FILETOOL,
-            App.SEARCHTOOL,
-        ]
-    ),
-    *composio_toolset.get_actions(
-        actions=[
-            Action.SHELL_EXEC_COMMAND,
-        ]
-    ),
-]
-
-tools.append(*composio_toolset.get_actions(actions=[calculate_operation]))
+tools = composio_toolset.get_tools(
+    apps=[
+        App.FILETOOL,
+        App.SHELLTOOL,
+    ]
+)
 
 # Define agent
 agent = Agent(
