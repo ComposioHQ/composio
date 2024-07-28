b35fd94e18fca839f08b1328384512de45470874
Himanshu Dixit <hudixt@gmail.com>
2024-07-28 18:43:55 +0530

    feat: add github and serapi test

--- python/tests/test_cli/base.py
+++ python/tests/test_cli/base.py
@@ -2,6 +2,7 @@
 CLI Test helper.
 """
 
+import re
 import typing as t
 from logging import Logger
 
@@ -10,22 +11,23 @@ from click.testing import CliRunner, Result
 from composio.cli import composio
 from composio.utils import logging
 
-
 class BaseCliTest:
     """Utility class for writing CLI tests."""
 
     result: Result
     logger: Logger = logging.get(name="tests")
+    runner: CliRunner = CliRunner()
 
     def run(
         self,
         *args: t.Any,
         mix_stderr: bool = False,
         env: t.Optional[t.Dict[str, str]] = None,
+        input: t.Optional[str] = None
     ) -> Result:
         """Run given command using click's CLI runner."""
         self.result = CliRunner(env=env, mix_stderr=mix_stderr).invoke(
-            cli=composio, args=tuple(map(str, args))
+            cli=composio, args=tuple(map(str, args)),input=input
         )
         return self.result
 
@@ -46,6 +48,13 @@ class BaseCliTest:
     def assert_stderr(self, match: str) -> None:
         """Check if given text is present in stderr."""
         assert match in self.result.stderr, {
+            "stdout": self.result.stdout,
+            "stderr": self.result.stderr,
+        }
+    
+    def assert_stdout_regex(self, match: re.Pattern) -> None:
+        """Check if given text is present in stdout."""
+        assert re.search(pattern=match, string=self.result.stdout), {
             "stdout": self.result.stdout,
             "stderr": self.result.stderr,
         }
--- python/tests/test_cli/test_add.py
+++ python/tests/test_cli/test_add.py
@@ -2,8 +2,8 @@
 Test `composio add`
 """
 
+import re
 from tests.test_cli.base import BaseCliTest
-
 
 class TestComposioAdd(BaseCliTest):
     """Test `composio add`"""
@@ -15,3 +15,22 @@ class TestComposioAdd(BaseCliTest):
         self.assert_stderr(
             match="Scheduler does not require authentication",
         )
+
+    def test_github_add(self) -> None:
+        """Test `composio add` with no-auth."""
+        self.run("add", "github",input="Y")
+
+        self.assert_stdout_regex(
+            match=re.compile("Do you want to replace the existing connection?|Adding integration..."),
+        )
+        self.assert_exit_code(code=0)
+    
+    def test_add_serpapi(self) -> None:
+        """Test `composio add` with no-auth."""
+        self.run("add", "serpapi",input="Y")
+
+        print(self.result)
+        self.assert_stdout_regex(
+            match=re.compile("Enter API Key"),
+        )
+        self.assert_exit_code(code=1)
