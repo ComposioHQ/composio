35beff617f5cc58671a54184e9c298113dfb36cb
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-07-29 17:42:21 +0530

    Multi platform builds for SWE images (#390)

--- python/swe/dockerfiles/build.py
+++ python/swe/dockerfiles/build.py
@@ -11,16 +11,40 @@ logs.mkdir(exist_ok=True)
 
 errors = []
 
+ARCHS = ("linux/arm64", "linux/amd64")
 
-def _build(file: Path, tag: str, *flags: str) -> None:
-    """Build docker image."""
+
+def _get_single_build_args(file: Path, tag: str, *flags: str) -> t.List[str]:
+    return ["docker", "build", str(file.parent), "-f", str(file), "-t", tag, *flags]
+
+
+def _get_multi_build_args(file: Path, tag: str, *flags: str) -> t.List[str]:
+    return [
+        "docker",
+        "buildx",
+        "build",
+        "--platform",
+        "linux/amd64,linux/arm64",
+        str(file.parent),
+        "-f",
+        str(file),
+        "-t",
+        tag,
+        "--push",
+        *flags,
+    ]
+
+
+def _build(file: Path, tag: str, multi: bool, *flags: str) -> None:
     log = f"composio_swe_{tag}.stderr"
     tag = f"composio/swe:{tag}"
 
-    print(f"Starting build for {tag} @ {file}")
+    print(f"Starting build for {tag} @ {file} with {multi=}")
     with open(logs / log, "w+", encoding="utf-8") as stderr:
         process = subprocess.run(
-            ["docker", "build", str(file.parent), "-f", str(file), "-t", tag, *flags],
+            (_get_multi_build_args if multi else _get_single_build_args)(
+                file=file, tag=tag
+            ),
             stderr=stderr,
         )
 
@@ -31,19 +55,17 @@ def _build(file: Path, tag: str, *flags: str) -> None:
         errors.append(f"Error building {tag} - {logs / log}")
 
 
-def _base(generated: Path) -> None:
-    """Build base images."""
+def _base(generated: Path, multi: bool = False) -> None:
     base = generated / "base"
-
     with ThreadPoolExecutor() as executor:
         futures = []
         for file in base.iterdir():
             _, tag = file.name.split(".", maxsplit=1)
-            futures.append(executor.submit(_build, file, tag))
+            futures.append(executor.submit(_build, file, tag, multi))
         _ = [fut.result() for fut in futures]
 
 
-def _swes(generated: Path) -> None:
+def _swes(generated: Path, multi: bool = False) -> None:
     with ThreadPoolExecutor() as executor:
         futures = []
         for child in generated.iterdir():
@@ -61,16 +83,17 @@ def _swes(generated: Path) -> None:
                         _build,
                         version / "Dockerfile",
                         tag,
+                        multi,
                     )
                 )
 
         _ = [fut.result() for fut in futures]
 
 
-def _pyenv(file: t.Optional[Path] = None) -> None:
+def _pyenv(file: t.Optional[Path] = None, multi: bool = False) -> None:
     print("Print building pyenv base")
     file = file or Path(__file__).parent / "templates" / "Dockerfile.pyenv"
-    _build(file=file, tag="pyenv")
+    _build(file=file, tag="pyenv", multi=multi)
 
 
 @click.command(name="build")
@@ -79,18 +102,27 @@ def _pyenv(file: t.Optional[Path] = None) -> None:
     type=str,
     default="./generated",
 )
-def build(generated: Path) -> None:
+@click.option(
+    "--multi",
+    is_flag=True,
+    help="Use this flag to build multi-plaform images",
+)
+def build(generated: Path, multi: bool = False) -> None:
     """Build docker images for SWEKIT."""
-    _pyenv()
-
-    generated = Path(generated or Path.cwd() / "generated").resolve()
-    _base(generated=generated)
+    _pyenv(multi=multi)
     if len(errors) > 0:
         print("==== Errors ====")
         print("\n".join(errors))
         return
 
-    _swes(generated=generated)
+    generated = Path(generated or Path.cwd() / "generated").resolve()
+    _base(generated=generated, multi=multi)
+    if len(errors) > 0:
+        print("==== Errors ====")
+        print("\n".join(errors))
+        return
+
+    _swes(generated=generated, multi=multi)
     print("==== Errors ====")
     print("\n".join(errors))
 
