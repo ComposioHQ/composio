b5165c515fe48254a58bb1bac4cbf60741be1fa0
Karan Vaidya <kaavee315@gmail.com>
2024-08-04 21:04:54 +0530

    Update

--- python/composio/tools/local/system/actions/notify.py
+++ python/composio/tools/local/system/actions/notify.py
@@ -8,11 +8,11 @@ from composio.tools.local.base import Action
 class NotifyRequest(BaseModel):
     title: str = Field(
         ...,
-        description="Title of the notification. Try to keep it short, max 20 characters.",
+        description="Title of the notification. Try to keep it short, max 20 characters. Avoid apostrophes or any other special characters.",
     )
     message: str = Field(
         ...,
-        description="Message of the notification. Try to keep it short, max 100 characters.",
+        description="Message of the notification. Try to keep it short, max 100 characters. Avoid apostrophes or any other special characters.",
     )
 
 
@@ -34,11 +34,23 @@ class Notify(Action[NotifyRequest, NotifyResponse]):
     def execute(self, request_data: NotifyRequest, authorisation_data: dict) -> dict:
         title = request_data.title
         message = request_data.message
-        command = (
-            f'osascript -e \'display notification "{message}" with title "{title}"\''
-        )
-        subprocess.run(command, shell=True, check=True)
-        execution_details = {"executed": True}
+        # Escape single quotes in the title and message
+        title = "".join(char for char in title if char.isalnum() or char.isspace())
+        message = "".join(char for char in message if char.isalnum() or char.isspace())
+        self.logger.info(f"Notifying: {title} - {message}")
+        command = [
+            "osascript",
+            "-e",
+            f'display notification "{message}" with title "{title}"',
+        ]
+        try:
+            subprocess.run(command, check=True, capture_output=True, text=True)
+            execution_details = {"executed": True}
+        except subprocess.CalledProcessError as e:
+            execution_details = {
+                "executed": False,
+                "error": f"Command failed: {e.stderr}",  # type: ignore
+            }
         response_data: dict = {}
 
         return {"execution_details": execution_details, "response_data": response_data}
@@ -46,6 +58,4 @@ class Notify(Action[NotifyRequest, NotifyResponse]):
 
 if __name__ == "__main__":
     print("Notifying...")
-    Notify().execute(
-        NotifyRequest(title="Test Title", message="https://composio.dev"), {}
-    )
+    Notify().execute(NotifyRequest(title="Test Title", message="This is Kara"), {})
