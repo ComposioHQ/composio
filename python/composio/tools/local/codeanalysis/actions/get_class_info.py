34083d2be9b65c5bc29bd83afc5cf40962e8d624
Shrey Singla <shrey.singla@sprinklr.com>
2024-08-08 12:31:06 +0530

    composio apps update

--- python/composio/tools/local/codeanalysis/actions/get_class_info.py
+++ python/composio/tools/local/codeanalysis/actions/get_class_info.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Type
 
 from pydantic import BaseModel, Field
 
-from python.composio.tools.local.codeanalysis.actions.base_action import (
+from composio.tools.local.codeanalysis.actions.base_action import (
     BaseCodeAnalysisAction,
 )
 
