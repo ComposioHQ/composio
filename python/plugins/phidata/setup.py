60abdff9fa55e565222dbc9bb75dc16720c072a6
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-08-12 18:31:21 +0530

    Release v0.4.2 (#432)

--- python/plugins/phidata/setup.py
+++ python/plugins/phidata/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_phidata",
-    version="0.4.1",
+    version="0.4.2",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Phidata Plugin.",
