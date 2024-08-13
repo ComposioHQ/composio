35676928f30f05975c4ef9ead154def313b96b23
angrybayblade <vptl185@gmail.com>
2024-08-13 12:42:06 +0530

    fix: pin composio-core

--- python/plugins/phidata/setup.py
+++ python/plugins/phidata/setup.py
@@ -22,6 +22,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9,<4",
-    install_requires=["composio_==0.4.1"],
+    install_requires=["composio_core==0.4.2"],
     include_package_data=True,
 )
