b7d7c5015d4c166adbc9bca069f89547e78e242d
shreysingla <shreysingla2@gmail.com>
2024-08-25 23:53:47 +0530

    fix: get relevant code added

--- python/composio/tools/local/codeanalysis/tool.py
+++ python/composio/tools/local/codeanalysis/tool.py
@@ -2,7 +2,7 @@ import typing as t
 
 from composio.tools.local.base import Action, Tool
 
-from .actions import CreateCodeMap, GetClassInfo, GetMethodBody, GetMethodSignature
+from .actions import CreateCodeMap, GetClassInfo, GetMethodBody, GetMethodSignature, GetRelevantCode
 
 
 class CodeAnalysisTool(Tool):
@@ -10,7 +10,7 @@ class CodeAnalysisTool(Tool):
 
     def actions(self) -> t.List[t.Type[Action]]:
         """Return the list of actions."""
-        return [CreateCodeMap, GetClassInfo, GetMethodBody, GetMethodSignature]
+        return [CreateCodeMap, GetClassInfo, GetMethodBody, GetMethodSignature, GetRelevantCode]
 
     def triggers(self) -> t.List:
         """Return the list of triggers."""
