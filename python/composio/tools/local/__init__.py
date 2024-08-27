95040a8a17becc007d7470a9f739a987d9a12c27
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-08-27 20:31:57 +0530

    Fix `get_action_schemas` on client handler (#488)
    Co-authored-by: Karan Vaidya <kaavee315@gmail.com>

--- python/composio/tools/local/__init__.py
+++ python/composio/tools/local/__init__.py
@@ -10,9 +10,11 @@ TOOLS_PATH = Path(__file__).parent
 
 
 def load_local_tools() -> ToolRegistry:
-    for tooldef in TOOLS_PATH.glob("*/tool.py"):
+    for tooldef in TOOLS_PATH.glob("**/tool.py"):
         importlib.import_module(
-            f"composio.tools.local.{tooldef.parent.name}.{tooldef.name.replace('.py', '')}"
+            "composio.tools.local."
+            + ".".join(tooldef.relative_to(TOOLS_PATH).parent.parts)
+            + ".tool"
         )
 
     return tool_registry
