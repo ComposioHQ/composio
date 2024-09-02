a9a99f79f95ead7964f887b73bb32f1481850083
angrybayblade <vptl185@gmail.com>
2024-09-02 15:23:06 +0530

    test: missing dependency installation check

--- python/tests/test_tools/test_toolset.py
+++ python/tests/test_tools/test_toolset.py
@@ -2,11 +2,15 @@
 Test composio toolset.
 """
 
+import logging
+from unittest import mock
+
 import pytest
 
 from composio import Action, App
 from composio.exceptions import ComposioSDKError
 from composio.tools import ComposioToolSet
+from composio.tools.base.abs import action_registry, tool_registry
 
 
 def test_get_schemas() -> None:
@@ -48,3 +52,49 @@ def test_uninitialize_app() -> None:
         ),
     ):
         ComposioToolSet().get_action_schemas(actions=[Action.APIFY_CREATE_APIFY_ACTOR])
+
+
+class TestValidateTools:
+    toolset: ComposioToolSet
+    package = "somepackage1"
+
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.toolset = ComposioToolSet()
+        tool_registry["local"][App.BROWSER_TOOL.slug].requires = [cls.package]
+        action_registry["local"][Action.BROWSER_TOOL_CLICK_ELEMENT.slug].requires = [
+            cls.package
+        ]
+
+    def test_validate_tools_app(self, caplog) -> None:
+        """Test `ComposioToolSet.validate_tools` method."""
+        with caplog.at_level(logging.INFO), mock.patch(
+            "subprocess.check_output",
+            return_value=b"Successfully installed",
+        ):
+            self.toolset.validate_tools(apps=[App.BROWSER_TOOL])
+            assert f"Installed {self.package}" in caplog.text
+
+    def test_validate_tools_action(self, caplog) -> None:
+        """Test `ComposioToolSet.validate_tools` method."""
+        with caplog.at_level(logging.INFO), mock.patch(
+            "subprocess.check_output",
+            return_value=b"Successfully installed",
+        ):
+            self.toolset.validate_tools(
+                actions=[
+                    Action.BROWSER_TOOL_CLICK_ELEMENT,
+                ]
+            )
+            assert f"Installed {self.package}" in caplog.text
+
+    def test_installation_failed(self, caplog) -> None:
+        """Test `ComposioToolSet.validate_tools` method."""
+        with caplog.at_level(logging.INFO), mock.patch(
+            "subprocess.check_output",
+            return_value=b"",
+        ), pytest.raises(
+            ComposioSDKError,
+            match=f"Error installing {self.package}",
+        ):
+            self.toolset.validate_tools(apps=[App.BROWSER_TOOL])
