91dc40d3a922bc35960c46717dbe939b67998b76
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-09-03 10:05:46 +0530

    Update python/swe/dockerfiles/run.sh

--- python/swe/dockerfiles/run.sh
+++ python/swe/dockerfiles/run.sh
@@ -6,12 +6,6 @@ docker build -t composio/swe:pyenv -f Dockerfile.pyenv .
 cd ../generated/base
 docker build -t composio/swe:py3.6 -f Dockerfile.py3.6 .
 
-# cd ../pallets__flask/2.3
-# docker build -t composio/swe:pallets-flask-2-3 -f Dockerfile .
-
-# cd ../django__django/5.0
-# docker build -t composio/swe:django-django-5-0 -f Dockerfile .
-
 
 cd ../django__django/3.0
 docker build -t composio/swe:django-django-3-0 -f Dockerfile .
