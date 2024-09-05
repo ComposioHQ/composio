0c0433d460acfa21dd04fe4d828278443b48162e
Karan Vaidya <kaavee315@gmail.com>
2024-09-05 19:39:22 +0530

    Remove composio llama depend on langchain

--- python/plugins/llamaindex/composio_llamaindex/toolset.py
+++ python/plugins/llamaindex/composio_llamaindex/toolset.py
@@ -5,9 +5,7 @@ from inspect import Signature
 import typing_extensions as te
 from llama_index.core.tools import FunctionTool
 
-from composio import Action, ActionType, AppType
-from composio import ComposioToolSet as BaseComposioToolSet
-from composio import TagType
+from composio import Action, ActionType, AppType, TagType, ComposioToolSet as BaseComposioToolSet
 from composio.utils.shared import get_pydantic_signature_format_from_schema_params
 
 
