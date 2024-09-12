306d70cd54913a1097dbc605724e79fcfca561e2
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-09-12 14:09:58 +0530

    Init sentry only when the user is logged in (#548)

--- python/composio/__init__.py
+++ python/composio/__init__.py
@@ -1,21 +1,9 @@
 # pylint: disable=wrong-import-position
 
+from composio.utils import sentry
 
-try:
-    import sentry_sdk
 
-    sentry_sdk.init(
-        dsn=(
-            "https://071d95df77abfdaaa87cd9441f42e12f@o4507825828462592.ingest.de."
-            "sentry.io/4507833628557392"
-        ),
-        traces_sample_rate=1.0,
-        profiles_sample_rate=1.0,
-        debug=False,
-    )
-except Exception:
-    pass
-
+sentry.init()
 
 import atexit  # noqa: E402
 
