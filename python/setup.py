8e14ce2c7a6e539b834203670a52b3032d6d1465
Karan Vaidya <kaavee315@gmail.com>
2024-09-14 00:03:45 +0530

    Update release (#590)

--- python/setup.py
+++ python/setup.py
@@ -76,7 +76,7 @@ all_requirements = core_requires + tools_require
 
 setup(
     name="composio_core",
-    version="0.5.21",
+    version="0.5.22",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
