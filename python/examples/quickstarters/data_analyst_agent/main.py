65dbcd3548f9f10871d64ac42a2b7afdda0ad8fc
Karan Vaidya <kaavee315@gmail.com>
2024-09-23 22:52:36 +0530

    Working code

--- python/examples/quickstarters/data_analyst_agent/main.py
+++ python/examples/quickstarters/data_analyst_agent/main.py
@@ -22,16 +22,19 @@ while True:
         role="Google Sheets Analyzer",
         goal="Understand the Sheets Data and Plot graphs that explain it better",
         backstory=(
-            "You are an AI agent specialized in analyzing Google Sheets data. "
-            "Your job is to analyse the data in the google sheets. Draw key and useful insights from it. "
-            "Represent the data in these sheets in plots and graphs that are easily readable"
+            "You are an AI agent specialized in analyzing Google Sheets data."
+            "Your job is to analyse the data in the google sheets. Draw key and useful insights from it."
+            "You can also run Python code, create charts and plots. Remember to get the file/chart if you are creating one in the code."
+            "Represent the data in these sheets in plots and graphs that are easily readable."
+            "NOTE: Mostly the user passes small sheets, so try to read the whole sheet at once and not via ranges."
         ),
         verbose=True,
         tools=tools,
         llm=llm,
     )
-
-    task = Task(
+    
+    
+    google_sheets_task = Task(
         description=f"Analyze the sheet and plot the graphs based on it. The task is: {main_task}",
         agent=google_sheets_agent,
         expected_output="Key insights and several plotted graphs based on the data were returned",
@@ -39,8 +42,9 @@ while True:
 
     my_crew = Crew(
         agents=[google_sheets_agent],
-        tasks=[task],
-        verbose=True
+        tasks=[google_sheets_task],
+        verbose=True,
+        memory=True,
     )
 
     result = my_crew.kickoff()
