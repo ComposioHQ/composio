9e6c3fcfd25cd2de4037438e6d3ea48f2c876e15
angrybayblade <vptl185@gmail.com>
2024-09-30 11:15:21 +0530

    fix: pin pillow

--- python/plugins/camel/setup.py
+++ python/plugins/camel/setup.py
@@ -22,6 +22,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9,<4",
-    install_requires=["composio_core==0.5.26", "camel-ai>=0.1.5.7"],
+    install_requires=["composio_core==0.5.26", "camel-ai>=0.1.5.7", "pillow"],
     include_package_data=True,
 )
