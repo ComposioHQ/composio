c0b232766153c1d7a08a7230853309525cebc011
Karan Vaidya <kaavee315@gmail.com>
2024-10-04 21:16:17 -0700

    Release new version

--- python/swe/setup.py
+++ python/swe/setup.py
@@ -35,7 +35,7 @@ def scan_for_package_data(
 
 setup(
     name="swekit",
-    version="0.2.28",
+    version="0.2.29",
     author="Shubhra",
     author_email="shubhra@composio.dev",
     description="Tools for running a SWE agent using Composio platform",
