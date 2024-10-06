b63f782b3f496e9c792c91495ac15694292eee0e
Prathit-tech <ppjoshi2100@gmail.com>
2024-10-06 12:03:08 +0530

    fix: Second brain agent was fixed

--- python/examples/advanced_agents/Second_brain_Agent/crewai/main.py
+++ python/examples/advanced_agents/Second_brain_Agent/crewai/main.py
@@ -1,67 +1,77 @@
-from composio_llamaindex import ComposioToolSet, App, Action
-from llama_index.core.agent import FunctionCallingAgentWorker
-from llama_index.core.llms import ChatMessage
-from llama_index.llms.openai import OpenAI
-from llama_index.llms.cerebras import Cerebras
-from llama_index.llms.groq import Groq
 from dotenv import load_dotenv
 from pathlib import Path
 import os
 
+from composio_crewai import ComposioToolSet, App, Action
+from crewai import Agent, Task, Crew, Process
+from langchain_openai import ChatOpenAI
+#from langchain_groq import ChatGroq
+#from langchain_cerebras import ChatCerebras
+
 load_dotenv()
-llm = OpenAI(model='gpt-4o')
-#llm = Groq(model="llama3-groq-70b-8192-tool-use-preview")
-#llm = Cerebras(model="llama3.1-70b")
+llm = ChatOpenAI(model='gpt-4o')
+#llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview")
+
 composio_toolset = ComposioToolSet()
 tools = composio_toolset.get_tools(apps = [App.EMBED_TOOL, App.RAGTOOL, App.WEBTOOL, App.SERPAPI, App.FILETOOL])
 
-prefix_messages = [
-    ChatMessage(
-        role="system",
-        content=(
-            f"""
+second_brain_agent = Agent(
+    role="Second Brain Agent",
+    goal="You are supposed to act as the second brain for the user.",
+    backstory= f"""
             You are an AI Assistant who's function it is to act like a second brain.
             You're a memory layer that stores all the information a user wants using RAG and embed tool.
             For Images use the embedtool. If it's a url make sure you browse the web and scrape the text
             when adding the information to the VectorStore. The path for both of them will be current directory.
    
-             """
-        )
-    )
-]
-
-
-agent = FunctionCallingAgentWorker(
-    tools=tools,  # Tools available for the agent to use
-    llm=llm,  # Language model for processing requests
-    prefix_messages=prefix_messages,  # Initial system messages for context
-    max_function_calls=10,  # Maximum number of function calls allowed
-    allow_parallel_tool_calls=False,  # Disallow parallel tool calls
-    verbose=True,  # Enable verbose output
-).as_agent()
+             """,
+    tools=tools,
+)
 
 vector_store_path = "./"
 while True:
     x = input("If you want to add something to the vector store, type 'add' and if you want to query, type 'query':")
     if x == 'add':
         a = input('Enter the url or image path to add in the vector store:')
-        task = f"""
+        add_task = Task(
+        description=f"""
         This is the item you've to add to the vector store: {a}.
         If its an image use Embed tool and if its a url scrape the text content and add it in RAG vector store
        
         The vector store/ Folder path should exist in {vector_store_path}.
         If its an image, the vector name should be Images
     
-        """
-        response = agent.chat(task)
+        """,
+            expected_output="task was completed.",
+            agent=second_brain_agent,
+            tools=tools,
+            verbose=True
+        )
+        crew = Crew(
+            agents=[second_brain_agent],
+            tasks=[add_task]
+        )
+        response = crew.kickoff()
         print(response)
     elif x == 'query':
         a = input("What is your query?")
         task = f"""
         Vector store exists in {vector_store_path}
-        Query is {a}. Query either the rag tool for textual content and embed tool for image related content
+        Query is {a}. Query either the rag tool for textual content and embed tool for image related content.
+        When the query is vague prioritise RAG tool.
         """
-        response = agent.chat(task)
+        query_task = Task(
+            description=task,
+            expected_output="task was completed.",
+            agent=second_brain_agent,
+            tools=tools,
+            verbose=True
+        )
+        crew = Crew(
+            agents=[second_brain_agent],
+            tasks=[query_task]
+        )
+        response = crew.kickoff()
         print(response)
     else:
         a = input("response fuzzy ending.")
