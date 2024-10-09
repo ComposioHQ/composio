64c3d32b90f51cf6d72fd496fe65add777bfd01a
angrybayblade <vptl185@gmail.com>
2024-10-09 10:45:46 +0530

    fix: revert test change

--- js/src/sdk/models/actions.spec.ts
+++ js/src/sdk/models/actions.spec.ts
@@ -59,7 +59,7 @@ describe("Apps class tests", () => {
             }
         });
 
-        expect(executionResult).toHaveProperty('successful', true);
+        expect(executionResult).toHaveProperty('successfull', true);
         //@ts-ignore
         expect((executionResult as any).data).toHaveProperty('stdout', 'Hello World\n');
         expect((executionResult as any).data).toHaveProperty('stderr', '');
