892dcf679e52c02ed66497a5218ebc1c4b22e322
tushar-composio <tushar@composio.dev>
2024-10-18 02:41:23 -0700

    Improve UX for action processor (#709)
    Adds a `processors` field on the `execute_action` and `get_tools`
    methods, and deprecates the use of `processors` field in the
    `ComposioToolSet` constructor.
    
    
    <!-- ELLIPSIS_HIDDEN -->
    
    
    ----
    
    > [!IMPORTANT]
    > Adds `processors` parameter to `execute_action` and `get_tools`,
    deprecating its use in `ComposioToolSet` constructor, with tests for new
    functionality.
    > 
    >   - **Behavior**:
    > - Adds `processors` parameter to `execute_action` and `get_tools`
    methods in `ComposioToolSet` and derived classes.
    > - Deprecates `processors` parameter in `ComposioToolSet` constructor.
    >   - **Methods**:
    > - Updates `execute_action` in `toolset.py` to merge processors if
    provided.
    > - Updates `get_tools` in `toolset.py` and derived classes to handle
    processors.
    >   - **Deprecation**:
    > - Adds deprecation warning for `processors` in `ComposioToolSet`
    constructor.
    >   - **Tests**:
    > - Adds tests in `test_toolset.py` for new `processors` handling in
    `execute_action` and `get_tools` methods.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=SamparkAI%2Fcomposio&utm_source=github&utm_medium=referral)<sup>
    for fe574c631348929e6980ae8a07683bcb620cbb50. It will automatically
    update as commits are pushed.</sup>
    
    
    <!-- ELLIPSIS_HIDDEN -->

--- python/composio/tools/toolset.py
+++ python/composio/tools/toolset.py
@@ -65,22 +65,28 @@ T = te.TypeVar("T")
 P = te.ParamSpec("P")
 
 _KeyType = t.Union[AppType, ActionType]
-_ProcessorType = t.Callable[[t.Dict], t.Dict]
+_CallableType = t.Callable[[t.Dict], t.Dict]
 
 MetadataType = t.Dict[_KeyType, t.Dict]
 ParamType = t.TypeVar("ParamType")
+
+# Enable deprecation warnings
+warnings.simplefilter("always", DeprecationWarning)
+
+
+ProcessorType = te.Literal["pre", "post", "schema"]
 
 
 class ProcessorsType(te.TypedDict):
     """Request and response processors."""
 
-    pre: te.NotRequired[t.Dict[_KeyType, _ProcessorType]]
+    pre: te.NotRequired[t.Dict[_KeyType, _CallableType]]
     """Request processors."""
 
-    post: te.NotRequired[t.Dict[_KeyType, _ProcessorType]]
+    post: te.NotRequired[t.Dict[_KeyType, _CallableType]]
     """Response processors."""
 
-    schema: te.NotRequired[t.Dict[_KeyType, _ProcessorType]]
+    schema: te.NotRequired[t.Dict[_KeyType, _CallableType]]
     """Schema processors"""
 
 
@@ -249,18 +255,24 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
             self._api_key = None
             self.logger.debug("`api_key` is not set when initializing toolset.")
 
-        self._processors = (
-            processors
-            if processors is not None
-            else {"post": {}, "pre": {}, "schema": {}}
-        )
+        if processors is not None:
+            warnings.warn(
+                "Setting 'processors' on the ToolSet is deprecated, they should"
+                "be provided to the 'get_tools()' method instead.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            self._processors: ProcessorsType = processors
+        else:
+            self._processors = {"post": {}, "pre": {}, "schema": {}}
+
         self._metadata = metadata or {}
         self._workspace_id = workspace_id
         self._workspace_config = workspace_config
         self._local_client = LocalClient()
 
         if len(kwargs) > 0:
-            self.logger.info(f"Extra kwards while initializing toolset: {kwargs}")
+            self.logger.info(f"Extra kwargs while initializing toolset: {kwargs}")
 
         self.logger.debug("Loading local tools")
         load_local_tools()
@@ -540,7 +552,10 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
             return [self._serialize_execute_params(p) for p in param]  # type: ignore
 
         if isinstance(param, dict):
-            return {key: self._serialize_execute_params(val) for key, val in param.items()}  # type: ignore
+            return {
+                key: self._serialize_execute_params(val)  # type: ignore
+                for key, val in param.items()
+            }
 
         raise ValueError(
             "Invalid value found for execute parameters"
@@ -567,7 +582,7 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
         self,
         key: _KeyType,
         type_: te.Literal["post", "pre", "schema"],
-    ) -> t.Optional[_ProcessorType]:
+    ) -> t.Optional[_CallableType]:
         """Get processor for given app or action"""
         processor = self._processors.get(type_, {}).get(key)  # type: ignore
         if processor is not None:
@@ -591,6 +606,13 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
                 f" through: {processor.__name__}"
             )
             data = processor(data)
+            # Users may not respect our type annotations and return something that isn't a dict.
+            # If that happens we should show a friendly error message.
+            if not isinstance(data, t.Dict):
+                warnings.warn(
+                    f"Expected {type_}-processor to return 'dict', got {type(data).__name__!r}",
+                    stacklevel=2,
+                )
         return data
 
     def _process_request(self, action: Action, request: t.Dict) -> t.Dict:
@@ -626,6 +648,22 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
             type_="schema",
         )
 
+    def _merge_processors(self, processors: ProcessorsType) -> None:
+        for processor_type in self._processors.keys():
+            if processor_type not in processors:
+                continue
+
+            processor_type = t.cast(ProcessorType, processor_type)
+            new_processors = processors[processor_type]
+
+            if processor_type in self._processors:
+                existing_processors = self._processors[processor_type]
+            else:
+                existing_processors = {}
+                self._processors[processor_type] = existing_processors
+
+            existing_processors.update(new_processors)
+
     @_record_action_if_available
     def execute_action(
         self,
@@ -635,6 +673,8 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
         entity_id: t.Optional[str] = None,
         connected_account_id: t.Optional[str] = None,
         text: t.Optional[str] = None,
+        *,
+        processors: t.Optional[ProcessorsType] = None,
     ) -> t.Dict:
         """
         Execute an action on a given entity.
@@ -649,6 +689,9 @@ class ComposioToolSet(WithLogger):  # pylint: disable=too-many-public-methods
         """
         action = Action(action)
         params = self._serialize_execute_params(param=params)
+        if processors is not None:
+            self._merge_processors(processors)
+
         if not action.is_runtime:
             params = self._process_request(action=action, request=params)
             metadata = self._add_metadata(action=action, metadata=metadata)
