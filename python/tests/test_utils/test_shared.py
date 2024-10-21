9058cd5f462c0c4b4c4cc6370bd3434bd81b90d0
Tushar Sadhwani <tushar@composio.dev>
2024-10-21 19:23:43 +0530

    add test for `pydantic_model_from_param_schema`

--- python/tests/test_utils/test_shared.py
+++ python/tests/test_utils/test_shared.py
@@ -4,6 +4,9 @@ Test shared module.
 
 from inspect import Parameter
 
+import pydantic
+import pytest
+
 from composio.utils import shared
 
 
@@ -50,7 +53,7 @@ def test_json_schema_to_pydantic_field() -> None:
 
     # Check the output
     assert result[0] == "owner"
-    assert result[1] == str
+    assert result[1] is str
     assert result[2].description == "The account owner of the repository."
     assert result[2].default == ...
 
@@ -79,8 +82,8 @@ def test_json_schema_to_fields_dict() -> None:
     assert len(result) == 2
     assert "owner" in result
     assert "repo" in result
-    assert result["owner"][0] == str
-    assert result["repo"][0] == str
+    assert result["owner"][0] is str
+    assert result["repo"][0] is str
     assert result["owner"][1].description == "The account owner of the repository."
     assert (
         result["repo"][1].description
@@ -88,3 +91,63 @@ def test_json_schema_to_fields_dict() -> None:
     )
     assert result["owner"][1].default == ...
     assert result["repo"][1].default is None
+
+
+def test_pydantic_model_from_param_schema() -> None:
+    schema = {
+        "description": "Request to get a Foo Bar.",
+        "properties": {
+            "id": {
+                "default": "",
+                "description": "ID of the file manager where the file will be opened, if not provided the recent file manager will be used to execute the action",
+                "title": "Id",
+                "type": "string",
+            },
+            "foo": {
+                "default": "",
+                "description": "The foo that we need",
+                "title": "Foo",
+                "type": "string",
+            },
+            "attr": {
+                "description": "List of attributes.",
+                "items": {"type": "string"},
+                "title": "Attr",
+                "type": "array",
+            },
+            "attrmap": {
+                "description": "List of attributes.",
+                "properties": {
+                    "attr": {
+                        "description": "List of attributes.",
+                        "items": {"type": "string"},
+                        "title": "Attr",
+                        "type": "array",
+                    }
+                },
+                "title": "Attr",
+                "type": "object",
+            },
+        },
+        "required": ["attr"],
+        "title": "FooBarRequest",
+        "type": "object",
+    }
+    model = shared.pydantic_model_from_param_schema(schema)
+
+    # Happy case:
+    model(attr=["xyz", "abc"])
+    attrs = ["xyz", "list of stuff"]
+    model(attr=attrs, attrmap={"attr": attrs})
+
+    # Sad cases:
+    with pytest.raises(pydantic.ValidationError):
+        model(attr=[123])
+    with pytest.raises(pydantic.ValidationError):
+        model(attr="foo")
+    with pytest.raises(pydantic.ValidationError):
+        model()
+    with pytest.raises(pydantic.ValidationError):
+        model(attr=attrs, attrmap=None)
+    with pytest.raises(pydantic.ValidationError):
+        model(attr=attrs, attrmap=["list directly"])
