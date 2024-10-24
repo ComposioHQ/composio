c31995ea21ad8962c6bd5c96d9dd85840588e3b3
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-10-24 12:17:09 +0530

    Pin tokenizers (#764)
    <!-- ELLIPSIS_HIDDEN -->
    
    
    
    > [!IMPORTANT]
    > Pin `tokenizers` version to `>=0.19,<0.20` in `tool.py` for
    compatibility.
    > 
    >   - **Dependencies**:
    > - Pin `tokenizers` version to `>=0.19,<0.20` in `tool.py` to ensure
    compatibility.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=SamparkAI%2Fcomposio&utm_source=github&utm_medium=referral)<sup>
    for 527582d75fd276ffed7ff71226f66e1b9315d43e. It will automatically
    update as commits are pushed.</sup>
    
    <!-- ELLIPSIS_HIDDEN -->

--- python/composio/tools/local/codeanalysis/tool.py
+++ python/composio/tools/local/codeanalysis/tool.py
@@ -18,6 +18,7 @@ class CodeAnalysisTool(LocalTool, autoload=True):
         "tree_sitter==0.21.3",
         "deeplake>3.9,<4",
         "sentence-transformers",
+        "tokenizers>=0.19,<0.20",
         "tree_sitter_languages",
         "git+https://github.com/DataDog/jedi.git@92d0c807b0dcd115b1ffd0a4ed21e44db127c2fb#egg=jedi",
         "PyJWT",  # deeplake/client/client.py:41
