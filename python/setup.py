1a8fe96036f644e795fa583fe48599bbd75e4233
Karan Vaidya <kaavee315@gmail.com>
2024-11-01 12:27:10 -0700

    Update release

--- python/setup.py
+++ python/setup.py
@@ -90,7 +90,7 @@ all_requirements = (
 
 setup(
     name="composio_core",
-    version="0.5.37",
+    version="0.5.37-rc.1",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
--- python/swe/setup.py
+++ python/swe/setup.py
@@ -35,7 +35,7 @@ def scan_for_package_data(
 
 setup(
     name="swekit",
-    version="0.2.34",
+    version="0.2.34-rc.1",
     author="Shubhra",
     author_email="shubhra@composio.dev",
     description="Tools for running a SWE agent using Composio platform",
@@ -66,7 +66,7 @@ setup(
         "swebench==2.1.0",
         "datasets>=2.20.0",
         "gitpython>=3.1.43",
-        "composio_core>=0.5.30,<=0.5.37",
+        "composio_core>=0.5.30,<=0.5.37-rc.1",
         "unidiff==0.7.5",
         "tqdm==4.66.4",
         "rich",
@@ -75,7 +75,7 @@ setup(
         "langgraph": [
             "langchain-aws==0.1.17",
             "langgraph>=0.2.16",
-            "composio_langgraph>=0.5.30,<=0.5.37",
+            "composio_langgraph>=0.5.30,<=0.5.37-rc.1",
             "python-dotenv==1.0.1",
         ]
     },
