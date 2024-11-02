e0b02ca7b8022a9f5268ff61568aba5f37f982a7
Himanshu Dixit <hudixt@gmail.com>
2024-11-02 10:06:24 +0530

    feat: SP

--- js/src/index.ts
+++ js/src/index.ts
@@ -7,11 +7,4 @@ import { Workspace } from "./env/";
 
 const { APPS,ACTIONS } = require("./constants");
 
-async function main(){
-    const client = new Composio("sdds");
-    await client.apps.list();
-}
-
-main();
-
 export { Composio, LangchainToolSet, OpenAIToolSet, CloudflareToolSet, VercelAIToolSet, Workspace,APPS,ACTIONS };
