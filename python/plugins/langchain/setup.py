2afe95b2fe86f6d9f7f4aff1dd270049a68a0bb5
Karan Vaidya <kaavee315@gmail.com>
2024-11-02 10:50:32 -0700

    Update

--- python/plugins/langchain/setup.py
+++ python/plugins/langchain/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="0.5.37-rc.1",
+    version="0.5.38-rc.1",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
@@ -27,7 +27,7 @@ setup(
         "langchain-openai>=0.0.2.post1",
         "pydantic>=2.6.4",
         "langchainhub>=0.1.15",
-        "composio_core>=0.5.30,<=0.5.37-rc.1",
+        "composio_core>=0.5.30,<=0.5.38-rc.1",
     ],
     include_package_data=True,
 )
