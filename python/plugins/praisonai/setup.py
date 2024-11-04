64c6d5ed01d59705dec10ce166ab45069aaa63ed
angrybayblade <vptl185@gmail.com>
2024-11-04 16:39:37 +0530

    feat: release v0.5.38

--- python/plugins/praisonai/setup.py
+++ python/plugins/praisonai/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_praisonai",
-    version="0.5.38-rc.1",
+    version="0.5.38",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio Tools to enhance your PraisonAI agents capabilities.",
@@ -22,6 +22,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=["composio_core>=0.5.30,<=0.5.38-rc.1", "PraisonAI>=0.0.2"],
+    install_requires=["composio_core>=0.5.30,<=0.5.38", "PraisonAI>=0.0.2"],
     include_package_data=True,
 )
