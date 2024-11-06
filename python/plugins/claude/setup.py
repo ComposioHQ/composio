f046efe5f0f2b557f0335d1a231dc867732c2df8
Karan Vaidya <kaavee315@gmail.com>
2024-11-05 18:23:32 -0800

    Release new version (#824)
    <!-- ELLIPSIS_HIDDEN -->
    
    
    
    > [!IMPORTANT]
    > Update version numbers to `0.5.39` across multiple files and modify a
    command in the documentation.
    > 
    >   - **Version Updates**:
    > - Update version to `0.5.39` in `__init__.py`, `setup.py` for
    `composio_core`, and multiple plugin `setup.py` files.
    >     - Update `swekit` version to `0.2.37` in `swe/setup.py`.
    > - Update `composio_core` dependency to `<=0.5.39` in various plugin
    `setup.py` files.
    >   - **Documentation**:
    > - Change command in `introduction.mdx` from `swekit scaffold crewai -o
    swe_agent` to `swekit scaffold swe -f crewai -o swe_agent`.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=SamparkAI%2Fcomposio&utm_source=github&utm_medium=referral)<sup>
    for 9b59a2517bf8779d8c97b3b096ff8c39de55c078. It will automatically
    update as commits are pushed.</sup>
    
    <!-- ELLIPSIS_HIDDEN -->

--- python/plugins/claude/setup.py
+++ python/plugins/claude/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_claude",
-    version="0.5.38",
+    version="0.5.39",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Claude LLMs.",
@@ -22,6 +22,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9,<4",
-    install_requires=["composio_openai>=0.5.30,<=0.5.38", "anthropic>=0.25.7"],
+    install_requires=["composio_openai>=0.5.30,<=0.5.39", "anthropic>=0.25.7"],
     include_package_data=True,
 )
