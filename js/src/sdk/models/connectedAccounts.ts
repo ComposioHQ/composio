4d7a76fea7cdb61634ec3f6e5005fef6f58a4af3
Himanshu Dixit <hudixt@gmail.com>
2024-11-08 19:40:46 +0530

    feat: redirect  uri

--- js/src/sdk/models/connectedAccounts.ts
+++ js/src/sdk/models/connectedAccounts.ts
@@ -13,6 +13,7 @@ type InitiateConnectionDataReq = InitiateConnectionPayloadDto & {
     entityId?: string;
     labels?: string[];
     integrationId: string;
+    redirectUri?: string;
 }
 
 export class ConnectedAccounts {
@@ -70,12 +71,13 @@ export class ConnectedAccounts {
 
     async initiate(payload: InitiateConnectionDataReq): Promise<ConnectionRequest> {
         try {
-            const {integrationId, entityId = 'default', labels,data={}} = payload;
+            const {integrationId, entityId = 'default', labels,data={}, redirectUri} = payload;
       
             const res = await client.connections.initiateConnection({ body: {
                 integrationId,
                 entityId,
                 labels,
+                redirectUri,
                 data,
             }  }).then(res => res.data);
             
