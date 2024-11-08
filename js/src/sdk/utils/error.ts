916e24bfa1edc20db8028bbfaddd014470716af0
Himanshu Dixit <hudixt@gmail.com>
2024-11-08 20:44:33 +0530

    feat: bettererror

--- js/src/sdk/utils/error.ts
+++ js/src/sdk/utils/error.ts
@@ -119,8 +119,8 @@ export class CEG {
 
         let axiosDataMessage = axiosError.response?.data?.message || axiosError.message;
         const status = axiosError.response?.status || axiosError.status || axiosError.code || 'unknown';
-        const urlAndStatus = axiosError.config?.url ? `in (URL: ${axiosError.config.url}, Status: ${status})` : '';
-        axiosDataMessage = `${axiosDataMessage || errorDetails.description || "No additional information available."} ${urlAndStatus}`;
+        const urlAndStatus = axiosError.config?.url ? `🔗 in URL: ${axiosError.config.url}, 📊 Status: ${status}` : '';
+        axiosDataMessage = `❌ ${ifObjectStringify(axiosDataMessage) || errorDetails.description || "No additional information available."} ${urlAndStatus}`;
         throw new ComposioError(
             errorKey as string,
             errorDetails.message,
@@ -151,4 +151,8 @@ export class CEG {
       
         throw new ComposioError(messageCode,  message || errorDetails.message, description || errorDetails.description, possibleFix || errorDetails.possibleFix);
     }
+}
+
+export const ifObjectStringify = (obj: any) => {
+    return typeof obj === 'object' ? JSON.stringify(obj) : obj;
 }
