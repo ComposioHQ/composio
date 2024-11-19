c1c9808b835744411a240c2d7632023ed73ce83b
angrybayblade <vptl185@gmail.com>
2024-11-19 19:14:51 +0530

    release: v0.5.43

--- python/setup.py
+++ python/setup.py
@@ -90,7 +90,7 @@ all_requirements = (
 
 setup(
     name="composio_core",
-    version="0.5.42",
+    version="0.5.43",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
