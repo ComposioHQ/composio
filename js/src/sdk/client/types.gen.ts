a180c966e94a02fe6e117d36b7145dcf00054ff0
Apoorv Taneja <purutaneja.com@gmail.com>
2024-11-25 20:08:22 +0530

    update ts

--- js/src/sdk/client/types.gen.ts
+++ js/src/sdk/client/types.gen.ts
@@ -955,7 +955,7 @@ export type Parameter = {
     /**
      * The location of the parameter. Can be 'query' or 'header'.
      */
-    location: ParameterLocation;
+    in: ParameterLocation;
     /**
      * The value of the parameter. For example, '1234567890', 'application/json', etc.,
      */
