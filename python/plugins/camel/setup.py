c84d5f7f58542bfc04761df529b9f80166ef0fe5
Karan Vaidya <kaavee315@gmail.com>
2024-11-26 22:44:56 +0530

    Release new version

--- python/plugins/camel/setup.py
+++ python/plugins/camel/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_camel",
-    version="0.5.48-rc.1",
+    version="0.5.49",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Claude LLMs.",
@@ -23,7 +23,7 @@ setup(
     ],
     python_requires=">=3.9,<4",
     install_requires=[
-        "composio_core>=0.5.40,<=0.5.48-rc.1",
+        "composio_core>=0.5.40,<=0.5.49",
         "camel-ai>=0.1.5.7,<=0.2.2",
         "pillow",
     ],
