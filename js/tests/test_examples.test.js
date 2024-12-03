b6b76d86e66e229a67c6d332590ec49a4154f516
Apoorv Taneja <purutaneja.com@gmail.com>
2024-12-03 18:11:25 +0530

    Marked working directory as false

--- js/tests/test_examples.test.js
+++ js/tests/test_examples.test.js
@@ -2,13 +2,13 @@
  * E2E Tests for plugin demos and examples.
  */
 
-const { execFile } = require('child_process');
-const path = require('path');
-const { promisify } = require('util');
+const { execFile } = require("child_process");
+const path = require("path");
+const { promisify } = require("util");
 const execFileAsync = promisify(execFile);
 
 // Paths
-const EXAMPLES_PATH = path.resolve(__dirname, '../examples');
+const EXAMPLES_PATH = path.resolve(__dirname, "../examples");
 
 // Require env vars
 const { COMPOSIO_API_KEY, OPENAI_API_KEY } = process.env;
@@ -16,50 +16,49 @@ const { COMPOSIO_API_KEY, OPENAI_API_KEY } = process.env;
 // Plugin test definitions
 const EXAMPLES = {
   portfolio_generator: {
-    file: path.join(EXAMPLES_PATH, 'portfolio-generator', 'demo.mjs'),
+    file: path.join(EXAMPLES_PATH, "portfolio-generator", "demo.mjs"),
     match: {
-      type: 'stdout',
-      values: ['🎉Output from agent: '],
+      type: "stdout",
+      values: ["🎉Output from agent: "],
     },
     env: { COMPOSIO_API_KEY },
   },
   lead_generator_agent: {
-    file: path.join(EXAMPLES_PATH, 'reddit-research', 'demo.mjs'),
+    file: path.join(EXAMPLES_PATH, "reddit-research", "demo.mjs"),
     match: {
-      type: 'stdout',
-      values: ['🎉Output from agent: '],
+      type: "stdout",
+      values: ["🎉Output from agent: "],
     },
     env: { COMPOSIO_API_KEY },
   },
   lead_outreach_agent: {
-    file: path.join(EXAMPLES_PATH, 'lead_outreach_agent', 'demo.mjs'),
+    file: path.join(EXAMPLES_PATH, "lead_outreach_agent", "demo.mjs"),
     match: {
-      type: 'stdout',
-      values: ['🎉Output from agent: '],
+      type: "stdout",
+      values: ["🎉Output from agent: "],
     },
     env: { COMPOSIO_API_KEY, OPENAI_API_KEY },
-  }
+  },
 };
 
-describe('E2E Tests for plugin demos and examples', () => {
+describe("E2E Tests for plugin demos and examples", () => {
   jest.setTimeout(300000); // Set timeout to 5 minutes
 
   for (const [exampleName, example] of Object.entries(EXAMPLES)) {
     test(`should run ${exampleName} example successfully`, async () => {
-
       const options = {
         env: { ...process.env, ...example.env },
         cwd: example.cwd || process.cwd(),
       };
 
-      try {
-        const { stdout, stderr } = await execFileAsync('node', [example.file], options);
-        const output = example.match.type === 'stdout' ? stdout : stderr;
-        for (const match of example.match.values) {
-          expect(output).toContain(match);
-        }
-      } catch (error) {
-        throw new Error(error);
+      const { stdout, stderr } = await execFileAsync(
+        "node",
+        [example.file],
+        options
+      );
+      const output = example.match.type === "stdout" ? stdout : stderr;
+      for (const match of example.match.values) {
+        expect(output).toContain(match);
       }
     });
   }
