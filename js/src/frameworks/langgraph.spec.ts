96063cecd84cdc45daea374db324c2496578db68
Apoorv Taneja <purutaneja.com@gmail.com>
2024-12-05 13:34:43 +0530

    changed casing

--- js/src/frameworks/langgraph.spec.ts
+++ js/src/frameworks/langgraph.spec.ts
@@ -1,13 +1,13 @@
 import { describe, it, expect, beforeAll } from "@jest/globals";
 import { z } from "zod";
 import { getTestConfig } from "../../config/getTestConfig";
-import { LanggraphToolSet } from "./langgraph";
+import { LangGraphToolSet } from "./langgraph";
 
 
-// Langgraph extens langchain class, all the properties are same
+// LangGraph extens langchain class, all the properties are same
 describe("Apps class tests", () => {
     it("getools", async () => {
-        let langgraphToolSet = new LanggraphToolSet({
+        let langgraphToolSet = new LangGraphToolSet({
             apiKey: getTestConfig().COMPOSIO_API_KEY,
             baseUrl: getTestConfig().BACKEND_HERMES_URL,
         });
--- js/src/frameworks/langgraph.ts
+++ js/src/frameworks/langgraph.ts
@@ -4,13 +4,13 @@ import type { Optional } from "../sdk/types";
 import { WorkspaceConfig } from "../env/config";
 import { Workspace } from "../env";
 
-export class LanggraphToolSet extends BaseComposioToolSet {
+export class LangGraphToolSet extends BaseComposioToolSet {
   /**
    * Composio toolset for Langgraph framework.
    *
    */
 
-  static FRAMEWORK_NAME = "langgraph";
+  static FRAMEWORK_NAME = "langGraph";
   static DEFAULT_ENTITY_ID = "default";
 
   constructor(
@@ -24,9 +24,9 @@ export class LanggraphToolSet extends BaseComposioToolSet {
     super({
       apiKey: config.apiKey || null,
       baseUrl: config.baseUrl || COMPOSIO_BASE_URL,
-      entityId: config.entityId || LanggraphToolSet.DEFAULT_ENTITY_ID,
+      entityId: config.entityId || LangGraphToolSet.DEFAULT_ENTITY_ID,
       workspaceConfig: config.workspaceConfig || Workspace.Host(),
-      runtime: LanggraphToolSet.FRAMEWORK_NAME,
+      runtime: LangGraphToolSet.FRAMEWORK_NAME,
     });
   }
 }
