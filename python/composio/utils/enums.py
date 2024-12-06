a880fdf4f87f206a0ef9b393ff61479571e05a04
angrybayblade <vptl185@gmail.com>
2024-12-06 19:29:00 +0530

    fix: filter `&` from tag name

--- python/composio/utils/enums.py
+++ python/composio/utils/enums.py
@@ -2,7 +2,7 @@
 
 
 def get_enum_key(name: str) -> str:
-    characters_to_replace = [" ", "-", "/", "(", ")", "\\", ":", '"', "'", "."]
+    characters_to_replace = [" ", "-", "/", "(", ")", "\\", ":", '"', "'", ".", "&"]
     for char in characters_to_replace:
         name = name.replace(char, "_")
     return name.upper()
