3872ed9ae51a4e250e1d27529b9801d3a7a1b793
Apoorv Taneja <purutaneja.com@gmail.com>
2024-12-11 19:14:30 +0530

    fixed action cli in TS

---
+++ js/src/cli/actions.ts
@@ -0,0 +1,80 @@
+import chalk from "chalk";
+import { Command } from "commander";
+import client from "../sdk/client/client";
+import { getOpenAPIClient } from "../sdk/utils/config";
+import { ListActionsV2Data } from "../sdk/client";
+
+export default class ActionCommand {
+  private program: Command;
+
+  constructor(program: Command) {
+    this.program = program;
+    this.program
+      .command("actions")
+      .description("Composio Actions")
+      .option(
+        "-a, --apps <appName>",
+        "List all actions for the given apps",
+        (value, previous: string[]) => previous.concat([value]),
+        []
+      )
+      .option(
+        "--tags <tagName>",
+        "List all actions for the given tags",
+        (value, previous: string[]) => previous.concat([value]),
+        []
+      )
+      .option("--use-case", "Search for actions based on the given use case")
+      .option("--limit <limit>", "Limit the number of actions to display")
+      .option("--enabled", "Only show enabled actions")
+      .option("--copy", "Copy the action to the clipboard")
+      .action(this.handleAction.bind(this));
+  }
+
+  private async handleAction(options: {
+    apps?: string[];
+    tags?: string[];
+    useCase?: string;
+    limit?: number;
+    enabled?: boolean;
+  }): Promise<void> {
+    getOpenAPIClient();
+    const { apps = [], tags = [], useCase, limit, enabled } = options;
+    if (apps) {
+      const data: ListActionsV2Data = {
+        query: {},
+      };
+      if (data?.query) {
+        if (tags) {
+          data.query.tags = tags.join(",");
+        }
+        if (limit) {
+          data.query.limit = limit;
+        }
+        if (enabled) {
+          data.query.showEnabledOnly = enabled;
+        }
+        if (useCase) {
+          data.query.useCase = useCase;
+        }
+        data.query.apps = apps.join(",");
+      }
+
+      try {
+        const response = await client.actionsV2.listActionsV2(data);
+        if (response.data?.items.length === 0) {
+          console.log(chalk.yellow("No actions found"));
+          return;
+        }
+        console.log(chalk.green("Here are the actions for the app:"));
+        console.log("");
+        // render list
+        const actions = response.data?.items || [];
+        actions.forEach((action) => console.log(action.name));
+      } catch (error) {
+        console.log(chalk.red((error as any).message));
+        return;
+      }
+    }
+  }
+}
