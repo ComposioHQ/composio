9dd9473fc25ac199906ef0cc9588587b4d28729b
Himanshu Dixit <hudixt@gmail.com>
2024-12-17 14:42:49 +0530

    feat: remove file

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('dist/index.d.ts', 'utf8');
-content = content.replace(/type\s+([A-Za-z0-9_]+)\s*=/g, 'export type $1 =');
-content = content.replace(/interface\s+([A-Za-z0-9_]+)\s*=/g, 'export interface $1 =');
-fs.writeFileSync('dist/index.d.ts', content);
