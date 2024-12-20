29e2627c84eba6d568296f4f297099709a2a8567
Karan Vaidya <kaavee315@gmail.com>
2024-12-20 15:21:10 +0530

    chore: Release (#1054)

--- python/plugins/crew_ai/setup.py
+++ python/plugins/crew_ai/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_crewai",
-    version="0.6.3",
+    version="0.6.4",
     author="Himanshu",
     author_email="himanshu@composio.dev",
     description="Use Composio to get an array of tools with your CrewAI agent.",
