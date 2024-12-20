69a6841f5c804b831effcf95ce7c3f9365e8a284
Karan Vaidya <kaavee315@gmail.com>
2024-12-20 21:23:05 +0530

    Fix recruiting agent

--- python/examples/advanced_agents/recruiter_agent/main.py
+++ python/examples/advanced_agents/recruiter_agent/main.py
@@ -1,7 +1,7 @@
 from composio_llamaindex import ComposioToolSet, App, Action
 from llama_index.core.agent import FunctionCallingAgentWorker
 from llama_index.core.llms import ChatMessage
-from llama_index.llms.openai import OpenAI
+from llama_index.llms.groq import Groq
 from dotenv import load_dotenv
 import os
 
@@ -12,21 +12,19 @@ agentops.init(os.getenv("AGENTOPS_API_KEY"))
 
 
 toolset = ComposioToolSet(api_key=os.getenv("COMPOSIO_API_KEY"))
-tools = toolset.get_tools(apps=[App.PEOPLEDATALABS, App.GOOGLESHEETS])
+tools = [*toolset.get_tools(apps=[App.GOOGLESHEETS]), *toolset.get_tools(actions=[Action.PEOPLEDATALABS_NATURAL_LANGUAGE_QUERY_ACTION])]
 
-llm = OpenAI(model="gpt-4o") #Groq(model="llama3-groq-70b-8192-tool-use-preview")
+llm = Groq(model="llama3-groq-70b-8192-tool-use-preview")
 
-spreadsheetid = '14T4e0j1XsWjriQYeFMgkM2ihyvLAplPqB9q8hytytcw'
 # Set up prefix messages for the agent
 prefix_messages = [
     ChatMessage(
         role="system",
         content=(
             f"""
-            Use the Google Sheets Action to add data to the google sheet 
             You are a recruiter agent. Based on user input, identify 10 highly qualified candidates using People Data Labs.
-            After identifying the candidates, create a Google Sheet and add their details for the provided candidate description, and spreadsheet ID: ${spreadsheetid}.
-            Print the list of candidates and their details along with the link to the Google Sheet.
+            After identifying the candidates, create a Google Sheet and add their details for the provided candidate description.
+            Print the list of candidates and their details along with the Linkedin profile to the Google Sheet.
             """
         ),
     )
@@ -41,7 +39,7 @@ agent = FunctionCallingAgentWorker(
     verbose=True,
 ).as_agent()
 
-candidate_description = '10 Senior Backend developers in San Francisco'
+candidate_description = '10 Senior Python Developers working in seed to series A startups living in San Francisco'
 user_input = f"Create a candidate list based on the description: {candidate_description}. Include all the important details required for the job. Add all of it the google sheet."
 response = agent.chat(user_input)
 print(response)
