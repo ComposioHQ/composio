040696635439b917de85ab39b797f8690ffacb36
tushar-composio <tushar@composio.dev>
2024-12-24 05:57:32 -0800

    chore: Release v0.6.7 (#1086)

--- python/plugins/langchain/setup.py
+++ python/plugins/langchain/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="0.6.6",
+    version="0.6.7",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
