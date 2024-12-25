c6d0a67ca98c53ab813b4cad8590ec51ecd4e2e8
Tushar Sadhwani <tushar@composio.dev>
2024-12-25 15:53:53 +0530

    dd back previous test

--- python/tests/test_cli/test_add.py
+++ python/tests/test_cli/test_add.py
@@ -27,6 +27,13 @@ class TestComposioAdd(BaseCliTest):
             ),
         )
 
+    def test_add_serpapi(self) -> None:
+        """Test `composio add` with no-auth."""
+        self.run("add", "serpapi", input="y")
+        self.assert_stdout_regex(
+            match=re.compile("Enter API Key"),
+        )
+
     def test_add_auth_mode_auto_uppercase(self) -> None:
         """Test `composio add` with lowercase --auth-mode."""
         self.run("add", "github", "--auth-mode", "oauth2", input="Y")
