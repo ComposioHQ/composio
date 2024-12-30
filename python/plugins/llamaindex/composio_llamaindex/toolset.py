ed10eb53a05881c5c7682405051e07ad505b4932
Viraj <35092918+angrybayblade@users.noreply.github.com>
2024-12-30 15:02:11 +0530

    fix: limit number of characters on every plugin (#1098)

--- python/plugins/llamaindex/composio_llamaindex/toolset.py
+++ python/plugins/llamaindex/composio_llamaindex/toolset.py
@@ -16,6 +16,7 @@ class ComposioToolSet(
     BaseComposioToolSet,
     runtime="llamaindex",
     description_char_limit=1024,
+    action_name_char_limit=64,
 ):
     """
     Composio toolset for LlamaIndex framework.
