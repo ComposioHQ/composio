8fa8fa25a160030d39a0cb0a78a9901abb4f0329
Tushar Sadhwani <tushar@composio.dev>
2025-01-02 13:20:38 +0530

    lints

--- python/composio/tools/env/base.py
+++ python/composio/tools/env/base.py
@@ -215,8 +215,10 @@ class RemoteWorkspace(Workspace):
             name=ENV_COMPOSIO_BASE_URL,
             default=config.composio_base_url,
         )
-        self.github_access_token = config.github_access_token or os.environ.get(
-            ENV_GITHUB_ACCESS_TOKEN, "NO_VALUE"
+        self.github_access_token = (
+            config.github_access_token
+            if config.github_access_token is not None
+            else os.environ.get(ENV_GITHUB_ACCESS_TOKEN, "NO_VALUE")
         )
         self.environment.update(
             {
