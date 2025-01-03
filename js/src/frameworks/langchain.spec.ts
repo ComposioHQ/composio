218404eadb153d2e287c79d0dffaaf9616cbd63a
Himanshu Dixit <hudixt@gmail.com>
2025-01-03 14:38:54 +0530

    feat: custom action fix (#1150)

--- js/src/frameworks/langchain.spec.ts
+++ js/src/frameworks/langchain.spec.ts
@@ -60,10 +60,11 @@ describe("Apps class tests", () => {
       },
     });
 
-    langchainToolSet.getTools({
+    const tools = await langchainToolSet.getTools({
       actions: ["starRepositoryCustomAction"],
     });
 
+    await expect(tools.length).toBe(1);
     const actionOuput = await langchainToolSet.executeAction({
       action: "starRepositoryCustomAction",
       params: {
