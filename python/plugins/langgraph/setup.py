39ff4f32d9cb6a1844fb62a320765e66fdc2a51c
Karan Vaidya <kaavee315@gmail.com>
2025-01-09 17:18:58 +0530

    Release (#1171)
    Co-authored-by: angrybayblade <vptl185@gmail.com>

--- python/plugins/langgraph/setup.py
+++ python/plugins/langgraph/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langgraph",
-    version="0.6.12",
+    version="0.6.13",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get array of tools with LangGraph Agent Workflows",
