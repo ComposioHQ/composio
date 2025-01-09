4ba0a05e0fb618ffc1d840454e90eb9259fbe44b
Prathit-tech <ppjoshi2100@gmail.com>
2025-01-09 19:00:05 +0530

    fix: code execution agent fixed

--- python/examples/quickstarters/code_execution_agent/crewai_ci_chart.py
+++ python/examples/quickstarters/code_execution_agent/crewai_ci_chart.py
@@ -10,16 +10,15 @@ while True:
     if main_task.lower() == "exit":
         break
 
-    code_interpreter_tools = ComposioToolSet().get_tools([App.CODEINTERPRETER])
+    code_interpreter_tools = ComposioToolSet().get_tools(apps=[App.CODEINTERPRETER])
 
     code_interpreter_agent = Agent(
         role="Python Code Interpreter Agent",
         goal=f"""Run I a code to get achieve a task given by the user""",
         backstory="""You are an agent that helps users run Python code.""",
         verbose=True,
-        tools=code_interpreter_tools,
+        tools=list(code_interpreter_tools), # type: ignore
         llm=llm,
-        memory=True,
     )
 
     code_interpreter_task = Task(
