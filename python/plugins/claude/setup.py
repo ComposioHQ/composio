19229791ed15bd853f20a802d196ade355317397
Karan Vaidya <kaavee315@gmail.com>
2025-01-09 17:18:58 +0530

    Release (#1171)
    Co-authored-by: angrybayblade <vptl185@gmail.com>

--- python/plugins/claude/setup.py
+++ python/plugins/claude/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_claude",
-    version="0.6.12",
+    version="0.6.13",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Claude LLMs.",
