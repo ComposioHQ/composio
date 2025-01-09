9f81026ae8210cde955ddbc38712587b95d10855
Prathit-tech <ppjoshi2100@gmail.com>
2025-01-09 18:51:22 +0530

    fix: pr agent crewai fixed

--- python/examples/advanced_agents/pr_agent/pr_agent_crewai/setup.sh
+++ python/examples/advanced_agents/pr_agent/pr_agent_crewai/setup.sh
@@ -16,12 +16,12 @@ pip install -r requirements.txt
 echo "Login to your Composio acount"
 composio login
 
-# Add trello tool
+
 echo "Add github tool. Finish the flow"
 composio add github
 
-echo "Add slackbot tool. Finish the flow"
-composio add slackbot
+echo "Add slack tool. Finish the flow"
+composio add slack
 
 echo "Updating apps"
 composio apps update
