f33976d516b6886f66523b7abca04ceb5a1605d9
abhishekpatil4 <abhishekpersonal2@gmail.com>
2025-01-10 23:29:44 +0530

    fix: llamaindex tests

--- js/src/frameworks/llamaindex.spec.ts
+++ js/src/frameworks/llamaindex.spec.ts
@@ -1,40 +1,39 @@
 import { beforeAll, describe, expect, it } from "@jest/globals";
-import { getTestConfig } from "../../config/getTestConfig";
-import { LlamaIndexToolSet } from "./llamaindex";
-import { ActionExecuteResponse } from "../sdk/models/actions";
 import { z } from "zod";
+import { getTestConfig } from "../../config/getTestConfig";
+import { ActionExecuteResponse } from "../sdk/models/actions";
+import { LlamaIndexToolSet } from "./llamaindex";
 
 describe("Apps class tests", () => {
-  let langchainToolSet: LlamaIndexToolSet;
+  let llamaindexToolSet: LlamaIndexToolSet;
   beforeAll(() => {
-    langchainToolSet = new LlamaIndexToolSet({
-    //   apiKey: getTestConfig().COMPOSIO_API_KEY,
-      apiKey: "dbosseffg2hmhuydsm1tej",
+    llamaindexToolSet = new LlamaIndexToolSet({
+        apiKey: getTestConfig().COMPOSIO_API_KEY,
       baseUrl: getTestConfig().BACKEND_HERMES_URL,
     });
   });
 
   it("getools", async () => {
-    const tools = await langchainToolSet.getTools({
-      apps: ["GITHUB"],
+    const tools = await llamaindexToolSet.getTools({
+      apps: ["github"],
     });
     expect(tools).toBeInstanceOf(Array);
   });
   it("check if tools are coming", async () => {
-    const tools = await langchainToolSet.getTools({
-      actions: ["GITHUB_STAR_A_REPOSITORY_FOR_THE_AUTHENTICATED_USER"],
+    const tools = await llamaindexToolSet.getTools({
+      actions: ["GITHUB_GITHUB_API_ROOT"],
     });
     expect(tools.length).toBe(1);
   });
   it("check if getTools, actions are coming", async () => {
-    const tools = await langchainToolSet.getTools({
+    const tools = await llamaindexToolSet.getTools({
       actions: ["GITHUB_GITHUB_API_ROOT"],
     });
 
     expect(tools.length).toBe(1);
   });
   it("Should create custom action to star a repository", async () => {
-    await langchainToolSet.createAction({
+    await llamaindexToolSet.createAction({
       actionName: "starRepositoryCustomAction",
       toolName: "github",
       description: "This action stars a repository",
@@ -56,19 +55,19 @@ describe("Apps class tests", () => {
       },
     });
 
-    const tools = await langchainToolSet.getTools({
+    const tools = await llamaindexToolSet.getTools({
       actions: ["starRepositoryCustomAction"],
     });
 
     await expect(tools.length).toBe(1);
-    const actionOuput = await langchainToolSet.executeAction({
+    const actionOuput = await llamaindexToolSet.executeAction({
       action: "starRepositoryCustomAction",
       params: {
         owner: "composioHQ",
         repo: "composio",
       },
       entityId: "default",
-      connectedAccountId: "4364bbb6-3382-4bb6-b4be-e7ff440f90b4",
+      connectedAccountId: "9442cab3-d54f-4903-976c-ee67ef506c9b",
     });
 
     expect(actionOuput).toHaveProperty("successful", true);
