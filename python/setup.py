85de9b63a4647293a16e9ed88668067f3534378e
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-01-15 11:10:52 +0530

    feat: release 0.6.15 (#1194)

--- python/setup.py
+++ python/setup.py
@@ -88,7 +88,7 @@ all_requirements = (
 
 setup(
     name="composio_core",
-    version="0.6.14",
+    version="0.6.15",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
