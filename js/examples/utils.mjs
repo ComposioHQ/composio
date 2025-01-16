258f525f5ca1ffe2b1cfac108fc9e2303575ea57
Apoorv Taneja <purutaneja.com@gmail.com>
2025-01-16 18:12:05 +0530

    updated headers

--- js/examples/utils.mjs
+++ js/examples/utils.mjs
@@ -1,10 +1,10 @@
 import { createOpenAI } from "@ai-sdk/openai";
 
 export const openai = createOpenAI({
-    baseURL: "https://oai.helicone.ai/v1",
-    headers: {
-      "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
-      "Helicone-Cache-Enabled": "true",
-      "Cache-Control": "max-age=2592000",
-    },
-  });
+  baseURL: "https://oai.helicone.ai/v1",
+  headers: {
+    "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
+    "Helicone-Cache-Enabled": "true",
+    "Helicone-User-Id": "GitHub-CI-Example-Tests",
+  },
+});
