e49e732054e3e759feec4c3465f896ce72fe63a6
Apoorv Taneja <purutaneja.com@gmail.com>
2025-01-16 17:32:49 +0530

    remove helicone key

--- js/examples/utils.mjs
+++ js/examples/utils.mjs
@@ -3,7 +3,7 @@ import { createOpenAI } from "@ai-sdk/openai";
 export const openai = createOpenAI({
     baseURL: "https://oai.helicone.ai/v1",
     headers: {
-      "Helicone-Auth": `Bearer sk-helicone-bkesgcq-dw5u3wy-qifu3iq-4qc53mi`,
+      "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
       "Helicone-Cache-Enabled": "true",
       "Cache-Control": "max-age=2592000",
     },
