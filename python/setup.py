21e099b302724c8447b87477349ff40f5aab6be0
Karan Vaidya <kaavee315@gmail.com>
2025-01-20 11:46:58 -0800

    chore: relase (#1214)

--- python/setup.py
+++ python/setup.py
@@ -88,7 +88,7 @@ all_requirements = (
 
 setup(
     name="composio_core",
-    version="0.6.15",
+    version="0.6.16",
     author="Utkarsh",
     author_email="utkarsh@composio.dev",
     description="Core package to act as a bridge between composio platform and other services.",
