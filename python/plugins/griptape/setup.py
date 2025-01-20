5fc79616447b8d4f21d547cd98f0795bc46a5bfe
Karan Vaidya <kaavee315@gmail.com>
2025-01-20 11:46:58 -0800

    chore: relase (#1214)

--- python/plugins/griptape/setup.py
+++ python/plugins/griptape/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_griptape",
-    version="0.6.15",
+    version="0.6.16",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Griptape workflow.",
