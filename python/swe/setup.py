de68cff8a3e0399c1aa02b4b28ece7184ce550d2
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-01-24 12:07:57 +0530

    feat: release v0.6.17 (#1221)

--- python/swe/setup.py
+++ python/swe/setup.py
@@ -35,7 +35,7 @@ def scan_for_package_data(
 
 setup(
     name="swekit",
-    version="0.3.17",
+    version="0.3.18",
     author="Shubhra",
     author_email="shubhra@composio.dev",
     description="Tools for running a SWE agent using Composio platform",
