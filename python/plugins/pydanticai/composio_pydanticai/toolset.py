303956299612b5aafd1666021cfe6118bee61a50
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-01-24 14:29:03 +0530

    chore: clean up `toolset.py` (#1220)

--- python/plugins/pydanticai/composio_pydanticai/toolset.py
+++ python/plugins/pydanticai/composio_pydanticai/toolset.py
@@ -179,7 +179,7 @@ class ComposioToolSet(
         """
         self.validate_tools(apps=apps, actions=actions, tags=tags)
         if processors is not None:
-            self._merge_processors(processors)
+            self._processor_helpers.merge_processors(processors)
 
         max_retries = max_retries or {}
 
