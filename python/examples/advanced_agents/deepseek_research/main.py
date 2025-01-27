8435cd3da7d3d0a7b726afa5dedf88a0a6bb3eb2
Prathit-tech <ppjoshi2100@gmail.com>
2025-01-27 17:46:45 +0530

    feat: report generation added to the agent

--- python/examples/advanced_agents/deepseek_research/main.py
+++ python/examples/advanced_agents/deepseek_research/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # Initialize toolset and LLM
 toolset = ComposioToolSet()
-tools = toolset.get_tools(actions=[Action.EXA_SEARCH, Action.EXA_SIMILARLINK])
+tools = toolset.get_tools(actions=[Action.EXA_SEARCH, Action.EXA_SIMILARLINK, Action.GOOGLEDOCS_CREATE_DOCUMENT])
 
 function_calling_llm = OpenAI(model="gpt-4o")
 deepseek_llm = Groq(model="deepseek-r1-distill-llama-70b")
@@ -48,7 +48,7 @@ def chatbot():
     print("🤖: Hi! I can help you research content based on the latest trends. Let’s start!")
     human_input = input("What do you want to research: ")
     questions = deepseek_llm.complete("Suggest a list of questions to research on the topic: " + human_input)
-    res = agent.chat(str(questions)+"\n These are the questions you need to research about, they are related to the topic: " + human_input)
+    res = agent.chat(str(questions)+"\n These are the questions you need to research about, they are related to the topic: " + human_input+"\n After research is complete, create a google doc with all the information and share the link with me")
     print(res.response)
 
 if __name__ == "__main__":
