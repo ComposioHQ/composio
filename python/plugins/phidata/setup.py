b8e535bd140f7e3fa0fa45d642a4ed5b50652cc9
Utkarsh Dixit <utkarshdix02@gmail.com>
2025-01-29 00:45:34 +0530

    feat: bump to 0.6.19 (#1233)

--- python/plugins/phidata/setup.py
+++ python/plugins/phidata/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_phidata",
-    version="0.6.18",
+    version="0.6.19",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Phidata Plugin.",
@@ -23,7 +23,7 @@ setup(
     ],
     python_requires=">=3.9,<4",
     install_requires=[
-        "composio_core>=0.6.11,<0.7.0",
+        "composio_core>=0.6.0,<0.7.0",
         "composio_openai>=0.5.0,<0.7.0",
         "phidata",
     ],
