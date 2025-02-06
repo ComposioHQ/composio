290aa34da17403f552417707ce79cda0f6caea7f
angrybayblade <vptl185@gmail.com>
2025-02-06 14:11:53 +0530

    feat: google gemini plugin

---
+++ python/plugins/gemini/composio_gemini/__init__.py
@@ -0,0 +1,16 @@
+from composio import WorkspaceType, action
+
+from composio_langchain import Action, App, Tag, Trigger
+
+from .toolset import ComposioToolSet
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "Trigger",
+    "WorkspaceType",
+    "action",
+    "ComposioToolSet",
+)
