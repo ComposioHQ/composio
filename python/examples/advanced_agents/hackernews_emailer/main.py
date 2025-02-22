725353ac2c4957e32d133cbe4155324489e49993
Lingala Rahul <lingalarahul@Lingalas-MacBook-Pro-2.local>
2025-02-22 11:17:35 +0530

    Add hacker news emailer example via openai assistant

---
+++ python/examples/advanced_agents/hackernews_emailer/main.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+from composio_openai import ComposioToolSet, Action
+
+load_dotenv()
+
+openai_client = OpenAI()
+composio_toolset = ComposioToolSet()
+
+tools = composio_toolset.get_tools(actions=[Action.HACKERNEWS_GET_FRONTPAGE, Action.GMAIL_SEND_EMAIL])
+
+receiver = "lingalarahul7@gmail.com"
+task = "You are a Search Agent for Hackernews. Get the frontpage posts from hackernews and then send an email about it to " + receiver
+
+assistant_instruction = "You are a super intelligent assistant"
+
+assistant = openai_client.beta.assistants.create(
+  name="Assistant",
+  instructions=assistant_instruction,
+  model="gpt-4o",
+  tools=tools,
+)
+
+thread = openai_client.beta.threads.create()
+message = openai_client.beta.threads.messages.create(thread_id=thread.id,role="user",content=task)
+
+run = openai_client.beta.threads.runs.create(thread_id=thread.id,assistant_id=assistant.id)
+
+response_after_tool_calls = composio_toolset.wait_and_handle_assistant_tool_calls(
+    client=openai_client,
+    run=run,
+    thread=thread,
+)
+
+print("Executed functions:")
+for tool in response_after_tool_calls.tools:
+    print(tool.function.name)
+
+messages = openai_client.beta.threads.messages.list(thread_id=thread.id)
+for msg in messages:
+    print(msg.content)
+    break
---
+++ python/examples/advanced_agents/hackernews_emailer/readme.md
@@ -0,0 +1,30 @@
+# Twitter Posting Agent Guide
+
+This guide provides detailed steps to fetch hacker news frontpage posts and send an email to a given email id.
+
+## Steps to Run
+**Navigate to the Project Directory:**
+Change to the directory where the `setup.sh`, `main.py`, `requirements.txt`, and `README.md` files are located. For example:
+```sh
+cd path/to/project/directory
+```
+
+### 1. Run the Setup File
+Make the setup.sh Script Executable (if necessary):
+On Linux or macOS, you might need to make the setup.sh script executable:
+```shell
+chmod +x setup.sh
+```
+Execute the setup.sh script to set up the environment, install dependencies, login to composio and 
+add necessary tools:
+```shell
+./setup.sh
+```
+Now, Fill in the .env file with your secrets.
+### 2. Run the python script
+```shell
+python python/examples/advanced_agents/hackernews_emailer/main.py
+```
+
+
+
