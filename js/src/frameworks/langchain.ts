4364ab8ba19913b2847d2d5e095feb30eb26babc
Apoorv Taneja <purutaneja.com@gmail.com>
2025-02-27 13:48:19 +0530

    feat: added allow tracing in toolset (#1374)

--- js/src/frameworks/langchain.ts
+++ js/src/frameworks/langchain.ts
@@ -24,6 +24,7 @@ export class LangchainToolSet extends BaseComposioToolSet {
       entityId?: string;
       runtime?: string;
       connectedAccountIds?: Record<string, string>;
+      allowTracing?: boolean;
     } = {}
   ) {
     super({
@@ -32,6 +33,7 @@ export class LangchainToolSet extends BaseComposioToolSet {
       runtime: config?.runtime || LangchainToolSet.FRAMEWORK_NAME,
       entityId: config.entityId || LangchainToolSet.DEFAULT_ENTITY_ID,
       connectedAccountIds: config.connectedAccountIds,
+      allowTracing: config.allowTracing || false,
     });
   }
 
