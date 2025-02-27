b4bffa20938ff37a23f1d178c24c0822b67a1d06
Karan Vaidya <kaavee315@gmail.com>
2025-02-27 14:39:58 +0530

    feat: Allow tracing to get log and session id (#1371)
    Co-authored-by: random <random@gmail.com>

--- python/composio/client/collections.py
+++ python/composio/client/collections.py
@@ -1306,6 +1306,7 @@ class Actions(Collection[ActionModel]):
         session_id: t.Optional[str] = None,
         text: t.Optional[str] = None,
         auth: t.Optional[CustomAuthObject] = None,
+        allow_tracing: bool = False,
     ) -> t.Dict:
         """
         Execute an action on the specified entity with optional connected account.
@@ -1329,6 +1330,7 @@ class Actions(Collection[ActionModel]):
                         "sessionInfo": {
                             "sessionId": session_id,
                         },
+                        "allowTracing": allow_tracing,
                     },
                 )
             ).json()
@@ -1353,6 +1355,7 @@ class Actions(Collection[ActionModel]):
                     "sessionInfo": {
                         "sessionId": session_id,
                     },
+                    "allowTracing": allow_tracing,
                 },
             )
         ).json()
