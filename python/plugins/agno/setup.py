1bca3351d33e71318d0a3eb9b94abe7de43d2d4f
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-03-10 12:27:37 +0530

    feat: release v0.7.7 (#1419)

--- python/plugins/agno/setup.py
+++ python/plugins/agno/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_agno",
-    version="0.7.6",
+    version="0.7.7",
     author="Devanshu",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Agno Plugin.",
