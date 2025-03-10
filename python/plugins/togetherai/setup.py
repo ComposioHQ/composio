3e63f1749524c7f45554d1d8a3c44f7733df4cb0
angrybayblade <vptl185@gmail.com>
2025-03-10 12:13:05 +0530

    feat: release v0.7.7

--- python/plugins/togetherai/setup.py
+++ python/plugins/togetherai/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_togetherai",
-    version="0.7.6",
+    version="0.7.7",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Together AI Function Call.",
