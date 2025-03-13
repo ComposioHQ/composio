b3a16fd5c281f1cfd69aeb7e6fb9ce260462e40a
Siddharth <balyan.sid@gmail.com>
2025-03-13 08:27:15 +0530

    refactor: Update chat integration with new styles and feedback options

--- fern/custom.js
+++ fern/custom.js
@@ -1,24 +1,35 @@
-(function(d, script) {
-  script = d.createElement('script');
+(function (d, script) {
+  script = d.createElement("script");
   script.async = false;
-  script.onload = function(){
+  script.onload = function () {
     Plain.init({
-        appId: 'liveChatApp_01JP6DQTFKKCPPVVVCZG5F99RP',
-        hideLauncher: false,
-        links: [
-          { icon: 'discord', text: 'Join our Discord', url: 'https://discord.com/invite/cNruWaAhQk' },
-        ],
-        hideBranding: true,
-        theme: 'dark',
-        style: {
-          brandColor: '#076CDF',
-          brandBackgroundColor: '#076CDF',
-          launcherBackgroundColor: '#FFFFFF',
-          launcherIconColor: '#076CDF',
+      appId: "liveChatApp_01JP6DQTFKKCPPVVVCZG5F99RP",
+      hideLauncher: false,
+      links: [
+        {
+          icon: "discord",
+          text: "Join our Discord",
+          url: "https://dub.composio.dev/discord",
         },
-        chatButtons: [{ icon: 'chat', text: 'Chat with us' }],
-      });
+      ],
+      hideBranding: true,
+      theme: "dark",
+      style: {
+        brandColor: "#6366F1",
+        brandBackgroundColor: "#6366F1",
+        launcherBackgroundColor: "#F8F9FF",
+        launcherIconColor: "#6366F1",
+      },
+      chatButtons: [
+        { icon: "chat", text: "Chat with us" },
+        {
+          icon: "bulb",
+          text: "Give feedback",
+          threadDetails: { labelTypeIds: ["lt_01JP6PBF53YD6EXC2TNEW0XES4"] },
+        },
+      ],
+    });
   };
-  script.src = 'https://chat.cdn-plain.com/index.js';
-  d.getElementsByTagName('head')[0].appendChild(script);
-}(document));
+  script.src = "https://chat.cdn-plain.com/index.js";
+  d.getElementsByTagName("head")[0].appendChild(script);
+})(document);
