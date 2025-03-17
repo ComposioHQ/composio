59099cc7772adff6b1e8bc2ee3b30fb8b52935ec
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-03-17 15:05:15 +0530

    feat: bump to v0.7.9 (#1452)

--- python/plugins/langchain/setup.py
+++ python/plugins/langchain/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="0.7.8",
+    version="0.7.9",
     author="Karan",
     author_email="karan@composio.dev",
     description="Use Composio to get an array of tools with your LangChain agent.",
