84f6c3264264bc363ad47c1745197a3fb947f203
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-03-18 16:12:20 +0530

    feat: release v0.7.10 (#1455)

--- python/plugins/llamaindex/setup.py
+++ python/plugins/llamaindex/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_llamaindex",
-    version="0.7.9",
+    version="0.7.10",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your LlamaIndex agent.",
