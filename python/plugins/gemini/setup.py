b5ab6a60ba1e394c33a4e91ef1b539b5effcce5d
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-04-01 15:53:17 +0530

    feat: release v0.7.13 (#1513)

--- python/plugins/gemini/setup.py
+++ python/plugins/gemini/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_gemini",
-    version="0.7.12",
+    version="0.7.13",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Gemini agent.",
