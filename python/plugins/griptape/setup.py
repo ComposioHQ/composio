2463db47d34b2c607d25f8bf17c3f6e32c1ef787
Viraj <35092918+angrybayblade@users.noreply.github.com>
2025-04-02 13:48:03 +0530

    feat: release v0.7.14 (#1515)

--- python/plugins/griptape/setup.py
+++ python/plugins/griptape/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_griptape",
-    version="0.7.13",
+    version="0.7.14",
     author="Sawradip",
     author_email="sawradip@composio.dev",
     description="Use Composio to get an array of tools with your Griptape workflow.",
