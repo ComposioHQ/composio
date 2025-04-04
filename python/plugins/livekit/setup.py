518a0a9362953f81864983317e666fa7a0664a68
Sushmithamallesh <sushdec6@gmail.com>
2025-04-04 15:57:52 +0530

    Create livekit composio plugin

---
+++ python/plugins/livekit/setup.py
@@ -0,0 +1,33 @@
+"""
+Setup configuration for Composio LiveKit plugin
+"""
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="composio_livekit",
+    version="0.1.0",
+    author="Sushmitha",
+    author_email="soham@composio.dev",
+    description="Use Composio Toolset with LiveKit",
+    long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ComposioHQ/composio",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9,<4",
+    packages=find_packages(),
+    install_requires=[
+        "composio_core>=0.7.0,<0.8.0",
+        "livekit>=0.14.0",
+        "pydantic>=2.0.0",
+        "typing-extensions>=4.0.0",
+    ],
+    include_package_data=True,
+)
