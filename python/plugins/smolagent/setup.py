c66c154186fc74eb86d82b314eb837e509affb93
random <random@gmail.com>
2025-04-08 03:15:16 +0530

    Release

--- python/plugins/smolagent/setup.py
+++ python/plugins/smolagent/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_smol",
-    version="0.7.14",
+    version="0.7.15",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get array of strongly typed tools for Smol Agents",
