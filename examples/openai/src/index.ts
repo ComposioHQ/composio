872dd4ab235477ffe15c613e82b6f9d9401d554e
haxzie <musthu.gm@gmail.com>
2025-04-28 12:22:06 +0530

    Fix github workflow

--- examples/openai/src/index.ts
+++ examples/openai/src/index.ts
@@ -1,5 +1,5 @@
-import { Composio } from "@composio/core";
-import { OpenAI } from "openai";
+import { Composio } from '@composio/core';
+import { OpenAI } from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,27 +13,27 @@ const composio = new Composio({
 
 // Fetch all the tools from the Composio API
 // these tools are automatically typed and wrapped with the OpenAI Toolset
-const tool = await composio.getTool("HACKERNEWS_GET_USER");
+const tool = await composio.getTool('HACKERNEWS_GET_USER');
 
 const task = "Fetch the details of the user 'haxzie'";
 
 const messages: OpenAI.ChatCompletionMessageParam[] = [
-  { role: "system", content: "You are a helpful assistant that can help with tasks." },
-  { role: "user", content: task },
-]
+  { role: 'system', content: 'You are a helpful assistant that can help with tasks.' },
+  { role: 'user', content: task },
+];
 
 const response = await openai.chat.completions.create({
-  model: "gpt-4o-mini",
+  model: 'gpt-4o-mini',
   messages,
   tools: [tool],
-  tool_choice: "auto",
+  tool_choice: 'auto',
 });
-
 
 if (response.choices[0].message.tool_calls) {
   try {
-
-    const result = await composio.toolset.executeToolCall(response.choices[0].message.tool_calls[0]);
+    const result = await composio.toolset.executeToolCall(
+      response.choices[0].message.tool_calls[0]
+    );
     console.log(JSON.stringify(result, null, 2));
   } catch (error) {
     console.error(JSON.stringify(error, null, 2));
