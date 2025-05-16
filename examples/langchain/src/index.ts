b9accfd6942891f7ecf197da9540605f3f51143b
haxzie <musthu.gm@gmail.com>
2025-05-16 15:39:49 +0530

    Update langchain example

--- examples/langchain/src/index.ts
+++ examples/langchain/src/index.ts
@@ -1,16 +1,68 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { HumanMessage, AIMessage } from '@langchain/core/messages';
+import { ToolNode } from '@langchain/langgraph/prebuilt';
+import { StateGraph, MessagesAnnotation } from '@langchain/langgraph';
 import { Composio } from '@composio/core';
 import { LangchainToolset } from '@composio/langchain';
-import process from 'process';
-
+// initiate composio
 const composio = new Composio({
   apiKey: process.env.COMPOSIO_API_KEY,
   toolset: new LangchainToolset(),
 });
 
-// direct tool access
-const tools = await composio.getTools();
+// fetch the tool
+const tool = await composio.tools.get('default', 'HACKERNEWS_GET_USER');
 
-console.log(tools);
+// Define the tools for the agent to use
+const tools = [tool];
+const toolNode = new ToolNode(tools);
 
+// Create a model and give it access to the tools
+const model = new ChatOpenAI({
+  model: 'gpt-4o-mini',
+  temperature: 0,
+}).bindTools(tools);
 
+// Define the function that determines whether to continue or not
+function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
+  const lastMessage = messages[messages.length - 1] as AIMessage;
 
+  // If the LLM makes a tool call, then we route to the "tools" node
+  if (lastMessage.tool_calls?.length) {
+    return 'tools';
+  }
+  // Otherwise, we stop (reply to the user) using the special "__end__" node
+  return '__end__';
+}
+
+// Define the function that calls the model
+async function callModel(state: typeof MessagesAnnotation.State) {
+  const response = await model.invoke(state.messages);
+
+  // We return a list, because this will get added to the existing list
+  return { messages: [response] };
+}
+
+// Define a new graph
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode('agent', callModel)
+  .addEdge('__start__', 'agent') // __start__ is a special name for the entrypoint
+  .addNode('tools', toolNode)
+  .addEdge('tools', 'agent')
+  .addConditionalEdges('agent', shouldContinue);
+
+// Finally, we compile it into a LangChain Runnable.
+const app = workflow.compile();
+
+// Use the agent
+const finalState = await app.invoke({
+  messages: [new HumanMessage('Find the details of the user `pg` on HackerNews')],
+});
+console.log(finalState.messages[finalState.messages.length - 1].content);
+
+const nextState = await app.invoke({
+  // Including the messages from the previous run gives the LLM context.
+  // This way it knows we're asking about the weather in NY
+  messages: [...finalState.messages, new HumanMessage('what about haxzie')],
+});
+console.log(nextState.messages[nextState.messages.length - 1].content);
