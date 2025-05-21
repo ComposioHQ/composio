3c36cce93e2e1af05c81987e712961c6715307c2
haxzie <musthu.gm@gmail.com>
2025-05-21 17:36:49 +0530

    Fix version checks

--- packages/core/src/utils/version.ts
+++ packages/core/src/utils/version.ts
@@ -1,5 +1,6 @@
 import logger from './logger';
 import { IS_DEVELOPMENT_OR_CI } from './constants';
+import semver from 'semver';
 
 /**
  * Compares two semantic versions and returns true if the first version is newer than the second.
@@ -24,15 +25,29 @@ export function isNewerVersion(version1: string, version2: string): boolean {
  */
 export async function checkForLatestVersionFromNPM(currentVersion: string) {
   try {
-    const packageName = 'composio-core';
+    const packageName = '@composio/core';
     const currentVersionFromPackageJson = currentVersion;
+
+    // ignore if the current version is not a valid semantic version
+    if (!semver.valid(currentVersionFromPackageJson)) {
+      return;
+    }
+
+    // ignore if the current version is alpha or beta
+    const prerelease = semver.prerelease(currentVersionFromPackageJson);
+    if (
+      prerelease &&
+      (String(prerelease[0]).includes('alpha') || String(prerelease[0]).includes('beta'))
+    ) {
+      return;
+    }
 
     // @TODO: Check if fetch is available, if not use node-fetch
     const response = await fetch(`https://registry.npmjs.org/${packageName}/latest`);
     const data = await response.json();
     const latestVersion = data.version;
 
-    if (isNewerVersion(latestVersion, currentVersionFromPackageJson) && !IS_DEVELOPMENT_OR_CI) {
+    if (semver.gt(latestVersion, currentVersionFromPackageJson) && !IS_DEVELOPMENT_OR_CI) {
       logger.info(
         `🚀 Upgrade available! Your composio-core version (${currentVersionFromPackageJson}) is behind. Latest version: ${latestVersion}.`
       );
