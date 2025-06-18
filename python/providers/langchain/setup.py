7001ab47dc95381deff49aaac03dfcc45df27436
angrybayblade <vptl185@gmail.com>
2025-06-18 11:20:31 +0530

    chore: bump python packages to rc2

--- python/providers/langchain/setup.py
+++ python/providers/langchain/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="1.0.0rc1",
+    version="1.0.0-rc2",
     author="composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Langchain agent.",
