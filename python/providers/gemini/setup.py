ee858a7ae40eea15fdfbc85fbc9999e8c8be79fc
angrybayblade <vptl185@gmail.com>
2025-06-19 12:52:26 +0530

    chore: version sync between core and providers

--- python/providers/gemini/setup.py
+++ python/providers/gemini/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_gemini",
-    version="1.0.0-rc3",
+    version="1.0.0-rc4",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Gemini agent.",
