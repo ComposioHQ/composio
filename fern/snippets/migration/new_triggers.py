b5eb33426b3147d670eabbe0fe17ad5b7c9cf5d8
balyan.sid@gmail.com <balyan.sid@gmail.com>
2025-06-25 21:07:26 -0700

    docs: update migration guide and add new trigger snippets

--- fern/snippets/migration/new_triggers.py
+++ fern/snippets/migration/new_triggers.py
@@ -3,14 +3,6 @@ from composio import Composio
 composio = Composio()
 
 user_id = "user@example.com"
-
-trigger = composio.triggers.create(
-    slug="GITHUB_COMMIT_EVENT",
-    user_id=user_id,
-    trigger_config={"repo": "composiohq", "owner": "composio"},
-)
-print(trigger)
-
 trigger_config = composio.triggers.get_type("GITHUB_COMMIT_EVENT")
 print(trigger_config.config)
 ### Trigger Config
@@ -30,3 +22,15 @@ print(trigger_config.config)
 #     "required": ["owner", "repo"],
 #     "title": "WebhookConfigSchema",
 #     "type": "object"
+
+trigger = composio.triggers.create(
+    slug="GITHUB_COMMIT_EVENT",
+    user_id=user_id,
+    trigger_config={"repo": "composiohq", "owner": "composio"},
+)
+print(trigger)
+
+
+# Managing triggers
+
+composio.triggers.enable(id="ti_abcd123")
