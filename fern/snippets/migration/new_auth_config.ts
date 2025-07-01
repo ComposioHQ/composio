594297c48078f9dab9bbbeaacef88fe5c8a12925
abhishekpatil4 <abhishekpersonal2@gmail.com>
2025-07-01 12:51:53 +0530

    fix callback url

--- fern/snippets/migration/new_auth_config.ts
+++ fern/snippets/migration/new_auth_config.ts
@@ -9,7 +9,7 @@ const authConfig = await composio.authConfigs.create('LINEAR', {
   //   credentials: {
   //     client_id: "1234567890",
   //     client_secret: "1234567890",
-  //     oauth_redirect_uri: "https://backend.composio.dev/api/v3/toolkits/callback",
+  //     oauth_redirect_uri: "https://backend.composio.dev/api/v3/toolkits/auth/callback",
   // },
 });
 
