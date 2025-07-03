386a3a561c0058588989ec4643dd50cc6f3a29f4
angrybayblade <vptl185@gmail.com>
2025-07-03 11:20:01 +0530

    fix: remove unwanted usage of abc

--- python/composio/core/provider/base.py
+++ python/composio/core/provider/base.py
@@ -6,14 +6,13 @@ Defines the barebones provider metaclass that needs to be subclassed for every p
 
 from __future__ import annotations
 
-import abc
 import typing as t
 
 TTool = t.TypeVar("TTool", covariant=True)
 TToolCollection = t.TypeVar("TToolCollection", covariant=True)
 
 
-class BaseProvider(abc.ABC, t.Generic[TTool, TToolCollection]):
+class BaseProvider(t.Generic[TTool, TToolCollection]):
     """
     BaseProvider class
 
