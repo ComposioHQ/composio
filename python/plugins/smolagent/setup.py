9feb145c8f584b7b25e34487668acf8d95a26a33
angrybayblade <vptl185@gmail.com>
2025-07-03 14:16:51 +0530

    feat: release v0.7.20

--- python/plugins/smolagent/setup.py
+++ python/plugins/smolagent/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="composio_smol",
-    version="0.7.19",
+    version="0.7.20",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get array of strongly typed tools for Smol Agents",
