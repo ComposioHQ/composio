edc4c4cf953da2d4f4ef7495b46aa2ca973a2f28
angrybayblade <vptl185@gmail.com>
2025-07-24 16:38:30 +0530

    fix: handle python reserved keywords when building a langchain tool

--- python/providers/langchain/composio_langchain/provider.py
+++ python/providers/langchain/composio_langchain/provider.py
@@ -15,6 +15,55 @@ from composio.utils.shared import (
     json_schema_to_model,
 )
 
+_python_reserved = {"for", "async"}
+_obj_marker = "-_object_-"
+
+
+def _clean_reserved_keyword(keyword: str):
+    return f"{keyword}_rs"
+
+
+def _substitute_reserved_python_keywords(schema: t.Dict) -> t.Tuple[dict, dict]:
+    if "properties" not in schema:
+        return schema, {}
+
+    keywords = {}
+    for p_name in list(schema["properties"]):
+        if p_name not in _python_reserved:
+            continue
+
+        _keywords = {}
+        p_val = schema["properties"].pop(p_name)
+        if p_val.get("type") == "object":
+            p_val, _keywords = _substitute_reserved_python_keywords(schema=p_val)
+
+        p_name_clean = _clean_reserved_keyword(keyword=p_name)
+        schema["properties"][p_name_clean] = p_val
+        keywords[p_name_clean] = p_name
+        keywords[f"{p_name_clean}{_obj_marker}"] = _keywords
+
+    return schema, keywords
+
+
+def _reinstate_reserved_python_keywords(request: dict, keywords: dict) -> dict:
+    for clean_key in sorted(list(keywords), reverse=True):
+        subkeys = None
+        if clean_key.endswith(_obj_marker):
+            subkeys = keywords[clean_key]
+            clean_key, _ = clean_key.split(_obj_marker, maxsplit=1)
+
+        if clean_key not in request:
+            continue
+
+        orginal_value = request.pop(clean_key)
+        if subkeys is not None:
+            orginal_value = _reinstate_reserved_python_keywords(
+                request=orginal_value,
+                keywords=subkeys,
+            )
+        request[keywords[clean_key]] = orginal_value
+    return request
+
 
 class StructuredTool(BaseStructuredTool):
     def run(self, *args, **kwargs):
@@ -34,21 +83,30 @@ class LangchainProvider(
 
     runtime = "langchain"
 
-    def _wrap_action(
-        self,
-        tool: str,
-        description: str,
-        schema_params: t.Dict,
-        execute_tool: AgenticProviderExecuteFn,
-    ):
+    def wrap_tool(
+        self, tool: Tool, execute_tool: AgenticProviderExecuteFn
+    ) -> StructuredTool:
+        """Wraps composio tool as Langchain StructuredTool object."""
+        # Replace reserved python keywords
+        schema_params, keywords = _substitute_reserved_python_keywords(
+            schema=tool.input_parameters
+        )
+
         def function(**kwargs: t.Any) -> t.Dict:
             """Wrapper function for composio action."""
-            return execute_tool(tool, kwargs)
+            print(kwargs)
+            kwargs = _reinstate_reserved_python_keywords(
+                request=kwargs,
+                keywords=keywords,
+            )
+            print(kwargs)
+            exit()
+            return execute_tool(tool.slug, kwargs)
 
         action_func = types.FunctionType(
             function.__code__,
             globals=globals(),
-            name=tool,
+            name=tool.slug,
             closure=function.__closure__,
         )
         action_func.__signature__ = Signature(  # type: ignore
@@ -56,29 +114,19 @@ class LangchainProvider(
                 schema_params=schema_params
             )
         )
-        action_func.__doc__ = description
-        return action_func
+        action_func.__doc__ = tool.description
 
-    def wrap_tool(
-        self, tool: Tool, execute_tool: AgenticProviderExecuteFn
-    ) -> StructuredTool:
-        """Wraps composio tool as Langchain StructuredTool object."""
         return t.cast(
             StructuredTool,
             StructuredTool.from_function(
                 name=tool.slug,
                 description=tool.description,
                 args_schema=json_schema_to_model(
-                    json_schema=tool.input_parameters,
+                    json_schema=schema_params,
                     skip_default=self.skip_default,
                 ),
                 return_schema=True,
-                func=self._wrap_action(
-                    tool=tool.slug,
-                    description=tool.description,
-                    schema_params=tool.input_parameters,
-                    execute_tool=execute_tool,
-                ),
+                func=action_func,
                 handle_tool_error=True,
                 handle_validation_error=True,
             ),
