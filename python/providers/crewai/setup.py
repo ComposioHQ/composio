c3555582c20929a09b9c8c8a298bff2202a95391
Prateek <prateek@composio.dev>
2025-09-26 17:38:37 +0530

    Update providers

--- python/providers/crewai/setup.py
+++ python/providers/crewai/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="composio_crewai",
-    version="0.8.13",
+    version="0.8.14",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your CrewAI agent.",
