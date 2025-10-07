7cdc530c5c5ee4e5591eadf4acf2e86d0d9eed62
Sushmithamallesh <sushdec6@gmail.com>
2025-10-06 19:00:49 -0700

    toolrouter docs updates

--- fern/snippets/tool-router/python/quick-start.py
+++ fern/snippets/tool-router/python/quick-start.py
@@ -6,5 +6,10 @@ userId = "hey@example.com"
 session = composio.experimental.tool_router.create_session(
     user_id=userId,
 )
+# Returns:
+# {
+#   'session_id': 'dKDoDWAGUf-hPM-Bw39pJ',
+#   'url': 'https://apollo.composio.dev/v3/mcp/tool-router/dKDoDWAGUf-hPM-Bw39pJ/mcp'
+# }
 
 mcpUrl = session['url']
