16f1f550273d2e8a28cefdd469b7e3d8b1213c2e
Rahul Tarak <cryogenicplanet@gmail.com>
2025-10-06 21:54:14 -0700

    use the proper type challenges package

--- ts/packages/core/type-tests/type-challenges-utils.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@type-challenges/utils' {
-  export type Expect<T extends true> = T;
-  export type Equal<X, Y> =
-    (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
-      ? (<T>() => T extends Y ? 1 : 2) extends <T>() => T extends X ? 1 : 2
-        ? true
-        : false
-      : false;
-}
