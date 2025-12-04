8739e59d08f51102b6b5216ae9c19cb7c306f767
haxzie <musthu.gm@gmail.com>
2025-12-04 13:01:12 +0530

    Add pre release tag

--- python/providers/anthropic/setup.py
+++ python/providers/anthropic/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="composio_anthropic",
-    version="0.9.4",
+    version="0.9.5-rc.1",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Anthropic LLMs.",
--- python/providers/openai_agents/setup.py
+++ python/providers/openai_agents/setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="composio_openai_agents",
-    version="0.9.4",
+    version="0.9.5-rc.1",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get array of strongly typed tools for OpenAI Agents",
