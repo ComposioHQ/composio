456d31534f870d4001aaa077b2be011be5997bfc
haxzie <musthu.gm@gmail.com>
2025-12-16 13:52:25 +0530

    Bump python package

--- python/providers/langchain/setup.py
+++ python/providers/langchain/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="composio_langchain",
-    version="0.10.0",
+    version="0.10.1",
     author="composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Langchain agent.",
