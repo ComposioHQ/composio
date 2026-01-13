5477b4375cdf442a0cfc2db7850277fc77ddc03b
haxzie <musthu.gm@gmail.com>
2026-01-13 14:06:45 +0530

    Bump python packages

--- python/providers/google/setup.py
+++ python/providers/google/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="composio_google",
-    version="0.10.5",
+    version="0.10.6",
     author="Composio",
     author_email="tech@composio.dev",
     description="Use Composio to get an array of tools with your Google AI Python Gemini model.",
