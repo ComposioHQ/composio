a013419295ca1cbbca24e69f4662d61e5c2b3ac3
jkomyno <12381818+jkomyno@users.noreply.github.com>
2026-01-29 04:16:42 +0400

    feat(ts): add trigger subscription example

--- ts/examples/triggers/src/subscribe.ts
+++ ts/examples/triggers/src/subscribe.ts
@@ -5,14 +5,19 @@ const composio = new Composio({
   apiKey: process.env.COMPOSIO_API_KEY,
 });
 
+// create a trigger
+const { triggerId } = await composio.triggers.create('default','GMAIL_NEW_GMAIL_MESSAGE', {
+  connectedAccountId: 'ca_uQvmu9uZOhQo',
+  triggerConfig: {},
+});
+
 // Subscribe to triggers with optional filters
-// Replace 'ti_ollBPgfhgmMK' with your actual trigger ID
 const sub = await composio.triggers.subscribe(
   metadata => {
     console.log('Received trigger event:');
     console.dir(metadata, { depth: 3 });
   },
-  { triggerId: 'ti_ollBPgfhgmMK' }
+  { triggerId }
 );
 
 console.log('Subscribed to triggers. Waiting for events...');
