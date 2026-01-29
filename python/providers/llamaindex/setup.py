a7a764428d9470d71f32f1c2c26c3cb6251466db
haxzie <musthu.gm@gmail.com>
2026-01-29 14:54:59 +0530

    Bump python packages

--- python/providers/llamaindex/setup.py
+++ python/providers/llamaindex/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="composio_llamaindex",
-    version="0.10.10",
+    version="0.10.11",
     author="composio",
     author_email="tech@composio.dev",
     description="Use Composio to get array of tools with LlamaIndex Agent Workflows",
