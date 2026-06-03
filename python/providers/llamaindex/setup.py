4cfc5308c5ba3db9c0513f39208ed2062e9a4d2f
jkomyno <alberto@composio.dev>
2026-06-03 10:52:18 +0400

    feat(python): remove legacy custom tools

--- python/providers/llamaindex/setup.py
+++ python/providers/llamaindex/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="composio_llamaindex",
-    version="0.13.1",
+    version="0.14.0",
     author="composio",
     author_email="tech@composio.dev",
     description="Use Composio to get array of tools with LlamaIndex Agent Workflows",
